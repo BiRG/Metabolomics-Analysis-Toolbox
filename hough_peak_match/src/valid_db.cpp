ee724ce0ba879956b07ecc82ad3165ca5aa0ab0c
Eric David Moyer <eric_moyer@yahoo.com>
2011-05-02 10:08:37 -0400

    Wrote argument parsing and file opening parts of valid_db -- and added test code exercising them.

--- hough_peak_match/src/valid_db.cpp
+++ hough_peak_match/src/valid_db.cpp
@@ -1,3 +1,33 @@
+#include "peak_matching_database.h"
+#include <iostream>
+#include <fstream>
+#include <cstdlib> //For exit
+#include <string>
+
+void printUsageAndExit(std::string errMsg){
+  std::cerr 
+    << "Synopsis: valid_db db_file\n"
+    << "\n"
+    << "Checks whether the given file parses as a valid peak-matching\n"
+    << "database or not.\n"
+    << "\n"
+    << "Prints \"Valid\" or \"Invalid\" to stdout depending on whether the\n"
+    << "database is valid or not.\n"
+    << "\n"
+    << errMsg << "\n";
+  std::exit(-1);
+}
+
 int main(int argc, char**argv){
+  if(argc != 2){
+    printUsageAndExit("ERROR: Wrong number of arguments");
+  }
+
+  char const * const db_file_name = argv[1];
+  std::ifstream db_stream(db_file_name);
+  if(!db_stream){
+    printUsageAndExit(std::string("ERROR: Could not open \"")+
+		      db_file_name+"\"");
+  }
   return 0;
 }
