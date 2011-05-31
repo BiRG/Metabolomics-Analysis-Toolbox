9208c1dc287116eb42d47fadd9e27e7bb3f990c3
Eric David Moyer <eric_moyer@yahoo.com>
2011-05-31 16:22:26 -0400

    Added argument parsing and usage message to hough_sample_params as well as tests of the same.

--- hough_peak_match/src/hough_sample_params.cpp
+++ hough_peak_match/src/hough_sample_params.cpp
@@ -1,25 +1,69 @@
 ///\file
 ///\brief Main routine and supporting code for the hough_sample_params executable
 
+#include <sstream>
 #include <iostream>
 #include <cstdlib> //For exit
 
-///The main routine for hough_sample_params
+///\brief Print error message and usage information before exiting with an error
+///
+///Prints the usage message for hough_sample_params and then prints errMsg
+///(followed by a newline) before finally exiting with a -1 error
+///code.  Does not return.
+///
+///\param errMsg the error message to print after the usage message
+void print_usage_and_exit(std::string errMsg){
+  std::cerr 
+    << "Synopsis: hough_sample_params [options] fractionVariance < initial_db > db_with_sample_params\n"
+    << "\n"
+    << "Reads a peak database from standard input. Extracts the set \n"
+    << "of peak_groups that have a representative in every sample. \n"
+    << "Creates a set of sample parameters that accounts for at least \n"
+    << "fractionVariance [0..1] of the variance of the peaks in those \n"
+    << "peak groups. Outputs a database with these new sample parameters \n"
+    << "along with a param_stats object added.\n"
+    << "\n"
+    << "If there are already sample_params objects in the database, then \n"
+    << "three things differ. First, the number of parameters is set to be \n"
+    << "the same as the number of parameters in the existing objects. \n"
+    << "Second, only samples not associated with an extant sample_params \n"
+    << "object are given new sample_params objects. Third, the \n"
+    << "param_stats object will be updated.\n"
+    << "\n"
+    << "Options:\n"
+    << "  --remove-sample-params  removes all sample_params objects from the \n"
+    << "                          input database before processing it.\n"
+    << "\n"
+    << errMsg << "\n";
+  std::exit(-1);
+}
 
+
+///\brief The main routine for hough_sample_params
+///
 ///\param argc The number of elements in the argv vector
 ///
 ///\param argv The command line arguments to this program - an array of strings
 ///
 ///\return error status to the operating system
 int main(int argc, char**argv){
+  using std::string;
   if(argc != 2 && argc != 3){
-    ///\todo write print usage and exit
-    exit(-1);
+    print_usage_and_exit("ERROR: Wrong number of arguments.");
   }
-  double fractionVariance = atof(argv[argc-1]);
-  bool shouldRemoveSampleParamsFirst=argc==3;
+
+  double fraction_variance = -1;
+  std::istringstream frac_var_in(argv[argc-1]);
+  frac_var_in >> fraction_variance;
+  if(fraction_variance < 0 || fraction_variance > 1){
+    print_usage_and_exit("ERROR: fraction of variance must be between "
+			 "0 and 1, inclusive.  You wrote: "+ 
+			 string(argv[argc-1]));
+  }
+  bool should_remove_sample_params_first=
+    argc==3 && argv[argc-2]==string("--remove-sample-params");
   ///\todo main is stub
-  std::cout << "#fractionVariance="<<fractionVariance 
-	    << "\n#removeSampleParams="<<shouldRemoveSampleParamsFirst<<"\n";
+  std::cout << "#fraction_variance="<<fraction_variance 
+	    << "\n#removeSampleParams="<<should_remove_sample_params_first<<"\n";
   return 0;
 }
--- hough_peak_match/tests/002-hough_sample_params.t
+++ hough_peak_match/tests/002-hough_sample_params.t
@@ -8,7 +8,67 @@ use warnings;
 
 #######################################################################
 
-use Test::More tests => 2;
+use Test::More tests => 10;
+
+#################
+#
+# Global variables
+#
+################
+undef $/; #Slurp mode for all files
+
+#TAP test assertion that the given command line fails and that the error message
+#output is the same as that recorded in the given file
+#
+#$error_condition is a string suitable for substitution in the string:
+#"equivalent_db did exit(-1) when $error_condition"
+sub command_line_error_message_is($$$){
+    my ($command_line,$error_message_file,$error_condition)=@_;
+    my $error_message = `$command_line 2>&1`;
+    my $exitValue = $?;
+    is($exitValue, 65280, "hough_sample_params did exit(-1) when $error_condition");
+    
+    open(my $fh,"<",$error_message_file) or die $!;
+    my $expected_error_message = <$fh>;
+    undef $fh;
+    
+    is($error_message, $expected_error_message, 
+       "hough_sample_params  correct error when $error_condition");
+}
+
+##################
+# 
+# Check the error mesage and status when too few or too many arguments
+#
+##################
+
+command_line_error_message_is(
+    "../hough_sample_params 0.99 0.99 0.99",
+    "data/hough_sample_params_usage_message_wrong_num_args.txt",
+    "there are too many arguments.");
+
+command_line_error_message_is(
+    "../hough_sample_params",
+    "data/hough_sample_params_usage_message_wrong_num_args.txt",
+    "there are too few arguments.");
+
+
+##################
+# 
+# Check the error mesage and status when frac_var too large or small
+#
+##################
+
+command_line_error_message_is(
+    "../hough_sample_params -0.9",
+    "data/hough_sample_params_usage_message_-0.9_frac_var.txt",
+    "fraction of variance is too small.");
+
+command_line_error_message_is(
+    "../hough_sample_params 1.1",
+    "data/hough_sample_params_usage_message_1.1_frac_var.txt",
+    "fraction of variance is too large.");
+
 
 #########
 #
