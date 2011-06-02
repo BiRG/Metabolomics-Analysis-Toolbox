90d9cd4e45e0275aaa27bfee6c169cbcc306a6a9
Eric David Moyer <eric_moyer@yahoo.com>
2011-06-02 18:35:45 -0400

    Added code to calculate the sample parameters to hough_sample_params

--- hough_peak_match/src/hough_sample_params.cpp
+++ hough_peak_match/src/hough_sample_params.cpp
@@ -3,13 +3,16 @@
 #include "remove_sample_params_from.hpp"
 #include "peak_matching_database.hpp"
 #include "peak_group_key.hpp"
-#include "file_format_sample_params.hpp"
+#include "gsl_vector.hpp"
+#include "gsl_matrix.hpp"
+#include <gsl/gsl_linalg.h>
 #include <sstream>
 #include <iostream>
 #include <cstdlib> //For exit
 #include <utility>
 #include <set>
 #include <map>
+
 
 ///\brief Print error message and usage information before exiting with an error
 ///
@@ -104,6 +107,24 @@ namespace HoughPeakMatch{
     }
     return ret;
   }
+
+  ///\brief subtract the mean of each column from each value in the column
+  ///
+  ///\param m The matrix whose columns will have the mean subtracted
+  void mean_center_columns(GSL::Matrix& m){
+    if(m.rows() < 1){  //Skip empty matrix
+      return; }
+    for(std::size_t col = 0; col < m.cols(); ++col){
+      double sum = 0;
+      for(std::size_t row = 0; row < m.rows(); ++row){
+	sum += m.at(row,col);
+      }
+      double mean = sum / m.rows();
+      for(std::size_t row = 0; row < m.rows(); ++row){
+	m.at(row,col) -= mean;
+      }
+    }
+  }
   
   ///\brief Calculate the sample parameters for all samples in \a db
   ///using the peak_groups in \a pg_in_all_samples
@@ -112,6 +133,8 @@ namespace HoughPeakMatch{
   ///the peak in each peak-group in each sample to calculate a set of
   ///parameters affecting peak positions in each sample that account
   ///for at least \a frac_variance fraction of the peak_position variance.
+  ///
+  ///Assumes db has no ParameterizedSample objects
   ///
   ///\param db The database for whose samples to calculate the parameters
   ///
@@ -124,14 +147,130 @@ namespace HoughPeakMatch{
   ///
   ///\return A pair consisting of the parameters inferred for each
   ///sample and the fractional variances of those parameters
-  std::pair<std::set<FileFormatSampleParams>,ParamStats> 
+  std::pair<std::set<ParameterizedSample>,ParamStats> 
   calculate_sample_parameters(const PeakMatchingDatabase& db, 
 			      const std::set<PeakGroupKey>& pg_in_all_samples,
-			      double frac_variance){
-    ///\todo stub
-    std::vector<double> d; d.push_back(1);
-    return std::make_pair(std::set<FileFormatSampleParams>(),
-			  ParamStats(d.begin(),d.end()));
+			      double frac_variance){    
+    using std::map;
+    assert(db.parameterized_samples().size() == 0);
+
+    //Make the matrix of peak positions (each column is a sample, each
+    //row is a peak-group) and record the mapping between sample_ids
+    //and columns and peak_group_ids and rows
+    GSL::Matrix peak_pos(pg_in_all_samples.size(),
+			 db.unparameterized_samples().size());
+    map<unsigned, unsigned> 
+      row_to_pg_id, pg_id_to_row, //pg_id = peak_group_id
+      col_to_sample_id, sample_id_to_col;
+    {
+      std::vector<UnparameterizedSample>::const_iterator cur_samp;
+      for(cur_samp = db.unparameterized_samples().begin();
+	  cur_samp != db.unparameterized_samples().end(); ++cur_samp){
+	unsigned col = cur_samp - db.unparameterized_samples().begin();
+	col_to_sample_id[col] = cur_samp->id();
+	sample_id_to_col[cur_samp->id()] = col;
+      }
+    }
+
+    {
+      std::set<PeakGroupKey>::const_iterator cur_pg;
+      unsigned row = 0;
+      for(cur_pg = pg_in_all_samples.begin(); 
+	  cur_pg != pg_in_all_samples.end(); ++cur_pg, ++row){
+	row_to_pg_id[row] = cur_pg->id();
+	pg_id_to_row[cur_pg->id()] = row;
+      }
+    }
+
+    {
+      std::vector<HumanVerifiedPeak>::const_iterator cur_peak;
+      for(cur_peak = db.human_verified_peaks().begin(); 
+	  cur_peak != db.human_verified_peaks().end(); ++cur_peak){
+	unsigned col = sample_id_to_col[cur_peak->sample_id()];
+	unsigned row = pg_id_to_row[cur_peak->peak_group_id()];
+	peak_pos.at(row,col)=cur_peak->ppm();
+      }
+    }
+    {
+      std::vector<UnverifiedPeak>::const_iterator cur_peak;
+      for(cur_peak = db.unverified_peaks().begin(); 
+	  cur_peak != db.unverified_peaks().end(); ++cur_peak){
+	unsigned col = sample_id_to_col[cur_peak->sample_id()];
+	unsigned row = pg_id_to_row[cur_peak->peak_group_id()];
+	peak_pos.at(row,col)=cur_peak->ppm();
+      }
+    }
+    
+    //Mean center the columns
+    mean_center_columns(peak_pos);
+    //SVD
+    GSL::Matrix u(1,1),v(1,1);
+    GSL::Vector sigma(1);
+    bool more_groups_than_samples = peak_pos.rows() > peak_pos.cols();
+    if(more_groups_than_samples){
+      u = peak_pos; 
+    }else{
+      u = peak_pos.transpose();
+    }
+    v = GSL::Matrix(u.cols(), u.cols());
+    sigma = GSL::Vector(u.cols());
+    gsl_linalg_SV_decomp_jacobi(u.ptr(), v.ptr(), sigma.ptr());    
+
+    //Copy the correct matrix to the sample-params object
+    //Sample params has one row for each sample and each column
+    //corresponds to a parameter.  The variance explained by the ith
+    //column is proportional to the square of the ith element of sigma
+    GSL::Matrix sample_params(1,1);
+    if(more_groups_than_samples){
+      sample_params = v;
+    }else{
+      sample_params = u;
+    }
+
+    //Convert sigmas to fractional variances && determine how many are needed
+    {
+      double sum = 0;
+      for(std::size_t i = 0; i < sigma.size(); ++i){
+	sigma[i] *= sigma[i];
+	sum += sigma[i];
+      }
+      if(sum > 0){
+	for(std::size_t i = 0; i < sigma.size(); ++i){
+	  sigma[i]/=sum;
+	}
+      }
+    }
+
+    std::size_t num_components = 0;
+    double sum_of_used_variances = 0.0;
+    while(sum_of_used_variances < frac_variance && 
+	  num_components < sigma.size()){
+      sum_of_used_variances += sigma[num_components];
+      ++num_components;
+    }
+    
+    //Convert fractional variances to ParamStats object (copying to a
+    //stl vector is necessary because you can't (always) pointer-index
+    //through a gsl vector
+    std::vector<double> vars(num_components); 
+    for(std::size_t i = 0; i < num_components; ++i){
+      vars.at(i) = sigma[i];
+    }
+    ParamStats param_stats(vars.begin(), vars.end());
+
+    //Convert rows of sample_params matrix to sample parameter objects
+    std::set<ParameterizedSample> samples;
+    for(std::size_t row=0; row < sample_params.rows(); ++row){
+      for(std::size_t col=0; col < num_components; ++col){
+	vars.at(col)=sample_params.at(row,col);
+      }
+      const UnparameterizedSample &orig=db.unparameterized_samples()[row];
+      samples.insert(ParameterizedSample(orig.id(), orig.sample_class(), 
+					 vars.begin(), vars.end()));
+    }
+    
+    //Pack results into a pair and return
+    return std::make_pair(samples, param_stats);
   }
   
   ///\brief set the parameters for all samples in the database to
@@ -148,7 +287,7 @@ namespace HoughPeakMatch{
   ///\param stats the param_stats object to add to the database
   void add_params_to_db
   (PeakMatchingDatabase& db, 
-   const std::set<FileFormatSampleParams>& params,
+   const std::set<ParameterizedSample>& params,
    const ParamStats stats){
     ///\todo stub  
   }
@@ -197,7 +336,7 @@ int main(int argc, char**argv){
     return !db.write(std::cout);  
   }
 
-  std::pair<std::set<FileFormatSampleParams>, ParamStats> params = 
+  std::pair<std::set<ParameterizedSample>, ParamStats> params = 
     calculate_sample_parameters(db, pg_in_all_samples, fraction_variance);
 
   add_params_to_db(db, params.first, params.second);
