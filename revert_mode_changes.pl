ec4cc5de4e8940e7e603604958751bdbaf18549f
Eric David Moyer <eric_moyer@yahoo.com>
2011-07-08 19:29:56 -0400

    Added comments documenting how to use the revert_mode_changes.pl script.

--- revert_mode_changes.pl
+++ revert_mode_changes.pl
@@ -1,6 +1,10 @@
 #!/usr/bin/perl
 ##############
 # Parse output of 'git show sha1blabla' to revert changes to executable mode
+#
+# usage: revert_mode_changes.pl git_show_output
+#
+# Must be run in the root directory of the repository
 ##############
 use strict;
 use warnings;
