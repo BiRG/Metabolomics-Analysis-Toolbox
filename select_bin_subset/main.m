06f458efcad27db9c11d6d37310cbe32984c75d9
Dan C. Wlodarski <dan.wlodarski@gmail.com>
2011-07-15 06:12:26 -0400

    - First beta version NB: Need to fix a bug when opening a second bin list or     loadings file.

--- select_bin_subset/main.m
+++ select_bin_subset/main.m
@@ -27,11 +27,11 @@ function varargout = select_bin_subset(varargin)
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
-                   'gui_Singleton',  gui_Singleton, ...
-                   'gui_OpeningFcn', @select_bin_subset_OpeningFcn, ...
-                   'gui_OutputFcn',  @select_bin_subset_OutputFcn, ...
-                   'gui_LayoutFcn',  [] , ...
-                   'gui_Callback',   []);
+    'gui_Singleton',  gui_Singleton, ...
+    'gui_OpeningFcn', @select_bin_subset_OpeningFcn, ...
+    'gui_OutputFcn',  @select_bin_subset_OutputFcn, ...
+    'gui_LayoutFcn',  [] , ...
+    'gui_Callback',   []);
 if nargin && ischar(varargin{1})
     gui_State.gui_Callback = str2func(varargin{1});
 end
@@ -66,7 +66,7 @@ set(handles.binlist_uitable, 'Data', {});
 
 
 % --- Outputs from this function are returned to the command line.
-function varargout = select_bin_subset_OutputFcn(hObject, eventdata, handles) 
+function varargout = select_bin_subset_OutputFcn(hObject, eventdata, handles)
 % varargout  cell array for returning output args (see VARARGOUT);
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
@@ -81,15 +81,60 @@ function load_binlist_button_Callback(hObject, eventdata, handles)
 % hObject    handle to load_binlist_button (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+overriddenEOR = ' ,;\t\n';
 [filename pathname] = uigetfile( ...
     {'*.txt','Comma and Semicolon Delimited Binlist (*.txt)'; ...
     '*.*','All Files (.*.*)'}, ...
-    'Select Binlist file...','bins.txt');
+    'Select bin list file...','bins.txt');
 if ( ischar(filename) )
-    fid = fopen(filename);
+    fullpath = [pathname filename];
+    fid = fopen(fullpath, 'r');
     if ( fid > 2 )
+        
+        binBounds = textscan(fid, '%f %f', 'Delimiter', overriddenEOR);
         fclose(fid);
+        handles.binListCount = length(binBounds{1});
+        guidata(hObject,handles);
+        
+        % -- DCW: Do the source bin bounds need to be sorted?
+        %binBounds = [ binBounds{1} binBounds{2} ];
+        %binBounds = flipud(sortrows(binBounds, 1));
+        
+        % Need the data to be stored in a cell of cells.
+        vectorOfOnes = ones(1, handles.binListCount);
+        %binBounds = mat2cell(binBounds, vectorOfOnes, [1 1]);
+        binBounds = [ ...
+            mat2cell(binBounds{1}, vectorOfOnes, 1) ...
+            mat2cell(binBounds{2}, vectorOfOnes, 1) ];
+        
+        if ( isfield(handles, 'binLoadingsCount') && ...
+                handles.binLoadingsCount ~= handles.binListCount )
+            msgbox(['Mismatched record counts!' 10 13 10 13 ...
+                'Bin list record count: ' ...
+                num2str(handles.binListCount) 10 13 ...
+                'Bin loadings record count: ' ...
+                num2str(handles.binLoadingsCount) ], ...
+                'Unmatched file record counts','error','modal');
+        else
+            dataOnTable = get(handles.binlist_uitable, 'Data');
+            logicalCellArray = mat2cell( ...
+                false(handles.binListCount, 1), ...
+                vectorOfOnes, 1);
+            nanCellArray = mat2cell( ...
+                NaN(handles.binListCount, 1), ...
+                vectorOfOnes, 1);
+            if ( isempty(dataOnTable) )
+                binBounds = [ logicalCellArray binBounds ...
+                    nanCellArray logicalCellArray ];
+            else
+                binBounds = [ dataOnTable(:, 1) binBounds ...
+                    dataOnTable(:, 4) dataOnTable(:, 5) ];
+            end;
+            set(handles.binlist_uitable, 'Data', binBounds);
+        end;
     else
+        msgbox(['Unable to open bin list file ' fullpath '!'], ...
+            'Could not open file', 'error', 'modal');
     end;
 end;
 
@@ -101,8 +146,59 @@ function load_loadings_button_Callback(hObject, eventdata, handles)
 % handles    structure with handles and user data (see GUIDATA)
 [filename pathname] = uigetfile( ...
     {'*.csv;*.txt','Tab/Comma Delimited file (*.csv, *.txt)'; ...
-    '*.*','All Files (.*.*)'}, ...
-    'Select Binlist CSV file...','bins.csv');
+    '*.*','All Files (*.*)'}, ...
+    'Select bin OPLS loadings CSV file...','binloadings.csv');
+if ( ischar(filename) )
+    fullpath = [pathname filename];
+    fid = fopen(fullpath, 'r');
+    if ( fid > 2 )
+        binLoadings = textscan(fid, '%f %f %f %d', ...
+            'Delimiter', ' ,\t', 'HeaderLines', 1);
+        fclose(fid);
+        handles.binLoadingsCount = length(binLoadings{1});
+        guidata(hObject,handles);
+        
+        % -- DCW: Do the source loadings need to be sorted?
+        %binLoadings = [ binLoadings{1} binLoadings{2} ...
+        %    binLoadings{3} binLoadings{4} ];
+        %binLoadings = flipud(sortrows(binLoadings, 1));
+        
+        % Need the data to be stored in a cell of cells.
+        vectorOfOnes = ones(1, handles.binLoadingsCount);
+        %binLoadings = mat2cell(binLoadings, vectorOfOnes, [1 1 1 1]);
+        binLoadings = [ ...
+            mat2cell(binLoadings{1}, vectorOfOnes, 1) ...
+            mat2cell(binLoadings{2}, vectorOfOnes, 1) ...
+            mat2cell(binLoadings{3}, vectorOfOnes, 1) ...
+            mat2cell(logical(binLoadings{4}), vectorOfOnes, 1) ];
+        
+        if ( isfield(handles, 'binListCount') && ...
+                handles.binListCount ~= handles.binLoadingsCount )
+            msgbox(['Mismatched record counts!' 10 13 10 13 ...
+                'Bin list record count: ' ...
+                num2str(handles.binListCount) 10 13 ...
+                'Bin loadings record count: ' ...
+                num2str(handles.binLoadingsCount) ], ...
+                'Unmatched file record counts','error','modal');
+        else
+            dataOnTable = get(handles.binlist_uitable, 'Data');
+            nanCellArray = mat2cell( ...
+                NaN(handles.binLoadingsCount, 1), ...
+                vectorOfOnes, 1);
+            if ( isempty(dataOnTable) )
+                binLoadings = [ binLoadings(:,4) nanCellArray ...
+                    nanCellArray binLoadings(:,3) binLoadings(:,4) ];
+            else
+                binLoadings = [ binLoadings(:,4) dataOnTable(:,2:3) ...
+                    binLoadings(:,2) binLoadings(:,4) ];
+            end;
+            set(handles.binlist_uitable, 'Data', binLoadings);
+        end;
+    else
+        msgbox(['Unable to open loadings file ' fullpath '!'], ...
+            'Could not open file', 'error', 'modal');
+    end;
+end;
 
 
 % --- Executes on button press in save_binlist_subset_button.
@@ -110,4 +206,29 @@ function save_binlist_subset_button_Callback(hObject, eventdata, handles)
 % hObject    handle to save_binlist_subset_button (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-msgbox('Save Subset as Binlist button clicked');
+if ( isfield(handles, 'binListCount') )
+    [filename pathname] = uiputfile(...
+        {'*.txt','Comma and Semicolon Delimited Binlist (*.txt)'; ...
+        '*.*','All Files (*.*)'}, ...
+        'Save subset bin list file...','selected_bins.txt');
+    if ( ischar(filename) )
+        fullpath = [pathname filename];
+        fid = fopen(fullpath, 'w');
+        if ( fid > 2 )
+            source = get(handles.binlist_uitable, 'Data');
+            for i = 1:handles.binListCount
+                if ( source{i,1} )
+                    % The "include?" box was selected for this row.
+                    fprintf(fid, '%f,%f;', source{i,2}, source{i,3});
+                end;
+            end;
+            fclose(fid);
+        else
+            msgbox(['Unable to open target file ' fullpath '!'], ...
+                'Could not open file', 'error', 'modal');
+        end;
+    end;
+else
+    msgbox('Please load a bin list file first.', ...
+        'No bin list data to save', 'error', 'modal');
+end;
