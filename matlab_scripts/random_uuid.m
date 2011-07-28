b1bc2da5d5e4288424e2a57176aa87697687acd4
Eric David Moyer <eric_moyer@yahoo.com>
2011-07-28 10:13:40 -0400

    Adjusted the documentation on random_uuid to fit within the quick help window in Matlab

--- matlab_scripts/random_uuid.m
+++ matlab_scripts/random_uuid.m
@@ -1,17 +1,19 @@
 function [ uuid_string ] = random_uuid
-%RANDOM_UUID Return a random uuid generated from random.org or the system
-%random number generator
-%   Returns a random uuid
-%   (http://en.wikipedia.org/wiki/Universally_unique_identifier).  This
-%   will be in version 4 format.  That means that it consists of 36
-%   characters in the form xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx where x is
-%   any hexadecimal digit and y is either 8, 9, A, or B.  This is
-%   equivalent to setting a version identifier nibble and two other
-%   reserved bits.
+%RANDOM_UUID Return a random uuid generated from random.org 
+%or the system random number generator
 %
-%   First attempts to get the bits needed from random.org.  Failing that,
-%   gets them from the random number generator using rand.
-
+% Returns a random uuid
+% ( http://en.wikipedia.org/wiki/Universally_unique_identifier ).
+% This will be in version 4 format.  That means that it  
+% consists of 36 characters in the form:
+%           xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx 
+% where x is any hexadecimal digit and y is either 8, 9, A, 
+% or B.  This is equivalent to setting a version identifier
+% nibble and two other reserved bits.
+%
+% First attempts to get the bits needed from random.org.  
+% Failing that, gets them from the random number generator 
+% using rand.
 rand_nums = [];
 
 % Try to get the bits from random.org
