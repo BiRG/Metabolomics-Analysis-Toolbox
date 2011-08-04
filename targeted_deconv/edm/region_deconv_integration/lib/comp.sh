0e5677bafdd5680ffd8f842142ba2bdeb13cded1
Eric David Moyer <eric_moyer@yahoo.com>
2011-08-04 10:44:12 -0400

    Improved the comparison script that will allow me to do the library integration.  It is now documented and it outputs an easily intelligible list of files.

--- targeted_deconv/edm/region_deconv_integration/lib/comp.sh
+++ targeted_deconv/edm/region_deconv_integration/lib/comp.sh
@@ -1,11 +1,23 @@
+#This script compares the matlab script files in this directory with
+#the global matlab library directories.  It prints, for each file,
+#whether it is a new file, the same as one in the global directories,
+#or it is different from a file of the same name in the global directories
 
-dirA="../../omics_dev/matlab_scripts/"
-dirB="../../omics_dev/lib/"
+dirA="../../../../matlab_scripts/"
+dirB="../../../../lib/"
 for i in *.m; do 
     if [ -e "$dirA$i" ]; then
-	diff -q "$i" "$dirA$i"
+	if diff -q "$i" "$dirA$i" > /dev/null; then
+	    echo "$i same"
+	else
+	    echo "$i changed"
+	fi
     elif [ -e "$dirB$i" ]; then
-	diff -q "$i" "$dirB$i"
+	if diff -q "$i" "$dirB$i" > /dev/null; then
+	    echo "$i same"
+	else
+	    echo "$i changed"
+	fi
     else
 	echo "$i is a new file"
     fi
