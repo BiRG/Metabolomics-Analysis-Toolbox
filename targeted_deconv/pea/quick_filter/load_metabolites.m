9f163b6c723b412acdd528fb42f1440efb0d6df7
Eric David Moyer <eric_moyer@yahoo.com>
2011-09-30 19:45:58 -0400

    Fixed reference to loadbinmap in load_metabolites
    Issue #66

--- targeted_deconv/pea/quick_filter/load_metabolites.m
+++ targeted_deconv/pea/quick_filter/load_metabolites.m
@@ -1,19 +1,19 @@
 function metabolites = load_metabolites(pathname,filename)
-bm = load_binmap(fullfile(pathname, filename));
+mm = load_metabmap(fullfile(pathname, filename));
 
-metabolites = cell(1,length(bm));
+metabolites = cell(1,length(mm));
 
-for i = 1:length(bm)
-    metabolites{i}.id = bm(i).id;
-    metabolites{i}.metabolite = bm(i).compound_descr;
-    metabolites{i}.left = bm(i).bin.left;
-    metabolites{i}.right = bm(i).bin.right;
-    metabolites{i}.multiplicity = bm(i).multiplicity;
-    if bm(i).is_clean
+for i = 1:length(mm)
+    metabolites{i}.id = mm(i).id;
+    metabolites{i}.metabolite = mm(i).compound_descr;
+    metabolites{i}.left = mm(i).bin.left;
+    metabolites{i}.right = mm(i).bin.right;
+    metabolites{i}.multiplicity = mm(i).multiplicity;
+    if mm(i).is_clean
         metabolites{i}.deconvolution = 'Clean';
     else
         metabolites{i}.deconvolution = 'Overlap';
     end
-    metabolites{i}.proton_id = bm(i).proton_id;
-    metabolites{i}.id_source = bm(i).id_source;
+    metabolites{i}.proton_id = mm(i).proton_id;
+    metabolites{i}.id_source = mm(i).id_source;
 end
