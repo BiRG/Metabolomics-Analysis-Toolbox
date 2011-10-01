95cf72b177b055194c4af2c63f9b2bf93446e12c
Eric David Moyer <eric_moyer@yahoo.com>
2011-10-01 18:31:44 -0400

    Created failing test case for reading in all CompoundBin entries from the file.
    Issue #68

--- common_scripts/tests/testCompoundBin.m
+++ common_scripts/tests/testCompoundBin.m
@@ -5,10 +5,17 @@ function test_suite = testCompoundBin %#ok<STOUT>
 %   runtest testCompoundBin
 initTestSuite;
 
-function testFirstInput %#ok<DEFNU>
-%Stuff
-fprintf('test');
+function dir=data_dir
+% Return the directory in which this test case data is located
+thisfile = mfilename('fullpath');
+dir=regexprep(thisfile,'/[^/]*$','');
 
+function testReadAll %#ok<DEFNU>
+% Tests whether load_metabmap (which uses CompoundBin) reads the correct
+% number of entries from a test file
+
+map = load_metabmap(fullfile(data_dir, 'testCompoundBin.01.readall.csv'));
+assertEqual(length(map),78);
     
 
 
