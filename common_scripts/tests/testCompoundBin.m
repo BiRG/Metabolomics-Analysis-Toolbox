7c6826a4233dc020d47dc59a46794eace29552b4
Eric David Moyer <eric_moyer@yahoo.com>
2011-09-30 23:19:26 -0400

    Added stub test case for CompoundBin
    Issue #68

---
+++ common_scripts/tests/testCompoundBin.m
@@ -0,0 +1,14 @@
+function test_suite = testCompoundBin %#ok<STOUT>
+%matlab_xUnit tests excercising CompoundBin
+%
+% Usage:
+%   runtest testCompoundBin
+initTestSuite;
+
+function testFirstInput %#ok<DEFNU>
+%Stuff
+fprintf('test');
+
+    
+
+
