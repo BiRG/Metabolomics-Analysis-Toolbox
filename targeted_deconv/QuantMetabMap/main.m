ecb40207937ff669604792828aa2b28d6a0f2834
Eric David Moyer <eric_moyer@yahoo.com>
2011-10-03 17:48:41 -0400

    Renamed Eric's init function to main.m
    Issue #69

--- targeted_deconv/QuantMetabMap/main.m
+++ targeted_deconv/QuantMetabMap/main.m
@@ -1,35 +1,35 @@
-function varargout = targeted_deconv_start(varargin)
-% TARGETED_DECONV_START MATLAB code for targeted_deconv_start.fig
-%      TARGETED_DECONV_START, by itself, creates a new TARGETED_DECONV_START or raises the existing
+function varargout = main(varargin)
+% MAIN MATLAB code for main.fig
+%      MAIN, by itself, creates a new MAIN or raises the existing
 %      singleton*.
 %
-%      H = TARGETED_DECONV_START returns the handle to a new TARGETED_DECONV_START or the handle to
+%      H = MAIN returns the handle to a new MAIN or the handle to
 %      the existing singleton*.
 %
-%      TARGETED_DECONV_START('CALLBACK',hObject,~,handles,...) calls the local
-%      function named CALLBACK in TARGETED_DECONV_START.M with the given input arguments.
+%      MAIN('CALLBACK',hObject,~,handles,...) calls the local
+%      function named CALLBACK in MAIN.M with the given input arguments.
 %
-%      TARGETED_DECONV_START('Property','Value',...) creates a new TARGETED_DECONV_START or raises the
+%      MAIN('Property','Value',...) creates a new MAIN or raises the
 %      existing singleton*.  Starting from the left, property value pairs are
-%      applied to the GUI before targeted_deconv_start_OpeningFcn gets called.  An
+%      applied to the GUI before main_OpeningFcn gets called.  An
 %      unrecognized property name or invalid value makes property application
-%      stop.  All inputs are passed to targeted_deconv_start_OpeningFcn via varargin.
+%      stop.  All inputs are passed to main_OpeningFcn via varargin.
 %
 %      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
 %      instance to run (singleton)".
 %
 % See also: GUIDE, GUIDATA, GUIHANDLES
 
-% Edit the above text to modify the response to help targeted_deconv_start
+% Edit the above text to modify the response to help main
 
-% Last Modified by GUIDE v2.5 01-Aug-2011 17:01:12
+% Last Modified by GUIDE v2.5 03-Oct-2011 17:47:38
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
                    'gui_Singleton',  gui_Singleton, ...
-                   'gui_OpeningFcn', @targeted_deconv_start_OpeningFcn, ...
-                   'gui_OutputFcn',  @targeted_deconv_start_OutputFcn, ...
+                   'gui_OpeningFcn', @main_OpeningFcn, ...
+                   'gui_OutputFcn',  @main_OutputFcn, ...
                    'gui_LayoutFcn',  [] , ...
                    'gui_Callback',   []);
 if nargin && ischar(varargin{1})
@@ -44,15 +44,15 @@ end
 % End initialization code - DO NOT EDIT
 
 
-% --- Executes just before targeted_deconv_start is made visible.
-function targeted_deconv_start_OpeningFcn(hObject, unused, handles, varargin) %#ok<INUSL>
+% --- Executes just before main is made visible.
+function main_OpeningFcn(hObject, unused, handles, varargin) %#ok<INUSL>
 % This function has no output args, see OutputFcn.
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-% varargin   command line arguments to targeted_deconv_start (see VARARGIN)
+% varargin   command line arguments to main (see VARARGIN)
 
-% Choose default command line output for targeted_deconv_start
+% Choose default command line output for main
 handles.output = hObject;
 
 % Set the uninitialized text box values (I do it here rather than in GUIDE
@@ -107,12 +107,12 @@ end
 % Update handles structure
 guidata(hObject, handles);
 
-% UIWAIT makes targeted_deconv_start wait for user response (see UIRESUME)
+% UIWAIT makes main wait for user response (see UIRESUME)
 % uiwait(handles.figure1);hObject
 
 
 % --- Outputs from this function are returned to the command line.
-function varargout = targeted_deconv_start_OutputFcn(unused, unused1, handles)  %#ok<INUSL>
+function varargout = main_OutputFcn(unused, unused1, handles)  %#ok<INUSL>
 % varargout  cell array for returning output args (see VARARGOUT);
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
