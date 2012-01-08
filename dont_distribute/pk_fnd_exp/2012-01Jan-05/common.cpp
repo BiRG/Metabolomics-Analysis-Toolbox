204fe21c43719f48f04a568cf4d81a22f9ac9033
Eric David Moyer <eric_moyer@yahoo.com>
2012-01-07 21:08:44 -0500

    Added code to generate amplitudes and peak locations in preparation to viewing my prior spectrum distribution.
    Added serialization to peak and peak-list
    
    Moved expected_exception to bottom of the file, where it is less obvious
    
    Added code for a simple discretization (which is all that this version
    of the code will need)

--- dont_distribute/pk_fnd_exp/2012-01Jan-05/common.cpp
+++ dont_distribute/pk_fnd_exp/2012-01Jan-05/common.cpp
@@ -22,3 +22,35 @@ PeakList peaksFromPrior(GClasses::GRandMersenneTwister& rng){
   }
   return ret;
 }
+
+std::vector<AmpAndIsPeak> ampsAndLocsFrom(const PeakList& l){
+  const double width = Prior::freq_int_max - Prior::freq_int_min;
+  const double min = Prior::freq_int_min;
+  const double max = Prior::freq_int_max;
+  const unsigned n = Prior::freq_int_num_samp;
+  std::vector<AmpAndIsPeak> ret(n);
+  for(unsigned i = 0; i < ret.size(); ++i){
+    double x = i*(width/(n-1))+min;
+    ret[i].amp = l.a(x);
+    ret[i].is_peak = false;
+  }
+  std::vector<Peak>::const_iterator pk;
+  for(pk = l.begin(); pk != l.end(); ++pk){
+    double x = pk->x0();
+    if(x <= min){ 
+      ret.at(0).is_peak = true;
+    }else if(x >= max){ 
+      ret.at(ret.size()-1).is_peak = true;
+    }else{
+      double bin = n*(x-min)/width;
+      unsigned low = bin;
+      unsigned high = low+1;
+      if((bin - low) < (high - bin)){
+	ret.at(low).is_peak = true;
+      }else{
+	ret.at(high).is_peak = true;
+      }
+    }
+  }
+  return ret;
+}
--- dont_distribute/pk_fnd_exp/2012-01Jan-05/common.hpp
+++ dont_distribute/pk_fnd_exp/2012-01Jan-05/common.hpp
@@ -7,9 +7,17 @@
 #include <vector>
 #include <cmath>
 
+//Allow boost serialization
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+
+#include <GClasses/GError.h>
+
 #ifndef BAYES_COMMON_HPP
 #define BAYES_COMMON_HPP
 
+
+///\brief Parameters of the prior distribution
 namespace Prior{
   ///\brief The minimum of the frequency interval
   const double freq_int_min=0;
@@ -39,17 +47,6 @@ namespace Prior{
 
 
 
-///Thrown when there is an expected speedy exit
-class expected_exception: public std::exception{
-public:
-  ///The exit status of the application.
-  int exit_status;
-
-  ///Create an expected exception that should cause the given
-  ///exit_status to be returned from the application
-  expected_exception(int exit_status):exit_status(exit_status){}
-};
-
 ///\brief A Gauss-Lorentzian Peak
 ///
 ///A linear combination between a Gaussian and a Lorentzian of equal
@@ -69,6 +66,21 @@ class Peak{
   ///
   ///Must be in the interval [0, 1], which includes both 0 and 1.
   double lorentzianness;
+
+  friend class boost::serialization::access;
+
+  ///\brief Serialization method - uses boost-serialize
+  ///
+  ///\param ar The archive to serialize to
+  ///
+  ///\param version the version number
+  template<class Archive>
+  void serialize(Archive& ar, const unsigned int version){
+    ar & amp;
+    ar & gamma;
+    ar & loc;
+    ar & lorentzianness;
+  }
 
 public:
   ///\brief Construct a peak with \a loc, \a amp, \a gamma, and \a
@@ -114,6 +126,19 @@ public:
 ///vector member.  The coupling is too tight with an isa relationship,
 ///but coding time for the prototype outweights it.
 class PeakList:public std::vector<Peak>{
+
+  friend class boost::serialization::access;
+
+  ///\brief Serialization method - uses boost-serialize
+  ///
+  ///\param ar The archive to serialize to
+  ///
+  ///\param version the version number
+  template<class Archive>
+  void serialize(Archive& ar, const unsigned int version){
+    ar && boost::serialization::base_object<std::vector<Peak> >(*this);
+  }
+
 public:
   ///\brief Create an empty PeakList object
   PeakList(){}
@@ -134,6 +159,99 @@ public:
   }
 };
 
+
+///\brief The amplitude for a given spectrum frequency and whether
+///that frequency was a peak
+struct AmpAndIsPeak{
+  ///\brief The amplitude at the frequency
+  double amp;
+  
+  ///\brief True iff the frequency is a peak
+  bool is_peak;
+};
+
+///\brief Create the samples that would be derived from measuring the
+///peaks in \a l together
+///
+///The number of samples specified in Prior::freq_int_num_samp is used
+///and the interval is that specified in Prior::freq_int_min and
+///Prior::freq_int_max
+std::vector<AmpAndIsPeak> ampsAndLocsFrom(PeakList& l);
+
+///\brief Discretizes an interval into n equal sized bins
+///
+///If a value lies outside of the explicitly discretized interval it
+///is put into the appropriate end bin. (That is, if it is less than
+///the minimum of the interval, it goes into the first bin and if it
+///is greater, it goes into the last bin.)
+class UniformDiscretization{
+  ///\brief The minimum value of the explicitly discretized interval
+  double min;
+  ///\brief The maximum value of the explicitly discretized interval
+  double max;
+  ///\brief The number of bins to form
+  unsigned m_num_bins;
+
+
+  friend class boost::serialization::access;
+
+  ///\brief Serialization method - uses boost-serialize
+  ///
+  ///\param ar The archive to serialize to
+  ///
+  ///\param version the version number
+  template<class Archive>
+  void serialize(Archive& ar, const unsigned int version){
+    ar && min;
+    ar && max;
+    ar && m_num_bins;
+  }
+
+public:
+  ///\brief Create a uniform discretization for the closed interval
+  ///[\a min,\a max]
+  ///
+  ///Discretizes the interval [\a min, \a max] which includes both \a min and
+  ///\a max into \a num_bins bins
+  ///
+  ///\param min The minimum value of the explicitly discretized
+  ///           interval (must be less than max)
+  ///
+  ///\param max The maximum value of the explicitly discretized
+  ///           interval (must be greater than min)
+  ///
+  ///\param num_bins The number of bins to form (must be at least 1)
+  UniformDiscretization(double min, double max, unsigned num_bins):
+    min(min), max(max), m_num_bins(num_bins){ 
+    using GClasses::to_str; using GClasses::ThrowError;
+    if(min >= max){ 
+      std::string s; 
+      ThrowError(s+"min >= max in UniformDiscretization (min="+to_str(min)+
+		 " max="+to_str(max));
+    }
+    if(num_bins < 1){ 
+      ThrowError("UniformDiscretization constructor called with 0 bins.  "
+		 "It must have at least one bin");
+    }
+  }
+
+  ///\brief Return the number of bins in this discretization
+  ///\return the number of bins in this discretization
+  unsigned num_bins(){ return m_num_bins; }
+
+  ///\brief Return the index of the bin containing the given number
+  ///
+  ///Bin indices are zero-based
+  ///
+  ///\return the index of the bin containing the given number
+  unsigned bin_for(double num){
+    if      (num <= min){  return 0;
+    }else if(num >= max){  return num_bins()-1; 
+    }else{                 return num_bins()*(num-min)/(max-min);    }
+  }
+  
+};
+
 namespace GClasses{  class GRandMersenneTwister; }
 
 ///\brief Returns a peak-list sampled from the prior distribution for
@@ -143,5 +261,17 @@ namespace GClasses{  class GRandMersenneTwister; }
 ///
 ///\param rng a random number generator to be used to generate the samples
 PeakList peaksFromPrior(GClasses::GRandMersenneTwister& rng);
+
+
+///Thrown when there is an expected speedy exit
+class expected_exception: public std::exception{
+public:
+  ///The exit status of the application.
+  int exit_status;
+
+  ///Create an expected exception that should cause the given
+  ///exit_status to be returned from the application
+  expected_exception(int exit_status):exit_status(exit_status){}
+};
 
 #endif //BAYES_COMMON_HPP
