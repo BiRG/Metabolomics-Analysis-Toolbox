ceeaa44fd80934a92900a8990dbf612e33cb8f0c
Eric David Moyer <eric_moyer@yahoo.com>
2012-01-12 10:49:57 -0500

    Added stub code for parsing command-line arguments and reading in the discretization.

--- dont_distribute/pk_fnd_exp/2012-01Jan-05/create_table.cpp
+++ dont_distribute/pk_fnd_exp/2012-01Jan-05/create_table.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include <fstream>
+#include <memory>
 #include <utility>
 #include <GClasses/GApp.h>
 #include <GClasses/GError.h>
 #include <GClasses/GMatrix.h>
+
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/serialization/vector.hpp>
+
 #include "common.hpp"
 
 using namespace GClasses;
@@ -42,14 +49,63 @@ void printUsageAndExit(std::ostream& out, const char*executableName, std::string
   throw expected_exception(-1);
 }
 
+///\brief create_table (see usage message in printUsageAndExit)
+void create_table(GArgReader& args){
+  std::string s("");//Empty string to make easy to do string formatting
+  const char* exe = args.pop_string();
+  if(args.size() !=  2){
+    printUsageAndExit(cerr, exe, "Error: Wrong number of arguments.  Expected "
+		      "2 arguments.");
+  }
+
+  const char* disc_file = args.pop_string();
+  const char* table_file= args.pop_string();
+
+  std::vector<UniformDiscretization> discretizations;
+
+  {
+    std::ifstream disc_stream(disc_file);
+    if(!disc_stream){
+      printUsageAndExit(cerr, exe, s+"Error: could not open discretization "
+			"file \""+disc_file+"\" for reading");
+    }
+    boost::archive::text_iarchive in(disc_stream);
+    in & discretizations;
+  }
+
+  //TODO: declare table variable here
+
+  {
+    std::ifstream table_stream(table_file);
+    if(!table_stream){
+      //TODO: stub      
+    }
+  }
+
+  //TODO: stub
+  //Check whether the table and the discretization are compatible
+  //Check that the table is what we are expecting to write (right
+  //field names, number of variables etc)
+
+  while(true){
+    //TODO: stub
+    //If the time limit has expired, write the current counts to the table
+    //Generate another sample
+    //Add the sample to the table
+  }
+  
+
+}
 
 
+///\brief start the create_table routine and handle uncaught exceptions
 int main(int argc, char *argv[]){
   GApp::enableFloatingPointExceptions();
   
   int nRet = 0;
   try {
     GArgReader args(argc, argv);
+    create_table(args);
   } catch(const GException& e){
     cerr << "Error: " << e.what() << std::endl;
   } catch(const expected_exception& e){
