14082061497bb9342c86b5d67d3e418773a54f30
Eric David Moyer <eric_moyer@yahoo.com>
2012-01-16 13:37:32 -0500

    Added cruft of my own to the publicize_toolbox.sh script so that it is easier to scan the output by eye

--- dont_distribute/publicize/publicize_toolbox.sh
+++ dont_distribute/publicize/publicize_toolbox.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 #Update the private repo
+echo "******************"
+echo "* Updating the private repostiory"
+echo "******************"
+echo ""
+
 cd ~/private/toolbox
 git pull
 
 #Mirror the private to the public using unison
+echo "******************"
+echo "* Mirroring the private to the public using unison"
+echo "******************"
+echo ""
+
 cd ~
 unison public_files_toolbox
 
 #Checkin the changes to the public repo as a new commit with some
 #reasonable commit message - right now the commit message is typed by the user
+echo "******************"
+echo "* Committing the changes to the public repository "
+echo "******************"
+echo ""
+
 cd ~/public/toolbox
 git add .  #Add untracked files (which will be new)
 git add -u #Process deletions and renames
