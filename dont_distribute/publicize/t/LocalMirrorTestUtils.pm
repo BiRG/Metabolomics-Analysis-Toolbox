82f630e84cc25371a78beb81ad2750ad44440388
Eric David Moyer <eric_moyer@yahoo.com>
2012-02-02 09:49:25 -0500

    Added the ability to include file contents in the hash returned from dir_as_hash

--- dont_distribute/publicize/t/LocalMirrorTestUtils.pm
+++ dont_distribute/publicize/t/LocalMirrorTestUtils.pm
@@ -14,25 +14,56 @@ use File::Find ();
 
 #####
 # Given a directory name
+#
 # Return the structure of a directory as a hash.  Filenames are keys
 # with empty string values.  Directory names are keys with hash values.
 #
 # If the name passed is not a directory, returns undef
+#
+# Can be called as:
+#
+# dir_as_hash('dirname');
+#
+# or
+#
+# dir_as_hash('dirname',with_contents=>1);
+#
+# when the option with_contents is defined, the the filenames become
+# keys with the string values being the contents of the file.  If a
+# filename points to a non-file object, the contents of that object
+# are not read, and the value associated with that key is left as a
+# blank.  If a file cannot be read, its contents will be undef.
 #####
 
 sub dir_as_hash{
-    my ($dname)=@_;
+    my $dname=shift;
+    my @options_array = @_;
+    my %options = @_; #Rest of arguments are treated as an options hash
     return undef unless -d $dname;
     
     my $this_dir = {};
     my $add_entry = sub {
 	if(-d){
 	    return if (/^\.\.?$/); #Skip . and ..
-	    $$this_dir{$_}=dir_as_hash($_);
+	    if( keys %options == 0){
+		$$this_dir{$_}=dir_as_hash($_);
+	    }else{
+		$$this_dir{$_}=dir_as_hash($_,@options_array);
+	    }
 	    $File::Find::prune = 1;
 	    return;
 	}elsif(-f){
-	    $$this_dir{$_}=''; return;
+	    if ( exists $options{'with_contents'} ){
+		local( $/ ) ;
+		if(open( my $fh, $_ )){
+		    $$this_dir{$_} = <$fh>;
+		}else{
+		    $$this_dir{$_}= undef;
+		}
+	    }else{
+		$$this_dir{$_}=''; 
+	    }
+	    return;
 	}else{ 
             #Treat non-file/non-directory objects the same as file
             #objects - just list them
