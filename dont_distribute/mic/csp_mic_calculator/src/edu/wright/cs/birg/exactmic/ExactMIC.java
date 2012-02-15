cb9e4ac18b5ee8dace14905505dac019b50b3ada
Eric David Moyer <eric_moyer@yahoo.com>
2012-02-15 13:06:58 -0500

    Deleted unnecessary import of LinkedList

--- dont_distribute/mic/csp_mic_calculator/src/edu/wright/cs/birg/exactmic/ExactMIC.java
+++ dont_distribute/mic/csp_mic_calculator/src/edu/wright/cs/birg/exactmic/ExactMIC.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 import au.com.bytecode.opencsv.CSVReader;
