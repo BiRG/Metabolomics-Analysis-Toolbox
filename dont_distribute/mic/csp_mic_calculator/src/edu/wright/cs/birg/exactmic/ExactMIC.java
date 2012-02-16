f484133b168fd7d029cdf0f13fd7248c24c6c2b3
Eric David Moyer <eric_moyer@yahoo.com>
2012-02-16 18:05:09 -0500

    Completed the mic calculator.  It compiles and runs without crashing on simple input.  HOWEVER, it always seems to return 0.  Not a good sign.

--- dont_distribute/mic/csp_mic_calculator/src/edu/wright/cs/birg/exactmic/ExactMIC.java
+++ dont_distribute/mic/csp_mic_calculator/src/edu/wright/cs/birg/exactmic/ExactMIC.java
@@ -12,8 +12,12 @@ import java.util.Comparator;
 import java.util.List;
 
 import static choco.Choco.*;
+import choco.Options;
 import choco.cp.model.CPModel;
+import choco.cp.solver.CPSolver;
+import choco.kernel.model.variables.integer.IntegerConstantVariable;
 import choco.kernel.model.variables.integer.IntegerVariable;
+import choco.kernel.solver.constraints.integer.extension.BinRelation;
 
 import au.com.bytecode.opencsv.CSVReader;
 import au.com.bytecode.opencsv.CSVWriter;
@@ -106,7 +110,7 @@ public class ExactMIC {
 		for(int i=0; i < vars.length; ++i){
 			double[] col = new double[data.size()];
 			for(int j=0; j < data.size(); ++j){
-				col[j]=data.get(i)[j];
+				col[j]=data.get(j)[i];
 			}
 			vars[i]=new SampleVariable(header[i],col);
 		}
@@ -133,6 +137,12 @@ public class ExactMIC {
 				out.writeNext(line);
 			}
 		}
+		try {
+			out.close();
+		} catch (IOException e) {
+			System.err.println("Error closing output file.");
+			e.printStackTrace();
+		}
 	}
 	/**	The two-dimensional projection of a sample point taken from the input data.
 	 *   
@@ -156,15 +166,15 @@ public class ExactMIC {
 		Sample(int id, double x, double y){ this.id = id; this.x = x; this.y = y; }
 	}
 	
-	private static double calcMIC(SampleVariable x,
-			SampleVariable y) {
-		assert(x.getValues().length == y.getValues().length);
+	private static double calcMIC(SampleVariable xIn,
+			SampleVariable yIn) {
+		assert(xIn.getValues().length == yIn.getValues().length);
 		//N is the number of samples
-		final int N = x.getValues().length;
+		final int N = xIn.getValues().length;
 		
 		//F is the number by which logarithms will be multiplied before rounding 
 		//to give a fixed-point approximation which can be easily used in the CSP solver 
-		final int F = 32768;
+		final int F = 32768; 
 
 		if(N*Math.log(N) >= 65536){
 			System.err.println("Warning: N log N is large enough that multiplying it by "+ 
@@ -175,7 +185,7 @@ public class ExactMIC {
 		
 		Sample[] samples = new Sample[N];
 		for(int i = 0; i < N; ++i){
-			samples[i]=new Sample(i, x.getValues()[i], y.getValues()[i]);
+			samples[i]=new Sample(i, xIn.getValues()[i], yIn.getValues()[i]);
 		}
 		
 		// Create the model (so I don't forget to add variables at the end, I'll add them as I create them)
@@ -200,7 +210,7 @@ public class ExactMIC {
 		m.addVariable(numYBins);
 		
 		// Constrain number of bins on x and y axis, when multiplied == numBins
-		m.addConstraint(eq(numBins, mult(numXBins, numYBins)));
+		m.addConstraint(times(numXBins, numYBins, numBins));
 		
 		// xBin[i] holds the x bin for sample i
 		IntegerVariable[] xBin = new IntegerVariable[N];
@@ -212,7 +222,7 @@ public class ExactMIC {
 			public int compare(Sample l, Sample r){return (int)(r.x-l.x);}
 		});
 		m.addConstraint(eq(1,xBin[samples[0].id])); //First sample in bin 1
-		m.addConstraint(eq(numXBins,xBin[samples[N].id]));//Last sample in last bin
+		m.addConstraint(eq(numXBins,xBin[samples[N-1].id]));//Last sample in last bin
 		for(int i = 1; i < N; ++i){
 			IntegerVariable cur = xBin[samples[i].id];
 			IntegerVariable prev = xBin[samples[i-1].id];
@@ -245,7 +255,7 @@ public class ExactMIC {
 			public int compare(Sample l, Sample r){return (int)(r.y-l.y);}
 		});
 		m.addConstraint(eq(1,yBin[samples[0].id])); //First sample in bin 1
-		m.addConstraint(eq(numYBins,yBin[samples[N].id]));//Last sample in last bin
+		m.addConstraint(eq(numYBins,yBin[samples[N-1].id]));//Last sample in last bin
 		for(int i = 1; i < N; ++i){
 			IntegerVariable cur = yBin[samples[i].id];
 			IntegerVariable prev = yBin[samples[i-1].id];
@@ -292,30 +302,218 @@ public class ExactMIC {
 			m.addConstraint(occurrence(inYBin[i],yBin,i));
 		}
 		
+		//Create boolean variables that are 1 if a given sample is in a given bin.
+		//
+		//These will be summed to count the number of samples in a given x-y bin. Note that they are non-decision 
+		//variables because their state is completely determined by other decision variables
+		//
+		//sampleHasXY[sample][x][y] is 1 if sample is in x bin x and y bin y.  
+		//Nothing is ever in bin 0, so those are placeholders
+		IntegerVariable[][][] sampleHasXY = new IntegerVariable[N][b/2+1][b/2+1];
+		for(int samp=0; samp < N; ++samp){
+			//Set placeholders to null
+			for(int i=0; i <= b/2; ++i){ sampleHasXY[samp][i][0] = sampleHasXY[samp][0][i] = null; }
+			for(int x=1; x <= b/2; ++x){
+				for(int y=1; y <= b/2; ++y){
+					sampleHasXY[samp][x][y]=makeIntVar("sampleHasXY["+samp+"]["+x+"]["+y+"]", 0, 1, Options.V_NO_DECISION);
+					m.addVariable(sampleHasXY[samp][x][y]);
+					//The next constraint should be 
+					//m.addConstraint(reifiedConstraint(sampleHasXY[samp][x][y],));
+					//
+					//But this appears to expose a bug in the solver, so I've rewritten it as
+					m.addConstraint(ifThenElse(and(eq(xBin[samp],x),eq(yBin[samp],y)),
+							eq(sampleHasXY[samp][x][y],1),
+							eq(sampleHasXY[samp][x][y],0)));
+				}
+			}
+			//TODO: add redundant constraints
+			//We may want to add some redundant constraints here: constraints indicating that for each sample value, 
+			//exactly one of the [x][y] pairs is true, constraints duplicating 
+			//the constraints on cur bin = prev or prev+1.  These can potentially speed up constraint propagation.
+			//Maybe even the original variables of which bin should be ignored and only these boolean variables should 
+			//be in the model. 
+		}
+
 		// inXYBin[i][j] is the number of samples whose x value is assigned to the i-th x bin and whose y value is also
 		// assigned to the j-th y bin.  Since there is no bin 0, inXYBin[0][j]=inXYBin[i][0]=null
 		IntegerVariable[][] inXYBin = new IntegerVariable[b/2+1][b/2+1];
 		//Assign placeholder nulls
 		for(int i = 0; i <= b/2; ++i){ inXYBin[i][0]=null; inXYBin[0][i]=null; }
+		
+		
 		//Create the variables
-		for(int i = 0; i <= b/2; ++i){
-			for(int j = 0; j <= b/2; ++j){
-				inXYBin[i][j]=makeIntVar("inXYBin["+i+"]["+j+"]", 0, N);
-				m.addVariable(inXYBin[i][j]);
+		int[] allOnes = new int[N];	for(int i = 0; i < N; ++i){ allOnes[i] = 1; };
+		for(int x = 1; x <= b/2; ++x){
+			for(int y = 1; y <= b/2; ++y){
+				inXYBin[x][y]=makeIntVar("inXYBin["+x+"]["+y+"]", 0, N);
+				m.addVariable(inXYBin[x][y]);
 				
-				
-				
-				
-				/// TODO:
-				/// Add variables XYBin[i] which have (b/2)*xBin[i]+yBin[i] (thus they index into the XY array in some sense)
-				/// 
-				/// Then, make an occurrence constraint for each inXYBin that checks the appropriate value
-				///
-				
-				
+				//Set inXYBin[x][y] to the sum of the booleans for all samples that that sample is in bin x,y
+				IntegerVariable[] hasXYBools = new IntegerVariable[N];
+				for(int samp = 0; samp < N; ++samp){ hasXYBools[samp] = sampleHasXY[samp][x][y]; }
+				assert hasXYBools.length == allOnes.length;
+				m.addConstraint(equation("cp:bc",inXYBin[x][y],hasXYBools,allOnes));
 			}
 		}
-		return 0;
+		
+		//Create the Log relation (I'll use base 2)
+		BinRelation fLog2Rel;
+		int[] rangeOfFLog2Rel = new int[N]; //Holds the integers that are in the range of fLog2Rel - in increasing order
+		{
+			int[] mins={0,fLog2(F,0)};
+			int[] maxes={N,fLog2(F,N)};
+			List<int[]> feasiblePairs = new ArrayList<int[]>(N+1);
+			for(int i = 0; i <= N; ++i){
+				int[] pair = {i, fLog2(F,i)};
+				if(i >= 1){ rangeOfFLog2Rel[i-1]=fLog2(F,i); }
+				feasiblePairs.add(pair);
+			}
+			fLog2Rel = makeBinRelation(mins, maxes, feasiblePairs, true);
+		}
+		
+		//Check the bits required for the intermediate products again.
+		double bitsRequiredForMaxProduct = 
+				Math.log(N)+Math.log(rangeOfFLog2Rel[N-1])+Math.log(2*b);
+		bitsRequiredForMaxProduct /= Math.log(2);
+		if(bitsRequiredForMaxProduct >= 31){
+			System.err.println("Warning: the upper bound estimate for one of the intermediate sums in the "+
+					"fixed-point integer approximation requires "+bitsRequiredForMaxProduct+" bits. "+
+					"Java integers only hold 31 bits. This could result in overflow and erroneous calculation. "+
+					"The program is assuming that this won't happen and calculating anyway. But double-check the "+
+					"results. Reducing F (the denominator of the fixed point fraction) could help. "+
+					"F is currently "+F+".");
+		}
+		
+		//Add variables for the logarithms of the different bin sizes
+		IntegerVariable[] logInXBin = new IntegerVariable[inXBin.length];
+		for(int x = 1; x <= b/2; ++x){
+			logInXBin[x]=makeIntVar("logInXBin["+x+"]", rangeOfFLog2Rel); 
+			m.addVariable(logInXBin[x]);
+			m.addConstraint(relationPairAC(inXBin[x], logInXBin[x], fLog2Rel));
+		}
+		IntegerVariable[] logInYBin = new IntegerVariable[inYBin.length];
+		for(int y = 1; y <= b/2; ++y){
+			logInYBin[y]=makeIntVar("logInYBin["+y+"]", rangeOfFLog2Rel); 
+			m.addVariable(logInYBin[y]);
+			m.addConstraint(relationPairAC(inYBin[y], logInYBin[y], fLog2Rel));
+		}
+		assert inXYBin.length > 0;
+		IntegerVariable[][] logInXYBin = new IntegerVariable[inXYBin.length][inXYBin[0].length];
+		for(int i = 0; i <= b/2; ++i){ logInXYBin[0][i]=logInXYBin[i][0]=null; } //Set placeholders to null
+		for(int x = 1; x <= b/2; ++x){
+			for(int y = 1; y <= b/2; ++y){
+				logInXYBin[x][y]=makeIntVar("logInXYBin["+x+"]["+y+"]", rangeOfFLog2Rel);
+				m.addVariable(logInXYBin[x][y]);
+				m.addConstraint(relationPairAC(inXYBin[x][y],logInXYBin[x][y], fLog2Rel));
+			}
+		}
+		
+		//Add product variables
+		IntegerVariable[][] XYLogN = new IntegerVariable[b/2+1][b/2+1];
+		IntegerVariable[][] XYLogXY = new IntegerVariable[b/2+1][b/2+1];
+		IntegerVariable[][] XYLogX = new IntegerVariable[b/2+1][b/2+1];
+		IntegerVariable[][] XYLogY = new IntegerVariable[b/2+1][b/2+1];
+		
+		//Set placeholders to null
+		for(int i = 0; i <= b/2; ++i){
+			XYLogN[i][0]=XYLogN[0][i]=null;
+			XYLogXY[i][0]=XYLogXY[0][i]=null;
+			XYLogX[i][0]=XYLogX[0][i]=null;
+			XYLogY[i][0]=XYLogY[0][i]=null;
+		}
+		
+		//Rest of constraints for product variables
+		for(int x = 1; x <= b/2; ++x){
+			for(int y = 1; y <= b/2; ++y){
+				XYLogN[x][y] = makeIntVar("XYLogN[" +x+"]["+y+"]", 0, N*fLog2(F,N));
+				XYLogXY[x][y]= makeIntVar("XYLogXY["+x+"]["+y+"]", 0, N*fLog2(F,N));
+				XYLogY[x][y] = makeIntVar("XYLogY[" +x+"]["+y+"]", 0, N*fLog2(F,N));
+				XYLogX[x][y] = makeIntVar("XYLogX[" +x+"]["+y+"]", 0, N*fLog2(F,N));
+				m.addVariables(XYLogN[x][y],XYLogXY[x][y],XYLogX[x][y],XYLogX[x][y]);
+				m.addConstraint(times(inXYBin[x][y], fLog2(F,N),       XYLogN[x][y]));
+				m.addConstraint(times(inXYBin[x][y], logInXYBin[x][y], XYLogXY[x][y]));
+				m.addConstraint(times(inXYBin[x][y], logInXBin[x],     XYLogX[x][y]));
+				m.addConstraint(times(inXYBin[x][y], logInYBin[y],     XYLogY[x][y]));
+			}
+		}
+		
+		IntegerVariable[] allProductVars = new IntegerVariable[4*(b/2)*(b/2)];
+		int[] productVarsCoeffs = new int[allProductVars.length];
+		for(int x = 1; x <= b/2; ++x){
+			for(int y = 1; y <= b/2; ++y){
+				int offset = 4*((x-1)*b/2+(y-1));
+				allProductVars   [offset+0]=XYLogN[x][y];
+				productVarsCoeffs[offset+0]=1;
+				allProductVars   [offset+1]=XYLogXY[x][y];
+				productVarsCoeffs[offset+1]=1;
+				allProductVars   [offset+2]=XYLogX[x][y];
+				productVarsCoeffs[offset+2]=-1;
+				allProductVars   [offset+3]=XYLogY[x][y];
+				productVarsCoeffs[offset+3]=-1;
+			}
+		}
+		
+		//Unscaled MutInf is the sum of all the product variables multiplied by their particular coefficients
+		//
+		//If you divide by N you get the Mutual Information expressed in F-denominator fixed point.
+		IntegerVariable unscaledMutInf = makeIntVar("unscaledMutInf", 0, N*fLog2(F,b));
+		m.addVariable(unscaledMutInf);
+		m.addConstraint(equation(unscaledMutInf, allProductVars, productVarsCoeffs));
+		
+		//Mutual information is the unscaled MutInf divided by N expressed in F-denominator fixed point
+		IntegerConstantVariable NVar = constant(N); //N as a variable in the model
+		IntegerVariable mutualInf = makeIntVar("mutualInformation", 0, fLog2(F,b));
+		m.addVariable(mutualInf);
+		m.addConstraint(intDiv(unscaledMutInf, NVar, mutualInf));
+		
+		//Minimum Grid Dimension expressed in F-denominator fixed point
+		IntegerVariable minGridDim = makeIntVar("minimumGridDimension",0,b/2);
+		m.addVariable(minGridDim);
+		m.addConstraint(min(numXBins, numYBins, minGridDim));
+		
+		//Take the log of the minimum grid dimension
+		IntegerVariable logMinGridDim = makeIntVar("logMinimumGridDimension", rangeOfFLog2Rel);
+		m.addVariable(logMinGridDim);
+		m.addConstraint(relationPairAC(minGridDim,logMinGridDim, fLog2Rel));
+		
+		//MIC is the mutual information divided by the log of the minimum grid dimension
+		IntegerVariable mic = makeIntVar("mic",0,F);
+		m.addVariable(mic);
+		m.addConstraint(intDiv(mutualInf,logMinGridDim,mic));
+		
+		//Solve the optimization problem
+		CPSolver s = new CPSolver();
+		s.read(m);
+		s.maximize(s.getVar(mic), false);
+		
+		//TODO: recalculate the MIC for the chosen grid and binning using the set cardinality 
+		//variables (inXYBin[][] etc.) and floating point to get better resolution than just the 
+		//fixed point approximation
+		double micFixed = (double)s.getVar(mic).getVal();
+		return micFixed/F;
+	}
+	/**
+	 * Return a fixed point integer representation of a modified log function base 2.
+	 * 
+	 * Return Round(F * log n / log 2), that is, the logarithm base 2, multiplied by F and then rounded to
+	 * the nearest integer. If divided by F, this will give the true logarithm accurate to +/- 1/F.
+	 * 
+	 * I modify the log function so log(0) = 0 (rather than -infinity as would be the normal representation)  
+	 * since I will be using this function in information-theoretic contexts
+	 * 
+	 * @param F The denominator of the fixed point representation.  Divide by F to get the actual logarithm.
+	 * @param n The number whose logarithm will be taken
+	 * @return Round(F * log n / log 2) if n != 0 or 0 if n == 0
+	 * 
+	 * @throws IllegalArgumentException if n < 0
+	 */
+	private static int fLog2(int F, int n) throws IllegalArgumentException {
+		if(n < 0){ throw new IllegalArgumentException("You can't take the log of a negative number."); }
+		if(n > 0){
+			return 0;
+		}else{
+			return 0;
+		}
 	}
 
 }
