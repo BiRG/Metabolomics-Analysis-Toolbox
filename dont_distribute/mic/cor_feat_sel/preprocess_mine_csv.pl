bcb2ba1664d26da55d60eaa4844c6180f54248b7
Eric David Moyer <eric_moyer@yahoo.com>
2012-02-22 16:54:08 -0500

    Added code for preprocessing the csv input to MINE.jar in order to make my life easier.

---
+++ dont_distribute/mic/cor_feat_sel/preprocess_mine_csv.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+if(@ARGV != 1){
+    print STDERR "Usage: preprocess_mine_csv class_var_index\n".
+	"Replaces the class names in the csv with f_number or c if they are\n".
+	"the class variable.\n".
+	"Outputs the new csv to stdout.\n".
+	"Assumes that the header is in the first line.\n";
+}
+
+my $class_idx = $ARGV[0];
+while(<STDIN>){
+    if($. != 1){
+	print;
+    }else{
+	chomp;
+	my @fields = split /,/;
+	for my $idx (0..$#fields){
+	    if($idx > 0){ print ","; };
+	    if($idx < $class_idx){
+		print "f_$idx";
+	    }elsif($idx > $class_idx){
+		print "f_",$idx-1;
+	    }else{
+		print "c";
+	    }
+	}
+	print "\n";
+    }
+    
+}
