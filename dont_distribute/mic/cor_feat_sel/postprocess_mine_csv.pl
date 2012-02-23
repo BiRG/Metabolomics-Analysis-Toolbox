9f848c9312434d1bd6a8ef059fa13e6ae2219b55
Eric David Moyer <eric_moyer@yahoo.com>
2012-02-23 17:43:02 -0500

    postprocess now adds lines of 1 MIC for every feature with itself

--- dont_distribute/mic/cor_feat_sel/postprocess_mine_csv.pl
+++ dont_distribute/mic/cor_feat_sel/postprocess_mine_csv.pl
@@ -4,13 +4,16 @@ use warnings;
 
 if(@ARGV != 0){
     print STDERR "Usage: postprocess_mine_csv < mine_output.csv > input_for_cor_based_fs.csv\n".
-	"Removes the header and replaces f_number with number\n".
+	"Removes the header, replaces f_number with number, only outputs\n".
+	"first three fields, adds lines x,x,1 for every pair of field\n".
+	"values.\n".
 	"\n".
 	"Outputs the new csv to stdout.\n".
 	"Assumes that the header is in the first line.\n";
     exit(-1);
 }
 
+my $maxField = 0;
 while(<STDIN>){
     if($. != 1){
 	chomp;	
@@ -26,9 +29,14 @@ while(<STDIN>){
 	    my $tmp = $fields[1];
 	    $fields[1] = $fields[0]; $fields[0]=$tmp;
 	}
+	$maxField = $fields[1] if $maxField < $fields[1];
 	print "$fields[0],$fields[1],$fields[2]\n";
     }else{
 	#Don't print anything for the header line
     }
-    
+}
+
+#Add extra lines for a field's correlation with itself.
+for my $i (0..$maxField){
+    print "$i,$i,1\n";
 }
