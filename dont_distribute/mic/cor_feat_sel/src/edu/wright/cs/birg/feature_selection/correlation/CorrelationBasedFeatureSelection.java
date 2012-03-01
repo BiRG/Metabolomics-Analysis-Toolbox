a06a85cddc4c3e1003277286a5cc5f8103b8b602
Eric David Moyer <eric_moyer@yahoo.com>
2012-03-01 10:06:22 -0500

    (This is yesterday and today)
    Major rewrite.  No longer reads correlations in at all.  Now just
    calculates them inline.  Calculates MIC does the feature selection.

--- dont_distribute/mic/cor_feat_sel/src/edu/wright/cs/birg/feature_selection/correlation/CorrelationBasedFeatureSelection.java
+++ dont_distribute/mic/cor_feat_sel/src/edu/wright/cs/birg/feature_selection/correlation/CorrelationBasedFeatureSelection.java
@@ -3,13 +3,13 @@
  */
 package edu.wright.cs.birg.feature_selection.correlation;
 
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.PriorityQueue;
+
+import org.sureassert.uc.annotation.Exemplars;
+import org.sureassert.uc.annotation.Exemplar;
 
 import edu.wright.cs.birg.CSVUtil;
 import edu.wright.cs.birg.mic.MIC;
@@ -24,11 +24,6 @@ import au.com.bytecode.opencsv.CSVReader;
 public class CorrelationBasedFeatureSelection {
 
 	/**
-	 * The default correlation that will be given to any entries not appearing in the input file
-	 */
-	private static final double default_correlation = 0.0000001;
-	
-	/**
 	 * The number of nodes that can be expanded without improving the solution before the best-first optimization quits.
 	 * That is if non_improvements_before_quit is 5, then on the if there have been 4 feature-sets in a row 
 	 * for which none of the solutions with one more feature have had a better optimum value, then if the next 
@@ -38,114 +33,168 @@ public class CorrelationBasedFeatureSelection {
 	private static final int non_improvements_before_quit = 5;
 		
 	/**
+	 * Print the usage message follwed by msg on its own line. If msg is null, it is not printed, 
+	 * but the usage is still printed.
+	 * @param msg The message to print after the usage message. not printed if null.
+	 */
+	
+	@Exemplars(set={
+	@Exemplar(args={"'foo'"}, expect=""),
+	@Exemplar(args={"null"}, expect=""), 
+	})
+	public static void printUsage(String msg){
+		System.err.println("Usage: java -jar cbfs.jar -genfeatures classIdx < input_data.csv > features");
+		System.err.println(" or:   java -jar cbfs.jar -gentestdata < input_data.csv > test_data.txt");
+		System.err.println(" or:   java -jar cbfs.jar -gentestdata input_data.csv > test_data.txt");
+		System.err.println("");
+		System.err.println("Reads a csv from stdin or a file and if the option is -genfeatures");
+		System.err.println("writes the selected list of features to stdout: 1 per line.");
+		System.err.println("classIdx is the column index of the class feature. First column is 0.");
+		System.err.println("");
+		System.err.println("The -gentestdata option reads in a csv but the columns of the csv ");
+		System.err.println("are features and the rows are samples. Then appropriate test lines ");
+		System.err.println("are printed to stdout.");
+		System.err.println("");
+		System.err.println("The first line of the csv may be a header (if it is, it must ");
+		System.err.println("contain a letter).  All other lines must be the same length and must ");
+		System.err.println("have the decimal representation of a real number. The columns of the ");
+		System.err.println("csv are features and the rows are samples.");
+		if(msg != null){
+			System.err.println("");			
+			System.err.println(msg);
+		}
+	}
+	
+	/**
+	 * Reads data matrix from in and writes list of features to stdout
+	 * @param in CSV file holding the data matrix
+	 * @param classIdx 0-based index of the class feature - there must be at least 1 other feature
+	 */
+	private static void generateFeatures(CSVReader in, final int classIdx){
+		//Read in the 2d array from the test file
+		double[][] data;
+		String[] varNames;
+		try{
+			CSVUtil.Matrix m = CSVUtil.csvToMatrix(in);
+			data = m.entries;
+			varNames = m.header;
+		}catch(java.text.ParseException e){
+			System.err.println("Error parsing input csv file as a matrix:" + e.getMessage());
+			System.exit(-1); return;
+		}catch(IOException e){
+			System.err.println("IO Error reading input csv file:" + e.getLocalizedMessage());
+			System.exit(-1); return;
+		}
+		if(data.length <= 0){ return; }
+		
+
+		//Transpose the array to make an array of variables/features
+		double[][] vars = new double[data[0].length][data.length];
+		for(int i = 0; i < vars.length; ++i){
+			for(int j=0; j < data.length; ++j){
+				vars[i][j]=data[j][i];
+			}
+		}
+
+		//Ensure valid classIdx input
+		if(classIdx < 0 || classIdx >= vars.length){
+			throw new IllegalArgumentException("classIdx out of bounds: classIdx was "+classIdx+" when there "+
+					"were only "+vars.length+" variables.  Legal indices were 0.."+(vars.length-1));
+		}
+		
+		//Separate features and class from one another
+		int numFeatures = varNames.length-1;
+		double[] classV = vars[classIdx];
+		double[][] featureV = new double[numFeatures][];
+		for(int feat = 0; feat < numFeatures; ++feat){
+			if(feat < classIdx){
+				featureV[feat]=vars[feat];
+			}else if(feat > classIdx){
+				featureV[feat]=vars[feat+1];
+			}else{
+				//Do nothing, this is the class variable
+			}
+		}
+
+		//Calculate class-feature MICs
+		double[] classCor = new double[numFeatures];
+	
+		System.err.println("Calculating class MICs.");
+		
+		for(int feat = 0; feat < numFeatures; ++feat){
+			classCor[feat]=MIC.mic(featureV[feat], classV);
+		}
+		
+		//Calculate feature-feature MICs
+		System.err.print("Calculating MICs for feature:");
+		double[][] featureCor = new double[numFeatures][numFeatures];
+		for(int f1 = 0; f1 < numFeatures; ++f1){
+			System.err.print(" "+f1);
+			featureCor[f1][f1] = 1.0;
+			double[] v1 = featureV[f1];
+			for(int f2 = 0; f2 < numFeatures; ++f2){
+				double[] v2 = featureV[f2];
+				featureCor[f1][f2] = featureCor[f2][f1] = MIC.mic(v1,v2);
+			}
+		}
+		System.err.println();
+	
+		//Do the feature selection
+		int[] best = bestFirstSearch(classCor, featureCor, non_improvements_before_quit).features();
+		for(int i = 0; i < best.length; ++i){
+			System.out.println(best[i]);
+		}
+		
+	}
+	
+	/**
 	 * @param args name of input file or (if empty) reads from stdin
 	 * 
 	 */
 	public static void main(String[] args) {
-		if(args.length != 0 && args.length != 1){
-			System.err.println("Usage: java edu.wright.cs.birg.feature_selection.correlation < filename.csv > features");
-			System.err.println(" or:   java edu.wright.cs.birg.feature_selection.correlation filename.csv > features");
-			System.err.println(" or:   java edu.wright.cs.birg.feature_selection.correlation -gentestdata < input_data.csv > test_data.txt");
-			System.err.println("");
-			System.err.println("Reads a csv from stdin or a file and writes the selected list of ");
-			System.err.println("features to stdout: 1 per line.");
-			System.err.println("");
-			System.err.println("The csv must have all lines as:");
-			System.err.println("var1,var2,correlation");
-			System.err.println("var1 must be either an integer from 0..n or the letter c (for the ");
-			System.err.println("     class variable).");
-			System.err.println("var2 must be an integer from 0..n.  If var1 is not c then ");
-			System.err.println("     var1 < var2 must hold.");
-			System.err.println("correlation must be the decimal representation of a real number");
-			System.err.println("");
-			System.err.println("Unless n is 0, if variable n is present, variable n-1 is assumed ");
-			System.err.println("to exist (no gaps in numbering)");
-			System.err.println("Any pairs that do not get assigned a \"correlation\" value will be ");
-			System.err.println("given a value of "+default_correlation+"because that is the cutoff MINE.jar ");
-			System.err.println("uses for display");
-			System.err.println("");
-			System.err.println("The -gentestdata option reads in a csv but the columns of the csv ");
-			System.err.println("are features and the rows are samples. Then appropriate test lines ");
-			System.err.println("are printed to stdout.");
+		if(args.length != 1 && args.length != 2){
+			printUsage("Wrong number of arguments");
 			return;
 		}
 
-		int numFeatures = 0;
-		double[] classCor = null;
-		double[][] featureCor = null;
-		try{
-			CSVReader in; 
-			if(args.length == 0){
-				in = new CSVReader(new InputStreamReader(System.in));
-			}else{
-				if(!args[0].equals("-gentestdata")){
-					in = new CSVReader(new FileReader(args[0]));
-				}else{
-					generateTestData(new CSVReader(new InputStreamReader(System.in)));
-					return;
-				}
-			}
-			
-			//Read in lines into lists of class-feature correlations and feature-feature correlations, counting the number of features
-			class CFCor{ int feat; double cor; public CFCor(int f, double c){feat = f; cor = c; } }
-			class FFCor{ int feat1; int feat2; double cor; public FFCor(int f1, int f2, double c){feat1 = f1; feat2 = f2; cor = c; } }
-			List<CFCor> cfcors = new LinkedList<CFCor>();
-			List<FFCor> ffcors = new LinkedList<FFCor>();
-			String[] line;
-			while((line = in.readNext()) != null){
-				if(line.length != 3){ System.err.println("Error: CSV Line not 3 fields long."); return; }
-				numFeatures = Math.max(numFeatures, 1+Integer.parseInt(line[1]));
+		if(!(args[1].equals("-gentestdata") || args[1].equals("-genfeatures"))){
+			printUsage("Unknown output generation option \""+args[1]+"\"");
+		}
+		boolean genFeatures = args[1].equals("-genfeatures");
 
-				int f1 = Integer.parseInt(line[1]);
-				double cor = Double.parseDouble(line[2]);
-				if(line[0].equals("c")){
-					cfcors.add(new CFCor(f1,cor));
-				}else{
-					int f0 = Integer.parseInt(line[0]);
-					ffcors.add(new FFCor(f0,f1,cor));
-				}				
+		if(genFeatures){
+			if(args.length != 2){
+				printUsage("You must include a class feature index to select features for predicting that class.");
+				return;
 			}
-			in.close();
-			
-			//Initialize arrays of class-feature correlations and feature-feature correlations to NaN using the now known number of features
-			classCor = new double[numFeatures];
-			featureCor = new double[numFeatures][numFeatures];
-			for(int i = 0; i < numFeatures; ++i){
-				classCor[i]=Double.NaN;
-				for(int j = 0; j < numFeatures; ++j){
-					featureCor[i][j]=Double.NaN;
-				}
+			try{
+				Integer.parseInt(args[1]);
+			}catch (NumberFormatException e){
+				printUsage("The argument to -genfeatures must be an integer");
+				return;
 			}
-			
-			//Take values read in and transfer them to the arrays
-			for(CFCor cur: cfcors){
-				classCor[cur.feat]=cur.cor;
-			}
-			for(FFCor cur: ffcors){
-				featureCor[cur.feat1][cur.feat2]=cur.cor;
-				featureCor[cur.feat2][cur.feat1]=cur.cor;
-			}
-			
-			//Initialize all correlations not read in to default_correlation
-			for(int i = 0; i < numFeatures; ++i){
-				if(Double.isNaN(classCor[i])){ classCor[i]=default_correlation; }
-				for(int j = 0; j < numFeatures; ++j){
-					if(Double.isNaN(featureCor[i][j])){ featureCor[i][j]=default_correlation; }
-				}
-			}
-		}catch(IOException e){
-			String filename;
-			if(args.length == 0){
-				filename = "the standard input stream";
-			}else{
-				filename = args[0];
-			}
-			System.err.println("Error reading from "+filename+": "+e.getMessage());
 		}
 		
-		int[] best = bestFirstSearch(classCor, featureCor, non_improvements_before_quit).features();
-		for(int i = 0; i < best.length; ++i){
-			System.out.println(best[i]);
+		try{
+			CSVReader in = new CSVReader(new InputStreamReader(System.in)); 
+
+			if(genFeatures){
+				assert(args.length == 2);
+				generateFeatures(in, Integer.parseInt(args[1]));
+				return;
+			}else{
+				generateTestData(in);
+				return;
+			}
+		} catch (IOException e) {
+			String filename;
+			if (args.length == 0) {
+				filename = "the standard input stream";
+			} else {
+				filename = args[0];
+			}
+			System.err.println("Error reading from " + filename + ": "
+					+ e.getMessage());
 		}
 	}
 	
@@ -163,7 +212,7 @@ public class CorrelationBasedFeatureSelection {
 		//Read in the 2d array from the test file
 		double[][] data;
 		try{
-			data = CSVUtil.csvToMatrix(in);
+			data = CSVUtil.csvToMatrix(in).entries;
 		}catch(java.text.ParseException e){
 			System.err.println("Error parsing input csv file as a matrix:" + e.getMessage());
 			System.exit(-1); return;
