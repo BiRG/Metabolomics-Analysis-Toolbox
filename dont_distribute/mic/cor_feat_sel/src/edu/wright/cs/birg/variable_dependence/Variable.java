1a915bc4b9782693bd8cf045bab17822918239fa
Eric David Moyer <eric_moyer@yahoo.com>
2012-03-05 12:13:32 -0500

    Added a few new named exemplars to the Variable constructor that will be useful in testing the Pearson correlation measure

--- dont_distribute/mic/cor_feat_sel/src/edu/wright/cs/birg/variable_dependence/Variable.java
+++ dont_distribute/mic/cor_feat_sel/src/edu/wright/cs/birg/variable_dependence/Variable.java
@@ -63,7 +63,17 @@ public class Variable{
 	@Exemplar(args={"'My variable'","null","0"}, ee="NullPointerException", expect={"retval.getMessage().contains('data field')"}), 
 	@Exemplar(args={"'My variable'","[pa:1.0]","-1"}, ee="IllegalArgumentException", expect={"retval.getMessage().contains('non-negative')"}), 
 	@Exemplar(name="MyVar1_0",args={"'MyVar'","[pa:1.0]","0"}, 
-		expect={"=(ArrayUtils.len(retval.data),1)","=(retval.name,'MyVar')","=(retval.index,0)"}), 
+	expect={"=(ArrayUtils.len(retval.data),1)","=(retval.name,'MyVar')","=(retval.index,0)"}), 
+	@Exemplar(name="Empty",args={"'Empty'","ArrayUtils.emptyDouble()","1"}, 
+	expect={"=(ArrayUtils.len(retval.data),0)","=(retval.name,'Empty')","=(retval.index,1)"}), 
+	@Exemplar(name="OneTwo",args={"'OneAndTwo'","[pa:1.0,2.0]","5"}, 
+	expect={"=(ArrayUtils.len(retval.data),2)","=(retval.name,'OneAndTwo')","=(retval.index,5)"}), 
+	@Exemplar(name="TwentyInLine",args={"'TwentyInLine'","[pa:1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0,11.0,12.0,13.0,14.0,15.0,16.0,17.0,18.0,19.0,20.0]","20"}, 
+	expect={"=(ArrayUtils.len(retval.data),20)","=(retval.name,'TwentyInLine')","=(retval.index,20)"}), 
+	@Exemplar(name="TwentySquares",args={"'TwentySquares'","[pa:1.0,4.0,9.0,16.0,25.0,36.0,49.0,64.0,81.0,100.0,121.0,144.0,169.0,196.0,225.0,256.0,289.0,324.0,361.0,400.0]","21"}, 
+	expect={"=(ArrayUtils.len(retval.data),20)","=(retval.name,'TwentySquares')","=(retval.index,21)"}), 
+	@Exemplar(name="UShape",args={"'UShape'","[pa:81.0,64.0,49.0,36.0,25.0,16.0,9.0,4.0,1.0,0.0,0.0,1.0,4.0,9.0,16.0,25.0,36.0,49.0,64.0,81.0]","22"}, 
+	expect={"=(ArrayUtils.len(retval.data),20)","=(retval.name,'UShape')","=(retval.index,22)"}), 
 	})
 	public Variable(String name, double[] data, int index){
 		if(name == null){
