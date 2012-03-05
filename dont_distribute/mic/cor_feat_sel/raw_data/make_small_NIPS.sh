50ad24b88bed0b74d398b303776417c2e59ee4e2
Eric David Moyer <eric_moyer@yahoo.com>
2012-03-05 17:09:03 -0500

    Wrote script to make a copy of the NIPS datasets with fewer lines for faster processing.

---
+++ dont_distribute/mic/cor_feat_sel/raw_data/make_small_NIPS.sh
@@ -0,0 +1,9 @@
+#This script makes a smaller version of the NIPS2003 GISETTE data set
+#with only the first 1000 samples
+#
+#It should be run from the raw_data directory
+
+mkdir -p SmallNIPS2003/GISETTE
+for i in gisette_train.data gisette_train.labels gisette_valid.data gisette_valid.labels; do
+    head -n 1000 < NIPS2003/GISETTE/$i > SmallNIPS2003/GISETTE/$i
+done
