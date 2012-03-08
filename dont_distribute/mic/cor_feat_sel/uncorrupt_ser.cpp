1110de1a9dfe7cab535bf8686b1488cd025ac060
Eric David Moyer <eric_moyer@yahoo.com>
2012-03-08 12:24:47 -0500

    Added buffering manually - still didn't help the time to read through the file, but made the code more complex.

--- dont_distribute/mic/cor_feat_sel/uncorrupt_ser.cpp
+++ dont_distribute/mic/cor_feat_sel/uncorrupt_ser.cpp
@@ -2,6 +2,32 @@
 #include <fstream>
 #include <cstdlib>
 #include <queue>
+
+
+//Return true if c was gotten successfully
+bool buffGetC(char& c, std::ifstream& in){
+  static std::deque<char> already_read;
+  if(already_read.size() > 0){
+    //Pop the next character from the queue and return it
+    c = already_read.front(); already_read.pop_front();
+    return true;;
+  }else{
+    //Refill the queue
+    const unsigned bufsize = 65535;
+    char buf[bufsize];
+    in.read(buf, bufsize);
+    std::streamsize num_read = in.gcount();
+    already_read.insert(already_read.end(), buf, buf+num_read);
+
+    if(already_read.size() > 0){
+      c = already_read.front(); already_read.pop_front();
+      return true;
+    }else{
+      return false;
+    }
+  }
+}
+
 int main(int argc, char**argv){
   if(argc != 3){
     std::cerr << "Usage: uncorrupt_ser input_file output_file\n"
@@ -13,22 +39,31 @@ int main(int argc, char**argv){
     return -1;
   }
 
-  ifstream in(argv[1]);
+  std::ifstream in(argv[1]);
   if(!in){
     std::cerr << "Error: Could not open " << argv[1] << " for reading.\n";
     return -1;
   }
 
   //Find the header
+  unsigned bytes_read = 0;
+  unsigned gb_read = 0;
   std::deque<unsigned char> last4Chars;
   bool foundHeader = false;
 
-  unsigned char c; 
-  while(in.get(c)){
+  char c; 
+
+  while(buffGetC(c,in)){
+    ++bytes_read;
+    if(bytes_read == 1024*1024*1024){
+      bytes_read = 0;
+      ++gb_read;
+      std::cerr << "Read " << gb_read << "GiB. ";
+    }
     while(last4Chars.size() > 3){
       last4Chars.pop_front();
     }
-    last4Chars.push_back(c);
+    last4Chars.push_back(c); //Note the conversion to unsigned
     
     if(last4Chars.size() == 4){
       if(last4Chars[0] == 172 && 
@@ -48,7 +83,7 @@ int main(int argc, char**argv){
   }
 
   //Copy the rest of the file
-  ofstream out(argv[2]);
+  std::ofstream out(argv[2]);
   if(!out){
     std::cerr << "Error: Could not open " << argv[2] << " for writing.\n";
     return -1;
@@ -58,7 +93,7 @@ int main(int argc, char**argv){
   out.put(172); out.put(237); out.put(0); out.put(5);
 
   //Copy the rest of the file
-  while(in.get(c)){
+  while(buffGetC(c,in)){
     out.put(c);
   }
 
