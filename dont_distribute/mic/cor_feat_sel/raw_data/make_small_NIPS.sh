cfc178ae9a92fc91e95b6c69b97a0683ef5b4179
Eric David Moyer <eric_moyer@yahoo.com>
2012-03-08 01:01:52 -0500

    Went back to 100 samples because bio-ws is so much faster.

--- dont_distribute/mic/cor_feat_sel/raw_data/make_small_NIPS.sh
+++ dont_distribute/mic/cor_feat_sel/raw_data/make_small_NIPS.sh
@@ -1,9 +1,9 @@
 #This script makes a smaller version of the NIPS2003 GISETTE data set
-#with only the first 25 samples
+#with only the first 100 samples
 #
 #It should be run from the raw_data directory
 
 mkdir -p SmallNIPS2003/GISETTE
 for i in gisette_train.data gisette_train.labels gisette_valid.data gisette_valid.labels; do
-    head -n 25 < NIPS2003/GISETTE/$i > SmallNIPS2003/GISETTE/$i
+    head -n 100 < NIPS2003/GISETTE/$i > SmallNIPS2003/GISETTE/$i
 done
