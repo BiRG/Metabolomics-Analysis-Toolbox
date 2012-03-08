66177b4c782795511baf236f2db56faa5e7e9cbd
Eric David Moyer <eric_moyer@yahoo.com>
2012-03-08 00:14:49 -0500

    Made the small nips even smaller 50 samples

--- dont_distribute/mic/cor_feat_sel/raw_data/make_small_NIPS.sh
+++ dont_distribute/mic/cor_feat_sel/raw_data/make_small_NIPS.sh
@@ -1,9 +1,9 @@
 #This script makes a smaller version of the NIPS2003 GISETTE data set
-#with only the first 100 samples
+#with only the first 50 samples
 #
 #It should be run from the raw_data directory
 
 mkdir -p SmallNIPS2003/GISETTE
 for i in gisette_train.data gisette_train.labels gisette_valid.data gisette_valid.labels; do
-    head -n 100 < NIPS2003/GISETTE/$i > SmallNIPS2003/GISETTE/$i
+    head -n 50 < NIPS2003/GISETTE/$i > SmallNIPS2003/GISETTE/$i
 done
