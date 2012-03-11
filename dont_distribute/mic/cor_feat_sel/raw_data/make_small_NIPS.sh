6fe34f53dc2d8daa31dc2731325e4cc534b2aa44
Eric David Moyer <eric_moyer@yahoo.com>
2012-03-11 18:58:20 -0400

    Fixed bug which also shrank the GISETTE validation set to the first 100 elements when it shrank the training set.

--- dont_distribute/mic/cor_feat_sel/raw_data/make_small_NIPS.sh
+++ dont_distribute/mic/cor_feat_sel/raw_data/make_small_NIPS.sh
@@ -4,6 +4,10 @@
 #It should be run from the raw_data directory
 
 mkdir -p SmallNIPS2003/GISETTE
-for i in gisette_train.data gisette_train.labels gisette_valid.data gisette_valid.labels; do
+for i in gisette_train.data gisette_train.labels ; do
     head -n 100 < NIPS2003/GISETTE/$i > SmallNIPS2003/GISETTE/$i
+done
+
+for i in gisette_valid.data gisette_valid.labels ; do
+    cp NIPS2003/GISETTE/$i SmallNIPS2003/GISETTE/$i
 done
