2cf1e4582996e9c3ee7f5ad9529b51b3e9d4af4f
Eric David Moyer <eric_moyer@yahoo.com>
2012-03-20 20:49:57 -0400

    Fixed definition of fullName and shortName to say that they can't be blank. Also implemented the isValidshortName routine (which was just a stub)

--- dont_distribute/mic/mic_distr/src/edu/wright/cs/birg/experiment/micdistribution/Relation.java
+++ dont_distribute/mic/mic_distr/src/edu/wright/cs/birg/experiment/micdistribution/Relation.java
@@ -16,7 +16,8 @@ import java.util.Random;
  */
 public abstract class Relation {
 	/**
-	 * The full, human readable name of this Relation. Should not have tabs.
+	 * The full, human readable name of this Relation. Should not have tabs or
+	 * be blank.
 	 */
 	private final String fullName;
 	
@@ -28,36 +29,35 @@ public abstract class Relation {
 	
 	/**
 	 * The short, safe name of the relation. May only consist of letters,
-	 * numbers, and _. Two objects have the same shortName if and only if they
-	 * have the same id.
+	 * numbers, and _ and must not be blank. Two objects have the same shortName
+	 * if and only if they have the same id.
 	 */
 	private final String shortName;
 	
 	/**
 	 * Return true iff s is a valid full name for a relation, that is, it does
-	 * not have tab characters.
+	 * not have tab characters and is not blank.
 	 * 
 	 * @param s
 	 *            The string to be tested.
 	 * @return true iff s is a valid full name for a relation, that is, it does
 	 *         not have tab characters.
 	 */
-	private static boolean isValidFullName(String s){
-		return !s.contains("\t");
+	private static boolean isValidFullName(String s) {
+		return s.length() > 0 && !s.contains("\t");
 	}
 	
 	/**
 	 * Return true iff s is a valid short name for a relation, that is, it
-	 * contains only _ and alphanumeric characters.
+	 * contains only _ and alphanumeric characters and is not blank.
 	 * 
 	 * @param s
 	 *            The string to be tested.
 	 * @return true iff s is a valid short name for a relation, that is, it
 	 *         contains only _ and alphanumeric characters.
 	 */
-	private static boolean isValidShortName(String shortName2) {
-		// TODO Auto-generated method stub
-		return false;
+	private static boolean isValidShortName(String s) {
+		return s.matches("\\w+");
 	}
 
 	/**
@@ -101,11 +101,11 @@ public abstract class Relation {
 	public Relation(int id, String shortName, String fullName){
 		if(!isValidFullName(fullName)){
 			throw new IllegalArgumentException("The string \""+fullName+"\" is not a valid full name for a"+
-					" relation because it contains tab characters.");
+					" relation because it contains tab characters or is blank.");
 		}
 		if(!isValidShortName(shortName)){
 			throw new IllegalArgumentException("The string \""+shortName+"\" is not a valid short name for a"+
-					" relation because it contains characters other than letters, numbers and _.");
+					" relation because it is blank or contains characters other than letters, numbers and _.");
 		}
 		this.fullName = fullName;
 		this.shortName = shortName;
