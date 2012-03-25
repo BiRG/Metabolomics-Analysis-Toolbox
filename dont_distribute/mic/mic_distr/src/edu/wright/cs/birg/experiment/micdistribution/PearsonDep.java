d61a0cd3957f1592c6352f2a9e8f085572d95a2e
Eric David Moyer <eric_moyer@yahoo.com>
2012-03-24 23:19:37 -0400

    Added test cases to Pearson
    Fixed several bugs:
    
    1. Ensured that behavior for zero-length arrays was correct.
    
    2. Added missing square root to standard deviation calculation
    
    3. Fixed incorrect formula for pearson coefficient (it was incorrect
       because I got it from Wikipedia, which was missing a square). I also
       fixed the Wikipedia article.

--- dont_distribute/mic/mic_distr/src/edu/wright/cs/birg/experiment/micdistribution/PearsonDep.java
+++ dont_distribute/mic/mic_distr/src/edu/wright/cs/birg/experiment/micdistribution/PearsonDep.java
@@ -3,12 +3,14 @@
  */
 package edu.wright.cs.birg.experiment.micdistribution;
 
+import org.sureassert.uc.annotation.Exemplar;
+import org.sureassert.uc.annotation.Exemplars;
+
 /**
- * Absolute value of pearson coefficient
+ * Absolute value of Pearson coefficient
  * @author Eric Moyer
  *
  */
-//TODO: fix Pearson so it returns values in the interval 0..1, it currently can return values like 3.333333254
 public final class PearsonDep implements DependenceMeasure {
 
 	/* (non-Javadoc)
@@ -38,10 +40,20 @@ public final class PearsonDep implements DependenceMeasure {
 	/**
 	 * Return the sample Pearson product-moment correlation coefficient for the given instance.
 	 * This is a public static method so it can be used by the Spearman correlation. 
-	 * @param inst The instance whose correlation is to be measured. Must have at least 2 samples.
+	 * @param inst The instance whose correlation is to be measured. Must have at least 2 samples. Cannot be null.
 	 * @return the sample Pearson product-moment correlation coefficient for the given instance
 	 */
+	@Exemplars(set={
+	@Exemplar(args={"null"}, ee="NullPointerException"),
+	@Exemplar(args={"Instance/i0_0"}, ee="IllegalArgumentException"), 
+	@Exemplar(args={"Instance/i01_10"}, e="-1f"),
+	@Exemplar(args={"Instance/i012_120"}, e="-0.5f"),
+	@Exemplar(args={"Instance/i0123_1032"}, e="0.6f"),
+	})
 	public static float valueOf(Instance inst){
+		if(inst.getNumSamples() < 2){
+			throw new IllegalArgumentException("Instances passed to Pearson.valueOf must have at least two samples.");
+		}
 		double mx = mean(inst.x);
 		double my = mean(inst.y);
 		double sx = std(inst.x, mx);
@@ -52,35 +64,56 @@ public final class PearsonDep implements DependenceMeasure {
 			double y = inst.y[i];
 			sum += ((x-mx)/sx)*((y-my)/sy);
 		}
-		return (float)sum/(inst.getNumSamples()-1);
+		double norm = inst.getNumSamples()-1;
+		return (float)(sum/(norm*norm));
 	}
 
 	/**
 	 * Return the sample standard deviation of the values in the x array. 
-	 * The standard deviation of an empty array is 0.
+	 * The standard deviation of an empty array and of one with one element is 0.
 	 * @param x The values whose standard deviation will be calculated.
 	 * @param mean The mean of the x array
 	 * @return the sample standard deviation of the values in the x array.
 	 */
+	@Exemplars(set={
+	@Exemplar(args={"null","0d"}, ee="NullPointerException"),
+	@Exemplar(args={"edu/wright/cs/birg/test/ArrayUtils.emptyFloat()","1d"}, expect="0.0"),
+	@Exemplar(args={"[pa:1.0f]","1d"}, expect="0.0"),
+	@Exemplar(args={"[pa:1.0f,2.5f]","1.75"}, expect="1.0606601717798213"),
+	})
 	private static double std(float[] x, double mean) {
 		double sum = 0;
 		for(float val:x){
 			double dx = val-mean;
 			sum += dx*dx;
 		}
-		return sum/(x.length-1);
+		if(x.length > 1){
+			return Math.sqrt(sum)/(x.length-1);
+		}else{
+			return 0;
+		}
 	}
 
 	/**
 	 * Return the mean of the values in the x array. The mean of an empty array is 0.
-	 * @param x The values whose mean will be calculated
+	 * @param x The values whose mean will be calculated. Can't be null
 	 * @return the mean of the values in the x array
 	 */
+	@Exemplars(set={
+	@Exemplar(args={"null"}, ee="NullPointerException"),
+	@Exemplar(args={"edu/wright/cs/birg/test/ArrayUtils.emptyFloat()"}, expect="0.0"),
+	@Exemplar(args={"[pa:1.0f]"}, expect="1.0"),
+	@Exemplar(args={"[pa:1.0f,2.5f]"}, expect="1.75"),
+	})
 	private static double mean(float[] x) {
 		double sum = 0;
 		for(float val:x){
 			sum += val;
 		}
-		return sum/x.length;
+		if(x.length == 0){ 
+			return 0;
+		}else{
+			return sum/x.length;
+		}
 	}
 }
