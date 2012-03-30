f960e02603506d4b5dad3050e89a71c79228da38
Eric David Moyer <eric_moyer@yahoo.com>
2012-03-30 19:13:18 -0400

    Added code to make geninstances not regenerate instances already extant in the instances directory.  Makes it run much faster.

--- dont_distribute/mic/mic_distr/geninstances.sh
+++ dont_distribute/mic/mic_distr/geninstances.sh
@@ -2,5 +2,8 @@
 mkdir -p instances
 rels=`java -jar distr.jar listrelations | tail -n+2 | cut -f2`
 for r in $rels; do
-    java -jar distr.jar geninstance $r 1000 0.01 0.01 | tail -n+2 > instances/$r.1000.txt
+    fn=instances/$r.1000.txt
+    if [ ! -e $fn ] ; then
+	java -jar distr.jar geninstance $r 1000 0.01 0.01 | tail -n+2 > $fn
+    fi
 done
