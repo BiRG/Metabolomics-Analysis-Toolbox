e3eea73f3c9afaae7d1bc118cc8b077e7021b66f
Eric David Moyer <eric_moyer@yahoo.com>
2012-04-07 18:32:32 -0400

    Added output and status printing to best_aucs.pl

--- dont_distribute/mic/mic_distr/best_aucs.pl
+++ dont_distribute/mic/mic_distr/best_aucs.pl
@@ -11,7 +11,7 @@ if(@ARGV != 1){
 	"Writes a file consisting of tab-separated entries with the bin\n",
 	"field of the filenames from the input removed and the best auc\n",
 	"and the bin portion separated by tabs\n",
-	"\n"
+	"\n",
 	"Note that this is a hack that depends on the ROC files having a \n",
 	"specific filename format. It is designed so Mathematica can quickly\n",
 	"stitch the two parts together to get the metadata back out of the\n",
@@ -41,4 +41,23 @@ while(<STDIN>){
     }else{
 	$best{$truncatedFilename}=[$endOfFilename,$auc];
     }
+    ++$numProcessed;
+    if($numProcessed % 1000 == 0){
+	print $status 
+	    $pid," processed ",$numProcessed, " entries in ",
+	    time-$startTime," seconds.\n";
+    }
 }
+
+for my $key (sort keys %best){
+    my $cur=$best{$key};
+    my $bin = $$cur[0];
+    my $auc = $$cur[1];
+    print STDOUT
+	$key,"\t",$bin,"\t",$auc,"\n";
+}
+
+print $status 
+    $pid," finished processing ",$numProcessed, " entries in ",
+    time-$startTime," seconds.\n";
+close $status;
