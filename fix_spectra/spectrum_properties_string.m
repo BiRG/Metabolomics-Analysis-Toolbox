d9df39478bc9869a0d7d293add66f84a360e591a
Eric David Moyer <eric_moyer@yahoo.com>
2012-05-15 16:51:42 -0400

    Caused spectrum properties string to ignore the type, description and processing log fields.

--- fix_spectra/spectrum_properties_string.m
+++ fix_spectra/spectrum_properties_string.m
@@ -1,6 +1,8 @@
 function str = spectrum_properties_string( collection, index )
 % Return a string describing the properties of spectrum index in collection
 %
+%
+% Ignores the type', 'description', and 'processing_log' properties.
 % -------------------------------------------------------------------------
 % Input arguments
 % -------------------------------------------------------------------------
@@ -36,24 +38,32 @@ function str = spectrum_properties_string( collection, index )
 % Eric Moyer (May 2012) eric_moyer@yahoo.com
 %
 fns = collection.formatted_input_names;
-out = cell(length(fns),1);
+ignore={'type', 'description', 'processing_log'};
+num_ignored = sum(cellfun(@(in) any(strcmp(in, fns)), ignore));
+out = cell(length(fns)-num_ignored,1);
 num_spectra = collection.num_samples;
 cur_idx = 1;
 
 for i = 1:length(fns)
     fn = fns{i};
     
-    value = collection.(fn);
-    if length(value) == num_spectra
-        if iscell(value)
-            value = value{index};
-        else
-            value = value(index);
+    if ~any(strcmp(ignore, fn)) %If we are not ignoring the given fieldname
+        value = collection.(fn);
+        if length(value) == num_spectra
+            if iscell(value)
+                value = value{index};
+            else
+                value = value(index);
+            end
+            if isnumeric(value) && numel(value) == 1
+                value = sprintf('%g', value);
+            else
+                value = to_str(value);
+            end
         end
-        value = to_str(value);
+        out{cur_idx}=sprintf('%s:\n%s\n\n', collection.input_names{i}, value);
+        cur_idx = cur_idx + 1;
     end
-    out{cur_idx}=sprintf('%s:\n%s\n\n', collection.input_names{i}, value);
-    cur_idx = cur_idx + 1;
 end
 
 str = [out{:}];
