087e7945aabebecf1a266517b6934e375c7cc906
Eric David Moyer <eric_moyer@yahoo.com>
2012-05-16 16:36:12 -0400

    Put my refactored code into the middle of the hartigan demo application (to give a bit more confidence that it worked as I expected)

--- lib/hartigan_dip/hartigansdipsigniftest.m
+++ lib/hartigan_dip/hartigansdipsigniftest.m
@@ -13,13 +13,7 @@ function		[dip, p_value, xlow,xup]=hartigansdipsigniftest(xpdf,nboot)
 N=length(xpdf);
 
 % calculate a bootstrap sample of size NBOOT of the dip statistic for a uniform pdf of sample size N (the same as empirical pdf)
-boot_dip=[];
-for i=1:nboot
-   unifpdfboot=sort(unifrnd(0,1,1,N));
-   [unif_dip]=hartigansdiptest(unifpdfboot);
-   boot_dip=[boot_dip; unif_dip];
-end;
-boot_dip=sort(boot_dip);
+boot_dip=hartigansdipofuniform(nboot, N);
 p_value=sum(dip<boot_dip)/nboot;
 
 % % Plot Boot-strap sample and the DIP statistic of the empirical pdf
