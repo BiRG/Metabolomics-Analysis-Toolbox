af2c2587a4dab05013025e0ee42a50abb678efc2
Eric David Moyer <eric_moyer@yahoo.com>
2012-07-27 19:31:33 -0400

    Wrote, documented, and tested normalization_error.m

--- dont_distribute/norm_ref_spec_xprmnts/SimSpec/normalization_error.m
+++ dont_distribute/norm_ref_spec_xprmnts/SimSpec/normalization_error.m
@@ -1,7 +1,83 @@
-function [rmse, rmse_log] = normalization_error( a, b )
-%NORMALIZATION_ERROR stub
-rmse = 0; 
-rmse_log = 0;
+function [rmse, rmse_log] = normalization_error( d1, d2 )
+% Calculates the error between two sets of dilution coefficients using two different error metrics
+%
+% d1 and d2 are the dilution constants calculated by two different methods.
+% One of them is the true set of dilution factors. Sets of dilution factors
+% are unique only up to a constant multiple. So normalization_error will 
+% regress d2 as a linear function of d1 (with 0 intercept) then calculate 
+% the rmse (root mean squared error) of the estimate. 
+%
+% Then because dilution error is really better measured on a log/log scale, 
+% the code next regresses log(d2) as constant+log(d1) and calculates the  
+% rmse of that estimate as rmse_log
+% 
+% -------------------------------------------------------------------------
+% Input arguments
+% -------------------------------------------------------------------------
+% 
+% d1 - row vector of scalars. Each dilution factor must be greater than 0. 
+%      Same size as d2
+%
+% d2 - row vector of scalars. Each dilution factor must be greater than 0. 
+%      Same size as d1
+%
+% -------------------------------------------------------------------------
+% Output parameters
+% -------------------------------------------------------------------------
+% 
+% rmse     - the minimum rmse between d1 and d2 when adjusting for the
+%            constant of proportionality. See the description for more
+%            details.
+%
+% rmse_log - the minimum rmse between log(d1) and log(d2) when adjusting
+%            for the (now additive) constant of proportionality. See the
+%            description for more details.
+%
+% -------------------------------------------------------------------------
+% Examples
+% -------------------------------------------------------------------------
+%
+% >> [a,b]=normalization_error([1,2,3,4,5],[10,20,30,40,50])
+%
+% a == 0
+% 
+% b == 8.8818e-16
+%
+% >> [a,b]=normalization_error([1,2,3,4,5],[11,29,31,49,51])
+%
+% a == 9.6352
+% 
+% b == 0.2923
+% 
+% -------------------------------------------------------------------------
+% Authors
+% -------------------------------------------------------------------------
+%
+% Eric Moyer (July 2012) eric_moyer@yahoo.com
+%
+assert(size(d1,1) == 1 && size(d2,1) == 1,'normalization_error:row_vec', ...
+    'The values passed to normalization_erro must be row vectors');
+
+assert(size(d1,2) == size(d2,2), 'normalization_error:same_size', ...
+    ['The two vectors passed to normalization_error must have the' ...
+    'same size.']);
+   
+
+% Calculate minimum squared error coefficient for regression between d1 and
+% d2. (a' x = b'    =>   a a' x = a b'    =>    x = a b' / a a' )
+coef = (d1*d1') \ (d1*d2');
+
+% Use the coefficient to calculate the error
+est = d1' * coef;
+rmse = sqrt(sum((est-d2').^2)); 
+
+% Calculate the minimum squared error coefficient for regression between
+% log(d1) and log(d2):
+%
+% log(d1 x) = log d2   =>  log(d1) + log x = log(d2)  => log x = log(d2) - log(d1)
+pf=polyfit(log(d1),log(d2)-log(d1),0);
+est=log(d1)+polyval(pf, log(d1));
+rmse_log = sqrt(sum((est-log(d2)).^2)); 
 
 end
 
---
+++ dont_distribute/norm_ref_spec_xprmnts/SimSpec/test_normalization_error.m
@@ -0,0 +1,37 @@
+function test_suite = test_normalization_error%#ok<STOUT>
+%matlab_xUnit tests excercising normalization_error
+%
+% Usage:
+%   runtests test_normalization_error
+initTestSuite;
+
+function testNoErrorCase%#ok<DEFNU>
+% Does it give no error when there is an exact multiple? 
+
+[rmse,rmse_log]=normalization_error([1,2,3,4,5,6,7,8,9,10],...
+    [10,20,30,40,50,60,70,80,90,100]);
+assertElementsAlmostEqual([rmse, rmse_log], [0,0]);
+
+function testErrorCase %#ok<DEFNU>
+% Test a hand-calculated case where the multiplication coeficient is 10 but
+% there have been errors of +/- 10% added.
+[rmse,rmse_log]=normalization_error([1,2,3,4,5,6,7,8,9,10],...
+    [ 9 18 33 44 45 54 63 88 99 110]);
+assertElementsAlmostEqual([rmse, rmse_log], ...
+    [17.9609967039,0.317288228655]);
+
+function testExceptionRowVec %#ok<DEFNU>
+% Ensure that an exception is thown when one of the input arguments is not 
+% a row vector
+assertExceptionThrown(@() normalization_error([1;2;3],[1,2,3]), ...
+    'normalization_error:row_vec');
+assertExceptionThrown(@() normalization_error([1,2,3],[1;2;3]), ...
+    'normalization_error:row_vec');
+assertExceptionThrown(@() normalization_error([1;2;3],[1;2;3]), ...
+    'normalization_error:row_vec');
+
+function testExceptionSameSize %#ok<DEFNU>
+% Ensure that an exception is thown when the input arguments have different
+% sizes
+assertExceptionThrown(@() normalization_error([1,3],[1,2,3]), ...
+    'normalization_error:same_size');
