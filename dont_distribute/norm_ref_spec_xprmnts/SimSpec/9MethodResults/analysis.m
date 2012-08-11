856e2775c152ecc7953fe8a5008eebc59d3de319
Eric David Moyer <eric_moyer@yahoo.com>
2012-08-11 18:47:49 -0400

    Rescaled graphs to match new error limits (and removed a duplicate plot that I hadn't noticed earlier)

--- dont_distribute/norm_ref_spec_xprmnts/SimSpec/9MethodResults/analysis.m
+++ dont_distribute/norm_ref_spec_xprmnts/SimSpec/9MethodResults/analysis.m
@@ -45,11 +45,11 @@ assert(num_meth == length(res.short_method_names));
 for m=1:num_meth
     subplot(3,3,m);
     vals = d(d(:,meth_col) == m, rmse_l_col);
-    edgs = 0:0.1:40;
+    edgs = 0:0.025:9;
     centers = (edgs(1:end-1)+edgs(2:end))/2;
     counts = histc(vals, edgs);
     bar(centers, counts(1:end-1), 'FaceColor','r','EdgeColor', 'none');
-    xlim([0, 40]);
+    xlim([0, 9]);
     ylim([0, 1000]);
     xlabel('RMSE (Log)');
     ylabel('# Spec Set');
@@ -63,11 +63,11 @@ d = res.data;
 for m=1:num_meth
     subplot(3,3,m);
     vals = d(d(:,meth_col) == m & d(:,cdil_col)<= 2 & d(:,tdil_col) <=2, rmse_l_col);
-    edgs = 0:0.1:40;
+    edgs = 0:0.025:9;
     centers = (edgs(1:end-1)+edgs(2:end))/2;
     counts = histc(vals, edgs);
     bar(centers, counts(1:end-1), 'FaceColor','r','EdgeColor', 'none');
-    xlim([0, 40]);
+    xlim([0, 9]);
     ylim([0, 1000]);
     xlabel('RMSE (Log)');
     ylabel('# Spec Set');
@@ -81,11 +81,11 @@ d = res.data;
 for m=1:num_meth
     subplot(3,3,m);
     vals = d(d(:,meth_col) == m & d(:,cdil_col)> 2 & d(:,tdil_col) > 2, rmse_l_col);
-    edgs = 0:0.1:40;
+    edgs = 0:0.025:9;
     centers = (edgs(1:end-1)+edgs(2:end))/2;
     counts = histc(vals, edgs);
     bar(centers, counts(1:end-1), 'FaceColor','r','EdgeColor', 'none');
-    xlim([0, 40]);
+    xlim([0, 9]);
     ylim([0, 1000]);
     xlabel('RMSE (Log)');
     ylabel('# Spec Set');
@@ -99,11 +99,11 @@ d = res.data;
 for m=1:num_meth
     subplot(3,3,m);
     vals = d(d(:,meth_col) == m & d(:,cdil_col)<= 2 & d(:,tdil_col) == 3, rmse_l_col);
-    edgs = 0:0.1:40;
+    edgs = 0:0.025:9;
     centers = (edgs(1:end-1)+edgs(2:end))/2;
     counts = histc(vals, edgs);
     bar(centers, counts(1:end-1), 'FaceColor','r','EdgeColor', 'none');
-    xlim([0, 40]);
+    xlim([0, 9]);
     ylim([0, 450]);
     xlabel('RMSE (Log)');
     ylabel('# Spec Set');
@@ -112,34 +112,16 @@ end
 mtit(4,'Error Per Method (Control normal, treatement large)', 'xoff',0, 'yoff', 0.03);
 
 %% Looking when control is normal and treatment is extreme
-figure(4);
-d = res.data;
-for m=1:num_meth
-    subplot(3,3,m);
-    vals = d(d(:,meth_col) == m & d(:,cdil_col)<= 2 & d(:,tdil_col) == 3, rmse_l_col);
-    edgs = 0:0.1:40;
-    centers = (edgs(1:end-1)+edgs(2:end))/2;
-    counts = histc(vals, edgs);
-    bar(centers, counts(1:end-1), 'FaceColor','r','EdgeColor', 'none');
-    xlim([0, 40]);
-    ylim([0, 450]);
-    xlabel('RMSE (Log)');
-    ylabel('# Spec Set');
-    title(res.short_method_names{m});
-end
-mtit(4,'Error Per Method (Control normal, treatement large)', 'xoff',0, 'yoff', 0.03);
-
-%% Looking when control is normal and treatment is large
 figure(5);
 d = res.data;
 for m=1:num_meth
     subplot(3,3,m);
     vals = d(d(:,meth_col) == m & d(:,cdil_col)<= 2 & d(:,tdil_col) == 4, rmse_l_col);
-    edgs = 0:0.1:40;
+    edgs = 0:0.025:9;
     centers = (edgs(1:end-1)+edgs(2:end))/2;
     counts = histc(vals, edgs);
     bar(centers, counts(1:end-1), 'FaceColor','r','EdgeColor', 'none');
-    xlim([0, 40]);
+    xlim([0, 9]);
     ylim([0, 450]);
     xlabel('RMSE (Log)');
     ylabel('# Spec Set');
@@ -153,11 +135,11 @@ d = res.data;
 for m=1:num_meth
     subplot(3,3,m);
     vals = d(d(:,meth_col) == m & d(:,cdil_col)== 3 & d(:,tdil_col) <= 2, rmse_l_col);
-    edgs = 0:0.1:40;
+    edgs = 0:0.025:9;
     centers = (edgs(1:end-1)+edgs(2:end))/2;
     counts = histc(vals, edgs);
     bar(centers, counts(1:end-1), 'FaceColor','r','EdgeColor', 'none');
-    xlim([0, 40]);
+    xlim([0, 9]);
     ylim([0, 450]);
     xlabel('RMSE (Log)');
     ylabel('# Spec Set');
@@ -168,12 +150,13 @@ mtit(6,'Error Per Method (Control large, treatement normal)', 'xoff',0, 'yoff', 
 %% Looking at all combinations of control and treatment dilutions
 figure(7);
 d = res.data;
-edgs = 0:0.33:40;
+edgs = 0:0.075:9;
 centers = (edgs(1:end-1)+edgs(2:end))/2;
 colors = {[0.5,0.5,0.5],'m',[0.5,0,0],'r','y','g','b','c',[0,0,0.5]};
 for cdil = 1:4
     for tdil = 1:4
         subplot(4,4,((cdil-1)*4)+tdil);
+        hold off;
 
         for m = 1:num_meth
             vals = d(d(:,meth_col) == m & ...
@@ -182,13 +165,13 @@ for cdil = 1:4
             counts = histc(vals, edgs);
             bars=bar(centers, counts(1:end-1), 'FaceColor',colors{m},...
                 'DisplayName', res.short_method_names{m}, ...
-                'EdgeColor', 'none');
+                'EdgeColor', 'none', 'BarWidth',1);
             if m==1
                 hold on;
             end
         end
         alpha(0.5);
-        xlim([0, 40]);
+        xlim([0, 9]);
         ylim([0, 350]);
         xlabel('RMSE (Log)');
         ylabel('# Spec Set');
@@ -201,7 +184,7 @@ mtit(7,'Affect of different dilution combinations on different methods', 'xoff',
 %% Looking at all combinations of control and treatment dilutions, but ignoring the prob histogram matching
 figure(8);
 d = res.data;
-edgs = 0:0.125:12;
+edgs = 0:0.0625:6;
 centers = (edgs(1:end-1)+edgs(2:end))/2;
 colors = {[0.5,0.5,0.5],'m',[0.5,0,0],'r','y','g','b'};
 bar_objs = cell(1,16);
@@ -225,7 +208,7 @@ for cdil = 1:4
             end
         end
         alpha(0.5);
-        xlim([0, 12]);
+        xlim([0, 6]);
         ylim([0, 500]);
         xlabel('RMSE (Log)');
         ylabel('# Spec Set');
@@ -233,7 +216,7 @@ for cdil = 1:4
     end
 end
 hold off;
-mtit(8,'Affect of different dilution combinations on different methods', 'xoff',0, 'yoff', 0.03);
+mtit(8,'Affect of different dilution combinations on different methods (ignoring 2 worst)', 'xoff',0, 'yoff', 0.03);
 
 %% Look only at summary statistics for the different dilutions rather than plotting full pdfs (still ignore bad methods)
 figure(9);
@@ -250,7 +233,7 @@ for cdil = 1:4
             d(:,cdil_col) == cdil &...
             d(:,tdil_col) == tdil, rmse_l_col);
         boxplot(vals,groups,'notch','on');
-        ylim([0, 10]);
+        ylim([0, 3]);
         xlabel('Method');
         ylabel('RMSE (Log)');
         title(sprintf('c:%s t:%s',dil_name{cdil},dil_name{tdil}));
@@ -273,7 +256,7 @@ for num_spec_idx=1:2
     groups = d(selected, meth_col);
     vals = d(selected, rmse_l_col);
     boxplot(vals,groups,'notch','on');
-    ylim([0, 3.5]);
+    ylim([0, .82]);
     xlabel('Method');
     ylabel('RMSE (Log)');
     title(sprintf('%d Spectra',num_spec));
