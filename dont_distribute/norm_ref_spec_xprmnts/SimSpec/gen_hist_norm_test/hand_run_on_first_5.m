a3e2a484900aab30cd3fd6dadb40873d693ef413
Eric David Moyer <eric_moyer@yahoo.com>
2012-11-04 23:22:07 -0500

    Changed to doing the exhaustive search using the second spectrum as the reference so I could have a known true multiplier. I also added a plot of the multiplier suggested by sum normalization and changed to a logarithmic x axis.
    I also used fminbnd to find the optima that the typical function
    should come up with - it turned out that they were close to (but not
    the same as) the exhaustive search ones, so I left them out of the
    plot.
    
    I included png files from the resulting plots in the repo - at only
    13k apiece, it is good to have them accessible.

--- dont_distribute/norm_ref_spec_xprmnts/SimSpec/gen_hist_norm_test/hand_run_on_first_5.m
+++ dont_distribute/norm_ref_spec_xprmnts/SimSpec/gen_hist_norm_test/hand_run_on_first_5.m
@@ -125,6 +125,37 @@ end
 
 %% Now do exhaustive search accumulating the errors for the first spectrum (just to see what the curve looks like)
 
+%% First make bin boundaries and binnings treating the second spectrum as the reference spectrum
+second_spectrum = ds{2};
+min_second= min(second_spectrum);
+max_second = max(second_spectrum);
+
+%% Generate bin boundaries for second log distribution (10 bins)
+min_z = min(log(second_spectrum+1));
+max_z = max(log(second_spectrum+1));
+zbins=linspace(min_z, max_z, 11);
+second_log_bins = exp(zbins)-1;
+clear('zbins','min_z','max_z');
+second_binned_log = histc_inclusive(second_spectrum, second_log_bins);
+
+%% Generate bin boundaries for second equi-distribution (10 bins)
+sorted_second=sort(second_spectrum);
+second_equi_bins = zeros(1,11);
+second_equi_bins(1)=sorted_second(1);
+second_equi_bins(11)=sorted_second(end);
+assert(length(sorted_second)==5777); % 7 bins of 578 and 3 of 577
+bin_last_elements=[578,... 578 in first bin, so 578 is last element of first bin
+    578*2, ... 578 in second bin, so last element of second bin is 578*2
+    578*3, 578*4, 578*5, 578*6, 578*7, 578*7+577*1, 578*7+577*2, 578*7+577*3];
+bin_first_elements=[1,bin_last_elements(1:9)+1];
+inner_bin_boundaries=(sorted_second(bin_first_elements(2:end))+...
+    sorted_second(bin_last_elements(1:end-1)))/2;
+second_equi_bins(2:10)=inner_bin_boundaries;
+clear('inner_bin_boundaries','bin_first_elements','bin_last_elements','sorted_second');
+second_binned_equi = histc_inclusive(second_spectrum, second_equi_bins);
+
+%% Now do the exhaustive search
+
 % First make a list of the multipliers to make each value go to each bin
 % boundary
 values=ds{1};
@@ -134,23 +165,20 @@ for bin_edge_idx = 1:length(log_bins)-1
 end
 clear('bin_edge_idx');
 
-% Now, get rid of those multipliers that are out of range and sort the rest
+% Now, sort the multipliers and set up space for the error storage
 exhaustive_multipliers = reshape(exhaustive_multipliers,[],1);
-in_range_multipliers = exhaustive_multipliers >= log_bin_new_lb(1) & ...
-    exhaustive_multipliers <= log_bin_new_ub(1);
-exhaustive_multipliers = sort(exhaustive_multipliers(in_range_multipliers));
+exhaustive_multipliers = sort(exhaustive_multipliers);
 exhaustive_multiplier_error_log = zeros(size(exhaustive_multipliers));
 exhaustive_multiplier_error_equi = zeros(size(exhaustive_multipliers));
 num_mults = length(exhaustive_multipliers);
-clear('in_range_multipliers');
 
-% Now calculate one error value for each multiplier
+%% Now calculate one error value for each multiplier
 progress_bar=waitbar(0,sprintf('Calcd mult %d/%d %g secs remaining',0,num_mults,0));
 start_time=now;
 for mult_idx = 1:num_mults
     mult = exhaustive_multipliers(mult_idx);
-    exhaustive_multiplier_error_log(mult_idx) = multiplierErr(values, mult, log_bins, ref_log_binned); 
-    exhaustive_multiplier_error_equi(mult_idx) = multiplierErr(values, mult, equi_bins, ref_equi_binned);
+    exhaustive_multiplier_error_log(mult_idx) = multiplierErr(values, mult, second_log_bins, second_binned_log); 
+    exhaustive_multiplier_error_equi(mult_idx) = multiplierErr(values, mult, second_equi_bins, second_binned_equi);
     if mod(mult_idx, 1000)==0
         elapsed_days = now - start_time;
         elapsed_seconds = 60*60*24*elapsed_days;
@@ -162,19 +190,40 @@ for mult_idx = 1:num_mults
     end
 end
 delete(progress_bar);
+best_multiplier_log_fminbnd = fminbnd(@(mult) multiplierErr(values, mult, second_log_bins, second_binned_log), 1/10,10);
+best_multiplier_equi_fminbnd = fminbnd(@(mult) multiplierErr(values, mult, second_equi_bins, second_binned_equi), 1/10,10);
+
 clear('progress_bar', 'num_mults', 'mult_idx', 'mult', 'elapsed_days', 'elapsed_seconds', 'fraction_completed', 'seconds_remaining');
 
+%% Calculate sum normalization ratio
+sum_1=sum(first_5.Y(:,1));
+sum_2=sum(first_5.Y(:,2));
+sum_n_ratio=sum_2/sum_1;
+
 %% Plot the resulting curves with the true dilution factor
-%true_dilution_factor = real_dilution_factors{1};
-%true_dilution_factor = median(true_dilution_factor)/true_dilution_factor(1);
+true_multiplier = real_dilution_factors{1};
+true_multiplier = true_multiplier(1)/true_multiplier(2);
 figure(1);
+
 figure_min = min([exhaustive_multiplier_error_equi; exhaustive_multiplier_error_log]);
 figure_max = max([exhaustive_multiplier_error_equi; exhaustive_multiplier_error_log]);
-plot(exhaustive_multipliers, exhaustive_multiplier_error_log, exhaustive_multipliers, exhaustive_multiplier_error_equi);
-title('Comparison of equi and log binned error for exhaustive multiplier search');
-ylabel('Squared error from reference distribution');
-xlabel('Multiplier');
-legend('log binned error','equi binned error');
-%hold('on');
-%line([true_dilution_factor, true_dilution_factor],[figure_min, figure_max]);
 
+[~,best_multiplier_idx_log] = min(exhaustive_multiplier_error_log);
+best_multiplier_log = exhaustive_multipliers(best_multiplier_idx_log);
+[~,best_multiplier_idx_equi] = min(exhaustive_multiplier_error_equi);
+best_multiplier_equi = exhaustive_multipliers(best_multiplier_idx_equi);
+
+curve_handles=semilogx(exhaustive_multipliers, exhaustive_multiplier_error_log, 'b', exhaustive_multipliers, exhaustive_multiplier_error_equi, 'g');
+title('Comparison of equi and log binned error for exhaustive multiplier search');
+ylabel('Squared error from second distribution');
+xlabel('Multiplier');
+hold('on');
+line([best_multiplier_log, best_multiplier_log], [figure_min, figure_max], 'Color','b');
+line([best_multiplier_equi, best_multiplier_equi], [figure_min, figure_max], 'Color','g');
+curve_handles(3)=line([true_multiplier, true_multiplier],[figure_min, figure_max], 'Color', 'r');
+curve_handles(4)=line([sum_n_ratio, sum_n_ratio],[figure_min, figure_max], 'Color', 'k');
+
+legend(curve_handles,'log binned error','equi binned error','actual ratio', 'sum normalization ratio');
+%line([best_multiplier_log_fminbnd, best_multiplier_log_fminbnd],[figure_min, figure_max], 'Color', 'm');
+%line([best_multiplier_equi_fminbnd, best_multiplier_equi_fminbnd],[figure_min, figure_max], 'Color', 'k');
+hold('off');
