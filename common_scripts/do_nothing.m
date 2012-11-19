86da19dcf2888defaa2bef5757d15d8a557fdacc
Eric David Moyer <eric_moyer@yahoo.com>
2012-11-19 17:40:05 -0500

    Added code to do nothing (so that I can make function handles that do nothing).

---
+++ common_scripts/do_nothing.m
@@ -0,0 +1,9 @@
+function do_nothing(varargin)
+% DO_NOTHING does nothing
+%
+% A function for when you deliberately want to do nothing (a NOP). Useful
+% when you need to create a function handle to a function that does
+% nothing. A possibly faster, but less readable way to do this is 
+% my_h = @(args) [];
+end
+
