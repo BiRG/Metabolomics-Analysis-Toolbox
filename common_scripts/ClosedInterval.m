4308e44f815db3421c1a3064916c94100f52f231
Eric David Moyer <eric_moyer@yahoo.com>
2012-12-30 21:43:42 -0500

    Wrote and ran test cases for ClosedInterval and fixed the bugs revealed.

--- common_scripts/ClosedInterval.m
+++ common_scripts/ClosedInterval.m
@@ -26,56 +26,56 @@ classdef ClosedInterval
         % ClosedInterval(min, max)
         %
         % Creates a closed interval with the given miniumum and maximum
-	%
-	% All parameters must be scalars. min <= max
-	% 
+        %
+        % All parameters must be scalars. min <= max
+        % 
         % ----------------------------------------------------------------
         % Examples
         % ---------------------------------------------------------------
-	%
-	% >> ClosedInterval(-1,1)
-	%
-	% Creates a closed interval containing all points from -1
+        %
+        % >> ClosedInterval(-1,1)
+        %
+        % Creates a closed interval containing all points from -1
         % to 1, including -1 and 1 themeselves.
-	  if nargin > 0
-	    assert(nargin == 2);
-	    assert(isscalar(min));
-	    assert(isscalar(max));
-	    assert(min <= max);
+          if nargin > 0
+            assert(nargin == 2);
+            assert(isscalar(min));
+            assert(isscalar(max));
+            assert(min <= max);
 
-	    objs.min = min;
-	    objs.max = max;
-	  end
+            objs.min = min;
+            objs.max = max;
+          end
         end
-        
+
         function length=get.length(obj)
         % Getter method calculating length
-	    length = max - min;
+            length = obj.max - obj.min;
         end
         
 	
-	function does_contain = contains(obj, val)
-	% Returns true if this ClosedInterval contains val and
+        function does_contain = contains(obj, val)
+        % Returns true if this ClosedInterval contains val and
         % false otherwise. 
-	%
-	% val must be a scalar
-	  assert(isscalar(val);
-	  does_contain = min <= val && val <= max;
-	end
+        %
+        % val must be a scalar
+          assert(isscalar(val));
+          does_contain = obj.min <= val && val <= obj.max;
+        end
 
         function str=char(obj)
         % Return a human-readable string representation of this
         % object. (Matlab's version of toString, however, Matlab
         % doesn't call it automatically)
           str = sprintf('ClosedInterval(%g,%g)', obj.min, obj.max);
-	end
+        end
 	    
         function display(obj)
         % Display this object to a console. (Called by Matlab
         % whenever an object of this class is assigned to a
         % variable without a semicolon to suppress the display).
-	   disp(obj.char);
-	end
+            disp(obj.char);
+        end
     end
     
 end
---
+++ common_scripts/tests/testClosedInterval.m
@@ -0,0 +1,68 @@
+function test_suite = testClosedInterval %#ok<STOUT>
+%matlab_xUnit tests excercising ClosedInterval
+%
+% Usage:
+%   runtests testClosedInterval
+initTestSuite;
+
+function dir=data_dir
+% Return the directory in which this test case data is located
+thisfile = mfilename('fullpath');
+dir=regexprep(thisfile,'/[^/]*$','');
+
+function testConstructor %#ok<DEFNU>
+% Tests the inputs of the constructor and if it fails appropriately
+
+c=ClosedInterval(-1,1);
+assertTrue(isa(c, 'ClosedInterval'));
+assertEqual(c.min,-1);
+assertEqual(c.max, 1);
+
+d=ClosedInterval(0.57,2);
+assertTrue(isa(d, 'ClosedInterval'));
+assertEqual(d.min,0.57);
+assertEqual(d.max,2);
+
+e=ClosedInterval(0.2,0.2);
+assertTrue(isa(e, 'ClosedInterval'));
+assertEqual(e.min,0.2);
+assertEqual(e.max,0.2);
+
+f=ClosedInterval(); %Ensure default constructor is implemented
+assertTrue(isa(f, 'ClosedInterval'));
+
+f=@() ClosedInterval(1,-1);
+assertExceptionThrown(f, 'MATLAB:assert:failed');
+f=@() ClosedInterval([-1,1],1);
+assertExceptionThrown(f, 'MATLAB:assert:failed');
+f=@() ClosedInterval(2,[-1,1]);
+assertExceptionThrown(f, 'MATLAB:assert:failed');
+f=@() ClosedInterval([-1,1]);
+assertExceptionThrown(f, 'MATLAB:assert:failed');
+
+function testLength %#ok<DEFNU>
+% Tests that the length function operates as expected
+assertEqual(ClosedInterval(-1,1).length,2);
+assertEqual(ClosedInterval(0.57,2).length,2-0.57);
+assertEqual(ClosedInterval(0.2,0.2).length,0);
+
+function testContains %#ok<DEFNU>
+% Tests that the contains operations operates as expected
+assertTrue(ClosedInterval(-1,1).contains(1));
+assertTrue(ClosedInterval(-1,1).contains(-1));
+assertTrue(ClosedInterval(-1,1).contains(0.2));
+assertTrue(ClosedInterval(-1,1).contains(-0.1));
+assertFalse(ClosedInterval(-1,1).contains(1.1));
+assertFalse(ClosedInterval(-1,1).contains(-100));
+
+assertTrue(ClosedInterval(3.444,7.221).contains(7.221));
+assertTrue(ClosedInterval(3.444,7.221).contains(3.444));
+assertTrue(ClosedInterval(3.444,7.221).contains(3.6));
+assertTrue(ClosedInterval(3.444,7.221).contains(4));
+assertFalse(ClosedInterval(3.444,7.221).contains(8.212));
+assertFalse(ClosedInterval(3.444,7.221).contains(-7.22500));
+
+function testChar %#ok<DEFNU>
+assertEqual(ClosedInterval(-1,1).char,'ClosedInterval(-1,1)');
+assertEqual(ClosedInterval(0.57,2).char,'ClosedInterval(0.57,2)');
+assertEqual(ClosedInterval(0.2,0.2).char,'ClosedInterval(0.2,0.2)');
