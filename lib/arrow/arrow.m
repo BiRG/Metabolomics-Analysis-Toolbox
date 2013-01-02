9f292ab0a00140732d5476e86830d3d34362244c
Eric David Moyer <eric_moyer@yahoo.com>
2013-01-02 12:39:02 -0500

    Added a message id to the arrow axis limits warning so it can be turned off.

--- lib/arrow/arrow.m
+++ lib/arrow/arrow.m
@@ -961,7 +961,7 @@ if (nargout<=1),
 		oldaxlims(mask,6:7) = lims(5:6,mask)';
 		ARROW_AXLIMITS = oldaxlims(find(any(oldaxlims(:,2:7)'~=lims)),:);
 		if ~isempty(ARROW_AXLIMITS),
-			warning(arrow_warnlimits(ARROW_AXLIMITS,narrows));
+			warning('arrow:axis_limits_changed', arrow_warnlimits(ARROW_AXLIMITS,narrows));
 		end;
 	end;
 else,
