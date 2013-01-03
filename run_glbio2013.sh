58ac0e504c0b054475953b60a3263e497011bb1b
Eric David Moyer <eric_moyer@yahoo.com>
2013-01-03 01:14:24 -0500

    Added script to run one process for the GLBIO2013 experiment.

---
+++ run_glbio2013.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+if [ $# -lt 2 ]; then
+    echo -e "Usage: $0 num_reps session_num(up to 64)\n";
+    exit;
+fi
+if [ $1 -lt 1 ]; then
+    echo -e "You must include at least 1 repetition\n";
+    exit;
+fi
+if [ $2 -gt 64 -o $2 -lt 1 ]; then
+    echo -e "The session number must be 1-64\n";
+    exit;
+fi
+
+#Usage: 
+unset DISPLAY
+# IMPORTANT: DONOT indent any of the below statements
+matlab -nodesktop -singleCompThread  > glbio2013_run_$2_terminal_output <<MATLAB_ENV
+% The below works like a script m-file between MATLAB_ENVs
+cd('targeted_deconv/QuantMetabMap');
+warning('off','MATLAB:RandStream:GetDefaultStream');
+warning('off','MATLAB:RandStream:SetDefaultStream');
+tic; results_$2=GLBIO2013_run_experiment($1,$2,64,false); toc
+save('glbio2013_run_$2_results.mat', 'results_$2');
+exit                      % don't forget to exit
+MATLAB_ENV
+# keep this line to ensure newline
+
+
