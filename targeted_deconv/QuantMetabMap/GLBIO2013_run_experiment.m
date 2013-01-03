8726d25f82074247aa83c605bf82f7807ac21403
Eric David Moyer <eric_moyer@yahoo.com>
2013-01-03 00:37:19 -0500

    Added option to exclude waitbar and have command-line status instead.

--- targeted_deconv/QuantMetabMap/GLBIO2013_run_experiment.m
+++ targeted_deconv/QuantMetabMap/GLBIO2013_run_experiment.m
@@ -1,7 +1,7 @@
-function results = GLBIO2013_run_experiment(num_spectra_per_width, instance_number, total_number_of_instances )
+function results = GLBIO2013_run_experiment( num_spectra_per_width, instance_number, total_number_of_instances, use_waitbar )
 % Return an array of GLBIO2013Datum objects from processing num_spectra_per_width spectra for each of the desired widths
 % 
-% Usage: GLBIO2013_run_experiment( num_spectra_per_width, seed )
+% Usage: GLBIO2013_run_experiment( num_spectra_per_width, instance_number, total_number_of_instances, use_waitbar )
 %
 % num_spectra_per_width - the number of spectra to generate for each 
 %        spectral width used in the experiment.
@@ -17,6 +17,8 @@ function results = GLBIO2013_run_experiment(num_spectra_per_width, instance_numb
 %        generating the random number streams. May exceed but not 
 %        undershoot the true number.
 %
+% use_waitbar - (logical scalar) if true, displays a waitbar. If not, 
+%               spams the terminal with status updates.
 original_default_stream = RandStream.getDefaultStream;
 seed = 925040534; % A 32 bit number from random.org
 my_stream = RandStream.create('mrg32k3a', ...
@@ -26,7 +28,9 @@ RandStream.setDefaultStream(my_stream);
 
 
 
-wait_h = waitbar(0,sprintf('Instance "%d"> initializing: MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM',instance_number));
+if use_waitbar
+   wait_h = waitbar(0,sprintf('Instance "%d"> initializing: MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM',instance_number));
+end
 widths_in_mean_peak_width = [5.75, 26.7855781172538272, 37.8140358572843098, 50.2757392223216968, 65.69707458955628, 86.6618791229460896, 116.956430245217931, 167.919406041352318, 267.692156378955815, 563.312931020470387];
 widths_in_mean_peak_width = sort(widths_in_mean_peak_width, 2, 'descend'); % Do largest widths first so time estimate always errs on the side of overestimate
 widths_in_ppm = widths_in_mean_peak_width .* 0.00453630122481774988;
@@ -39,19 +43,32 @@ for rep = 1:num_spectra_per_width
     for width_idx = 1:length(widths_in_ppm)
         width = widths_in_ppm(width_idx);
         spectra_remaining = final_num_spectra - num_completed_spectra;
-        waitbar(num_completed_spectra/final_num_spectra, wait_h, sprintf(...
-            'Instance %d> rep: %d width: %6.4f (%d of %d) time remaining: %d +/- %d minutes, %d elapsed', ...
-            instance_number, rep, width, width_idx, length(widths_in_ppm), ...
-            round(spectra_remaining*mean(spectrum_times)), ...
-            round(spectra_remaining*std(spectrum_times)), ...
-            round(sum(spectrum_times))));
+	update_format_string = 'Instance %d> rep: %d width: %6.4f (%d of %d) time remaining: %d +/- %d minutes, %d elapsed';
+	if use_waitbar
+           waitbar(num_completed_spectra/final_num_spectra, wait_h, sprintf(...
+              update_format_string, ...
+              instance_number, rep, width, width_idx, length(widths_in_ppm), ...
+              round(spectra_remaining*mean(spectrum_times)), ...
+              round(spectra_remaining*std(spectrum_times)), ...
+              round(sum(spectrum_times))));
+	else
+           fprintf(...
+              ['%6.2f%%: ', update_format_string,'\n'], ...
+	      100*num_completed_spectra/final_num_spectra...
+              instance_number, rep, width, width_idx, length(widths_in_ppm), ...
+              round(spectra_remaining*mean(spectrum_times)), ...
+              round(spectra_remaining*std(spectrum_times)), ...
+              round(sum(spectrum_times))));
+	end
         cur_spectrum_time = tic;
         results(num_completed_spectra+1) = GLBIO2013Datum(width);
         num_completed_spectra = num_completed_spectra + 1;
         spectrum_times = [spectrum_times, toc(cur_spectrum_time)/60]; %#ok<AGROW>
     end
 end
-close(wait_h);
+if use_waitbar
+   close(wait_h);
+end
 
 
 RandStream.setDefaultStream(original_default_stream);
