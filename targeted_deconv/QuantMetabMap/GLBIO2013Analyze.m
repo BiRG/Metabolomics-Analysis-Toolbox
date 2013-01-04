d76a58f2dd1ebff45267778e44def3c09bde241b
Eric Moyer <eric_moyer@yahoo.com>
2013-01-04 18:22:37 -0500

    Added code to calcuate whether significant improvement exists under particular special cases.

--- targeted_deconv/QuantMetabMap/GLBIO2013Analyze.m
+++ targeted_deconv/QuantMetabMap/GLBIO2013Analyze.m
@@ -6,7 +6,9 @@ load('glbio2013_combined_raw_results.mat');
 %% Calculate the parameters
 pe_list = GLBIO2013_calc_param_error_list(glbio_combined_results);
 
-%% Does an improvement exist independent of any other measure? The histogram suggests yes
+%% Does an improvement exist independent of where we look? 
+% The histogram suggests yes. And a paired t-test gives an unbelieveably
+% low p-value (an actual 0 in double precision).
 clf;
 hist([pe_list.error_diff],100);
 title('Histogram of all parameter improvements for all peak picking methods and all parameters');
@@ -15,3 +17,166 @@ ylabel('Number of (deconvolution,parameter) pairs');
 [~,p_value] = ttest([pe_list.mean_error_anderson],[pe_list.mean_error_summit],0.05,'right');
 sig_box_handle = annotation('textbox',[0.5,0.5,0.2,0.2],'String', ...
     sprintf('mean difference is greater than 0: p=%.18g',p_value));
+
+%% Plot by parameter
+% Now we break up the results into one graph for each parameter. Each
+% graph has 3 lines - one per peak-picking method and gives the improvement
+% as a function of spectrum crowding.
+%
+% The graphs show that the improvement decreases as the crowding increases.
+% They also show that the smoothed local max peak picking algorithm has
+% lower improvement - probably because the algorithms are trying to fit an
+% incorrect model with too few peaks and the error due to the incorrect 
+% model is greater on average than the error due to the improper starting
+% point.
+%
+% _The code below also calculates an matrix of the improvement values
+% ordered by the properties of interest (parameter, picker, and
+% crowdedness).
+clf;
+param_names = unique({pe_list.parameter_name});
+assert(length(param_names) == 4,'Right number of param names');
+peak_pickers = unique({pe_list.peak_picking_name});
+picker_legend = {'Gold Standard','Noisy Gold Standard', 'Smoothed Local Max'};
+picker_formats = {'r+-','bd-','*k-'};
+assert(length(picker_legend) == length(peak_pickers),'Right # picker legend entries');
+assert(length(picker_formats) == length(peak_pickers),'Right # picker formats');
+collision_probs = unique([pe_list.collision_prob]);
+pe_values_per_triple = length(pe_list)/length(param_names)/length(peak_pickers)/length(collision_probs);
+improvements = zeros(length(param_names),length(peak_pickers),length(collision_probs),pe_values_per_triple);
+for param_idx = 1:length(param_names)
+    subplot(2,2,param_idx);
+    title_tmp = param_names{param_idx};
+    title([upper(title_tmp(1)),title_tmp(2:end)]);
+    xlabel('Probability of peak collision');
+    ylabel('Mean improvement');
+    hold on;
+    pe_has_param = strcmp({pe_list.parameter_name},param_names{param_idx});
+    handle_for_picker = zeros(1,length(peak_pickers));
+    for picker_idx = 1:length(peak_pickers)
+        pe_has_picker = strcmp({pe_list.peak_picking_name},peak_pickers{picker_idx});
+        mean_error_for_prob = zeros(1,length(collision_probs));
+        std_dev_error_for_prob = zeros(1,length(collision_probs));
+        for prob_idx = 1:length(collision_probs)
+            pe_has_prob = [pe_list.collision_prob] == collision_probs(prob_idx);
+            selected_pes = pe_list(pe_has_prob & pe_has_picker & pe_has_param);
+            selected_diffs = [selected_pes.error_diff];
+            improvements(param_idx, picker_idx, prob_idx,:) = selected_diffs;
+            mean_error_for_prob(prob_idx) = mean(selected_diffs);
+            std_dev_error_for_prob(prob_idx) = std(selected_diffs);
+        end
+        handle_for_picker(picker_idx) = plot(collision_probs, ...
+            mean_error_for_prob, picker_formats{picker_idx});
+%         handle_for_picker(picker_idx) = errorbar(collision_probs, ...
+%             mean_error_for_prob, std_dev_error_for_prob, ...
+%             picker_formats{picker_idx});
+    end
+    if param_idx == 3
+        legend(handle_for_picker, picker_legend, 'location','Best');
+    end
+end
+
+%% For which values is there a difference?
+% I do multiple t-tests using a holm-bonferroni correction to see which
+% values there is evidence of a significant improvement and a second set 
+% of tests to see where there is evidence of a significant detriment. I
+% use a 0.05 as a significance threshold.
+%
+% There was significant improvement in all cases except for some of the 
+% smoothed local maximum peak-picking. There was no significant worsening 
+% for any combination of parameters.
+%
+% For the smoothed local maximum peak picking, the test did not detect
+% improvement for all but the least crowded bin of the lorentzianness,
+% for all but the second most crowded of the location parameter, and for
+% 0.6,0.7, and 1.0 collision probabilities of the height parameter.
+
+improvement_p_vals = zeros(length(param_names),length(peak_pickers),length(collision_probs));
+detriment_p_vals = improvement_p_vals;
+for param_idx = 1:length(param_names)
+    for picker_idx = 1:length(peak_pickers)
+        for prob_idx = 1:length(collision_probs)
+            diffs = improvements(param_idx, picker_idx, prob_idx,:);
+            [~, improvement_p_vals(param_idx, picker_idx, prob_idx)] = ttest(diffs,0,0.05,'right');
+            [~, detriment_p_vals(param_idx, picker_idx, prob_idx)] = ttest(diffs,0,0.05,'left');
+        end
+    end
+end
+
+% Correct the p-values
+improvement_p_vals_corrected = improvement_p_vals;
+improvement_p_vals_corrected(:) = bonf_holm(improvement_p_vals(:),0.05);
+detriment_p_vals_corrected = detriment_p_vals;
+detriment_p_vals_corrected (:) = bonf_holm(detriment_p_vals(:),0.05);
+
+fprintf('Peak Property            |Peak Picking Method   |Prob. Collision|Sig. Improved?     |Sig. Worsened?     \n');
+for param_idx = 1:length(param_names)
+    for picker_idx = 1:length(peak_pickers)
+        for prob_idx = 1:length(collision_probs)
+            i = improvement_p_vals_corrected(param_idx, picker_idx, prob_idx);
+            if i >= 0.05
+                istr = '?  ??  ?';
+            else
+                istr = 'Improved';
+            end
+            d = detriment_p_vals_corrected(param_idx, picker_idx, prob_idx);
+            if d >= 0.05
+                dstr = '?  ??  ?';
+            else
+                dstr = 'Worsened';
+            end
+            
+            fprintf('%25s %22s %15.1f %8s p=%8.3g %8s p=%8.3g\n', ...
+                param_names{param_idx}, peak_pickers{picker_idx}, ...
+                collision_probs(prob_idx), istr, i, dstr, d);
+        end
+    end
+end
+
+%% For what values might there be a difference if we didn't have the multiple test correction?
+% Here I plot the same table, but without the multiple values correction.
+% The results are similar, but there are several changes for the smoothed
+% local maximum peak picking. Now, the lorentzianness parameter worsens 
+% in two additional cases (0.5 and 0.7), but improves in one additional 
+% case (0.2).  The location parameter improves in three additional cases
+% (0.1, 0.3, and 0.5).
+%
+% To me, this suggests that we might be able to tease significance out of
+% these results by doing more trials.
+%
+% If I do more trials, I'd like to also include a few more
+% noisy-gold-standard peaks with larger standard deviations to see how
+% robust the algoritms are to that. I think my current noisy gold-standard
+% is likely too nice.
+fprintf('Peak Property            |Peak Picking Method   |Prob. Collision|Sig. Improved?     |Sig. Worsened?     \n');
+for param_idx = 1:length(param_names)
+    for picker_idx = 1:length(peak_pickers)
+        for prob_idx = 1:length(collision_probs)
+            i = improvement_p_vals(param_idx, picker_idx, prob_idx);
+            if i >= 0.05
+                istr = '?  ??  ?';
+            else
+                istr = 'Improved';
+            end
+            d = detriment_p_vals(param_idx, picker_idx, prob_idx);
+            if d >= 0.05
+                dstr = '?  ??  ?';
+            else
+                dstr = 'Worsened';
+            end
+            
+            fprintf('%25s %22s %15.1f %8s p=%8.3g %8s p=%8.3g\n', ...
+                param_names{param_idx}, peak_pickers{picker_idx}, ...
+                collision_probs(prob_idx), istr, i, dstr, d);
+        end
+    end
+end
+
+%% How robust is each algorithm to location errors?
+% Here, I look at each peak in the noisy gold standard data, the
+% distance of that peak from its corresponding initial peak, and the
+% distance of the corresponding deconvolved peak parameters from the true
+% values.
+%
+% I then plot for each peak parameter, a scatter plot of initial location
+% error versus final difference for that peak parameter.
