ee71a154df631995a06022b9bb928dc57705b433
Eric Moyer <eric_moyer@yahoo.com>
2013-02-19 18:35:05 -0500

    Added two more test cases (both passing) for some edge cases for the by_fields_listbox routine.

--- common_scripts/tests/test_by_fields_listbox.m
+++ common_scripts/tests/test_by_fields_listbox.m
@@ -37,4 +37,34 @@ assertEqual(c.bar, collection.bar);
 assertEqual(c.baz, collection.baz);
 assertEqual(collection.value, {'1, 1'  '100, 2'  '10, 3'  '100, 4'  '2, 5'});
 
+function testWithEmptySlice %#ok<DEFNU>
+% Tests whether correct projection is achieved when the list of fields is
+% empty
+
+c.foo=[1,100,10,100,2];
+c.bar={'human','rat','rat','rat','human'};
+c.baz=[1,2,3,4,5];
+[sorted_fields_str,group_by_inxs,inxs,collection]=by_fields_listbox(c, {});
+assertEqual(sorted_fields_str, cell(0,1));
+assertEqual(group_by_inxs, {});
+assertEqual(inxs, zeros(0,1));
+assertEqual(c.foo, collection.foo);
+assertEqual(c.bar, collection.bar);
+assertEqual(c.baz, collection.baz);
+assertEqual(collection.value, {});
+
+function testWithSingleField %#ok<DEFNU>
+% Tests whether correct projection is achieved when there is only one field
+
+c.foo=[1,100,10,100,2];
+c.bar={'human','rat','rat','rat','human'};
+c.baz=[1,2,3,4,5];
+[sorted_fields_str,group_by_inxs,inxs,collection]=by_fields_listbox(c, {'bar'});
+assertEqual(sorted_fields_str, {'human'    'rat'});
+assertEqual(group_by_inxs, { [1,5]    [2, 3, 4]});
+assertEqual(inxs, [1,2]);
+assertEqual(c.foo, collection.foo);
+assertEqual(c.bar, collection.bar);
+assertEqual(c.baz, collection.baz);
+assertEqual(collection.value,  {'human'  'rat'  'rat'  'rat'  'human'});
 
