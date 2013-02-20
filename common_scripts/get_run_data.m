0bad2d6d1e29240e54971560a9d3a81cabd944a3
Eric Moyer <eric_moyer@yahoo.com>
2013-02-19 22:11:17 -0500

    Added some comments explaining the paired_by_inxs variable and clarified the text printed when no matching sample is found for paring.

--- common_scripts/get_run_data.m
+++ common_scripts/get_run_data.m
@@ -56,6 +56,14 @@ for g = 1:length(model_by_inxs)
 end
 model_by_inxs = new_model_by_inxs;
 
+% paired_by_inxs is a cell array of double vectors. paired_by_inxs{i}
+% contains a list of the samples in the current collection that all have 
+% the value specified by the i'th entry in the paired_by_listbox.
+%
+% The next code chooses only those indices that were selected. So that
+% hereafter paired_by_inxs contains the list of samples which have one of
+% the values selected in the paired_by listbox when considering only the
+% fields in the paired_by_fields listbox.
 paired = false;
 try
     paired_by_inxs = handles.paired_by_inxs;
@@ -120,7 +128,7 @@ if paired % Pair up the data
                 end
             end
             if ~found
-                fprintf('Could not find a match for sample %d at time %d classified as "%s"\n', ...
+                fprintf('Could not find a match for sample with subject id %d at time %d classified as "%s"\n', ...
                     collection.subject_id(inx_unpaired), ...
                     collection.time(inx_unpaired), ...
                     collection.classification{inx_unpaired});
