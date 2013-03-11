30dbfbd5fa43b21157e65ac67bf15a0d59f4e19f
Eric Moyer <eric_moyer@yahoo.com>
2013-03-11 15:13:52 -0400

    Drastic modifications to the data_density_plot code - it now plots a 3d histogram (2 independent variables) with heights represented by colors and does not use an interactive mode or a new figure for plotting. The plot command now takes height, width, and limit arguments. The plots are now right-side-up for math plotting (none of this inverted y axis). I eliminated the fudge parameter to the dataDensity function. The dataDensity function now returns counts in equally-speced bins rather than a distance-based density estimator.
    Performance could be made n log n now rather than n^2 by sorting on
    each coordinate separately. Not going to bother, however.

--- lib/data_density_plot/DataDensityPlot.m
+++ lib/data_density_plot/DataDensityPlot.m
@@ -1,22 +1,45 @@
-function [ f ] = DataDensityPlot( x, y, levels )
+function DataDensityPlot( x, y, levels, limits, width, height )
 %DATADENSITYPLOT Plot the data density 
 %   Makes a contour map of data density
 %   x, y - data x and y coordinates
 %   levels - number of contours to show
+%   limits - [xmin xmax ymin ymax] - defaults to data max/min
+%   width  - width in pixels - number of horizontal bins - defaults to 256
+%   height - height in pixels - number of vertical bins - defaults to 256
 %
 % By Malcolm Mclean
 %
-    map = dataDensity(x, y, 256, 256);
+% Modified 11 Mar 2013 by Eric Moyer
+%
+
+    if ~exist('limits', 'var')
+        limits(1) = min(x);
+        limits(2) = max(x);
+        limits(3) = min(y);
+        limits(4) = max(y);
+    end
+    
+    if ~exist('width','var')
+        width = 256;
+    end
+
+    if ~exist('height','var')
+        height = 256;
+    end
+    
+    map = dataDensity(x, y, width, height, limits);
+    map = flipud(map);
+
+    density_min = min(min(map));
+    density_max = max(max(map));
     map = map - min(min(map));
     map = floor(map ./ max(max(map)) * (levels-1));
-    f = figure();
     
     image(map);
     colormap(jet(levels));
-    set(gca, 'XTick', [1 256]);
-    set(gca, 'XTickLabel', [min(x) max(x)]);
-    set(gca, 'YTick', [1 256]);
-    set(gca, 'YTickLabel', [min(y) max(y)]);
-    uiwait;
+    set(gca, 'XTick', [1 width]);
+    set(gca, 'XTickLabel', limits(1:2));
+    set(gca, 'YTick', [1 height]);
+    set(gca, 'YTickLabel', limits(4:-1:3));
 end
 
--- lib/data_density_plot/dataDensity.m
+++ lib/data_density_plot/dataDensity.m
@@ -1,11 +1,12 @@
-function [ dmap ] = dataDensity( x, y, width, height, limits, fudge )
+function [ dmap ] = dataDensity( x, y, width, height, limits)
 %DATADENSITY Get a data density image of data 
 %   x, y - two vectors of equal length giving scatterplot x, y co-ords
 %   width, height - dimensions of the data density plot, in pixels
 %   limits - [xmin xmax ymin ymax] - defaults to data max/min
-%   fudge - the amount of smear, defaults to size of pixel diagonal
 %
 % By Malcolm McLean
+%
+% Radically modified by Eric Moyer on 11 March 2013
 %
     if(nargin == 4)
         limits(1) = min(x);
@@ -15,18 +16,25 @@ function [ dmap ] = dataDensity( x, y, width, height, limits, fudge )
     end
     deltax = (limits(2) - limits(1)) / width;
     deltay = (limits(4) - limits(3)) / height;
-    if(nargin < 6)
-        fudge = sqrt(deltax^2 + deltay^2);
-    end
     dmap = zeros(height, width);
     for ii = 0: height - 1
-        yi = limits(3) + ii * deltay + deltay/2;
+        ymin = limits(3) + ii * deltay;
+        ymax = limits(3) + ii * deltay + deltay;
+        include_ymax = ii == height - 1;
         for jj = 0 : width - 1
-            xi = limits(1) + jj * deltax + deltax/2;
+            xmin = limits(1) + jj * deltax;
+            xmax = limits(1) + jj * deltax + deltax;
+            include_xmax = jj == width -1;
             dd = 0;
             for kk = 1: length(x)
-                dist2 = (x(kk) - xi)^2 + (y(kk) - yi)^2;
-                dd = dd + 1 / ( dist2 + fudge); 
+                curx = x(kk);
+                cury = y(kk);
+                if xmin <= curx && ...
+                   (curx < xmax || (include_xmax && curx == xmax)) && ...
+                   ymin <= cury && cury < ymax && ...
+                   (cury < ymax || (include_ymax && cury == ymax))
+                    dd = dd + 1; 
+                end
             end
             dmap(ii+1,jj+1) = dd;
         end
