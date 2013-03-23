d9db16b073d02e394e7bea30036ee0d95be9aa4c
Eric David Moyer <eric_moyer@yahoo.com>
2013-03-23 15:57:17 -0400

    Lots of plot improvement for my investigation.
    Focused sampling on multiple local-max area and allowed easy changing
    of plot parameters.
    
    Added more documentation of what I'm doing.
    
    Added improved plot of minimum between peaks versus width and
    distance. Now adapts to multiple widths and plots in reverse order so
    that when the lines join at the top of the plot, one can see each line
    at the point where it starts to overwrite the others.
    
    Removed commented out code from plotting transition indices
    
    Added code for area plot of # of peaks for a given distance and width.
    
    Got surface plot of spectra working (although plot of local maxima
    doesn't seem correct yet and the plot, as it sits, is not useful in
    understanding WHY when moving two peaks closer that have merged, they
    can unmerge again.

--- targeted_deconv/QuantMetabMap/investigate_functions_to_determine_which_peaks_to_merge.m
+++ targeted_deconv/QuantMetabMap/investigate_functions_to_determine_which_peaks_to_merge.m
@@ -1,15 +1,40 @@
 function mins=investigate_functions_to_determine_which_peaks_to_merge( )
 % My work investigating which peaks I should in the noiseless_merged peak picker
+%
+% This will eventually be merged with GLBIO2013Analyze.m
+%
 % I have this as a separate function so that I can run it and set
-% breakpoints
+% breakpoints.
 
 %% Calculate raw data
-% Examine minimum between the 
-num_widths = 80;
+% Examine height of local minimum between two peaks as a function of the
+% distance between them and their width. Also calculate the local maxima
+% detected. If num_widths is small I use geometric progression to ensure
+% symmetry in the ratios of the widths.
+num_widths = 40;
+max_width = 3;
+min_width = 1/max_width;
+assert(max_width > min_width);
 if num_widths <= 10
-    widths = exp(linspace(log(1/3),log(3),num_widths)); % Use geometric progression when few widths
+    % Use geometric progression
+    widths = exp(linspace(log(min_width),log(max_width),num_widths)); % Use geometric progression when few widths
 else
-    widths = linspace(log(1/3),log(3),num_widths); %Otherwise use linear progression
+    % Otherwise use linear progression modified to focus on the discontinuity in number of transitions
+    non_function_start_width = 1.7;
+    non_function_end_width = 2.1;
+    num_non_function_widths = ceil(num_widths/3);
+    num_function_widths = num_widths - num_non_function_widths;
+    start_widths_length = non_function_start_width - min_width;
+    end_widths_length = max_width - non_function_end_width;
+    function_widths_length = start_widths_length + end_widths_length;
+    num_start_widths = round(num_function_widths * start_widths_length/function_widths_length);
+    num_end_widths = round(num_function_widths * end_widths_length/function_widths_length);
+    
+    widths = unique([...
+        linspace(min_width, non_function_start_width, num_start_widths + 1) ... % +1 takes care of the fact that duplicate end-point with the following will be eliminated by unique
+        linspace(non_function_start_width, non_function_end_width, num_non_function_widths) ...
+        linspace(non_function_end_width, max_width, num_end_widths + 1) ...     % see previous comment about +1
+        ]);        
 end
 ds=linspace(0,2,1024); ds = ds(2:end);  % 1023 distances from 0 to 2, excluding 0
 
@@ -22,34 +47,38 @@ for w_index = 1:length(widths)
         g = GaussLorentzPeak([1,1,0,0,  2,width,0,d]); 
         x=linspace(0,d,1024); x = x(2:end-1);
         a=g(1).at(x); b=g(2).at(x); s=a+b;
-        local_maxes = [true, s(1:end-1) < s(2:end)] & [s(1:end-1) > s(2:end),true];
-        num_maxes(d_index, w_index) = sum(local_maxes);
+        is_local_max = [true, s(1:end-1) < s(2:end)] & [s(1:end-1) > s(2:end),true];
+        num_maxes(d_index, w_index) = sum(is_local_max);
         mins(d_index, w_index)=min(s); 
     end; 
 end
 
-%% Plot width vs distance
+%% Plot height of minimum vs width vs distance
+% The minimum between the two peaks varies with their widths and the
+% distance between them. This plot attempts to capture some of that.
+%
+% It seems that The wider the larger peak, the farther apart the two peaks
+% are before the lowest point is at the location of the smaller peak. 
 figure(1); clf;
-legend(plot(ds, mins), arrayfun(@(x) sprintf('Width of larger peak %f', x), widths, 'UniformOutput', false));
+if length(widths) > 14
+    plotted_min_indices = unique(round(linspace(1, length(widths), 14)));
+else
+    plotted_min_indices = 1:length(widths);
+end
+plotted_min_indices = sort(plotted_min_indices, 'descend');
+plotted_mins = mins(:, plotted_min_indices);
+plotted_widths = widths(plotted_min_indices);
+legend(plot(ds, plotted_mins), arrayfun(@(x) sprintf('Width of larger peak %f', x), plotted_widths, 'UniformOutput', false));
 title('Minimum between peaks versus distance for peaks of height 1 and 2')
 xlabel('Distance apart');
 ylabel('Height of minimum between peaks');
 
-fprintf([...
-    'The wider the larger peak, the farther apart the two peaks are '...
-    'before the lowest point is at the location of the smaller peak.'...
-    ]);
 
-%% Plot tansition indices
+%% Plot tansition distances
+% Plot the distances at which a transition between 1 and 2 local maxima
+% occurs as a relation between it and width. It can be observed that this
+% relation is not a function (something which shocked me).
 figure(2); clf;
-% legend(plot(ds, num_maxes), arrayfun(@(x) sprintf('Width of larger peak %f', x), widths, 'UniformOutput', false));
-% title('Number of local maxima versus distance')
-% xlabel('Distance apart');
-% ylabel('Number of local maxima');
-
-% Calculate d_index (distance index) at which we switch from 1 maximum to
-% two maxima - assumes that the transition does not occur at the first or
-% last distance examined.
 is_transition = cell2mat(arrayfun(@(w_index) num_maxes(1:end-1, w_index) ~= num_maxes(2:end, w_index), ...
     1:size(widths,2),'UniformOutput',false));
 transition_indices = arrayfun(@(w_index) find(is_transition(:, w_index))', ...
@@ -63,12 +92,24 @@ title('Distance at which the peaks join for a given width')
 xlabel('Width');
 ylabel('Distance at which the peaks join');
 
+%% Plot the number of peaks for given distance and width
+% This is another view of the transition distances, but easier to plot and
+% very obvious where the 2d shape comes from. I just plot an image.
+figure(3); clf;
+colormap(gray(2));
+image(widths, ds, num_maxes)
+set(gca, 'YDir','normal');
+title('# of peaks for a given width and distance (black = 1 peak, white = 2)');
+xlabel('Width');
+ylabel('Distance');
+
 %% Look at width, distance combinations with multiple transitions
 % There are several widths where there is more than one transition between 
-% 1 and 2 peaks when distance is changed. Here I plot the spectra for those 
+% 1 and 2 peaks when distance is changed. Here I plot the spectra for three 
 % widths where there is more than one transition
 width_has_more_than_one_transition = cellfun(@(x) length(x), transition_widths) > 1;
 doubled_width_indices = find(width_has_more_than_one_transition);
+doubled_width_indices = doubled_width_indices(round(linspace(1, length(doubled_width_indices), 3)));
 transition_distance_indices = cell2mat(...
     arrayfun(...
         @(doubled_width_index) ...
@@ -76,29 +117,34 @@ transition_distance_indices = cell2mat(...
         doubled_width_indices, 'UniformOutput',false ...
     )...
 );
-distance_indices = unique([transition_distance_indices, 1:4:length(ds)]);
+distance_indices = unique([transition_distance_indices-1,transition_distance_indices,transition_distance_indices+1, round(linspace(1,length(ds)-1,50))]);
+x = linspace(0, max(ds), 1024);
 for doubled_width_index = doubled_width_indices
     doubled_width = widths(doubled_width_index);
     figure(doubled_width_index);
     clf;
-    x = linspace(0, max(ds), 1024);
     z = zeros(length(distance_indices), length(x));
-    fprintf('%s %s %s %s\n', to_str(size(x)), to_str(size(ds(distance_indices))), to_str(size(z)), to_str(size(c)));
     c = zeros(size(z));
-    for d_index = distance_indices
+    for d_index_index = 1:length(distance_indices)
+        d_index = distance_indices(d_index_index);
         d = ds(d_index);
         g = GaussLorentzPeak([1,1,0,0,  2, doubled_width,0,d]); 
         a=g(1).at(x); b=g(2).at(x); s=a+b;
-        z(d_index, :) = s;
-        c(d_index, :) = s;
+        is_local_max = [true, s(1:end-1) < s(2:end)] & [s(1:end-1) > s(2:end),true];
+        z(d_index_index, :) = s;
+        c(d_index_index, :) = s;
+        c(d_index_index, is_local_max) = 0;
     end
-    for d_index = distance_indices
+    for d_index_index = 1:length(distance_indices)
+        d_index = distance_indices(d_index_index);
         if is_transition(d_index, doubled_width_index)
-            c(d_index, :) = max(max(z));
+            c(d_index_index, :) = max(max(z));
         end
-    end
-    fprintf('%s %s %s %s\n', to_str(size(x)), to_str(size(ds(distance_indices))), to_str(size(z)), to_str(size(c)));
-    surf(x, ds(distance_indices), z, c);
+    end    
+    colormap(jet(256));
+    caxis([min(min(c)) max(max(c))]);
+    surf_h = surf(x, ds(distance_indices), z, c);
+    set(surf_h, 'EdgeColor', 'none', 'FaceColor', 'interp');
     title(sprintf('Width %f', doubled_width));
     xlabel('PPM');
     ylabel('Distance');
