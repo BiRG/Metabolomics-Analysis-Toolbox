c3a6b2c247ffcffb5dfe797455029cf65348a380
Eric David Moyer <eric_moyer@yahoo.com>
2013-03-23 18:26:34 -0400

    Added image based spectrum vs. distance plots and explored individual spectra. Also increased number of points calculated and added a waitbar during the main calculation. (It takes about 10 minutes on my home pc). I can now see the behavior of the disappearing and reappearing multiple maxima on the image plots and it makes a little sense - though I still don't understand it. I intend to plot the derivative=0 curves as a function of width and distance to see if that helps me. But that will require installing Mathematica or learning how to use Matlab's symbolic stuff - which may not be up to the math yet since it is new.

--- targeted_deconv/QuantMetabMap/investigate_functions_to_determine_which_peaks_to_merge.m
+++ targeted_deconv/QuantMetabMap/investigate_functions_to_determine_which_peaks_to_merge.m
@@ -11,7 +11,7 @@ function mins=investigate_functions_to_determine_which_peaks_to_merge( )
 % distance between them and their width. Also calculate the local maxima
 % detected. If num_widths is small I use geometric progression to ensure
 % symmetry in the ratios of the widths.
-num_widths = 40;
+num_widths = 96;
 max_width = 3;
 min_width = 1/max_width;
 assert(max_width > min_width);
@@ -36,11 +36,17 @@ else
         linspace(non_function_end_width, max_width, num_end_widths + 1) ...     % see previous comment about +1
         ]);        
 end
-ds=linspace(0,2,1024); ds = ds(2:end);  % 1023 distances from 0 to 2, excluding 0
+ds=linspace(0,2,2048); ds = ds(2:end);  % 2047 distances from 0 to 2, excluding 0
 
+start_time = now;
+wait_h = waitbar(0,'Calculating width 0 of 1000000');
 mins=zeros(size(ds,2),size(widths,2)); 
 num_maxes = mins;
 for w_index = 1:length(widths)
+    elapsed_time = now - start_time;
+    time_per = elapsed_time / (w_index - 1);
+    time_remaining = (length(widths) - w_index + 1) * time_per * 24 * 60;
+    waitbar((w_index-1)/length(widths), wait_h, sprintf('Calculating width %d of %d. %g min remain', w_index, length(widths), time_remaining));
     width = widths(w_index);
     for d_index = 1:length(ds); 
         d=ds(d_index); 
@@ -52,6 +58,7 @@ for w_index = 1:length(widths)
         mins(d_index, w_index)=min(s); 
     end; 
 end
+delete(wait_h);
 
 %% Plot height of minimum vs width vs distance
 % The minimum between the two peaks varies with their widths and the
@@ -106,7 +113,11 @@ ylabel('Distance');
 %% Look at width, distance combinations with multiple transitions
 % There are several widths where there is more than one transition between 
 % 1 and 2 peaks when distance is changed. Here I plot the spectra for three 
-% widths where there is more than one transition
+% widths where there is more than one transition.
+%
+% What I want to see is how two peaks, when they have been close enough to
+% merge into a single local maximum can then separate again into multiple
+% maxima.
 width_has_more_than_one_transition = cellfun(@(x) length(x), transition_widths) > 1;
 doubled_width_indices = find(width_has_more_than_one_transition);
 doubled_width_indices = doubled_width_indices(round(linspace(1, length(doubled_width_indices), 3)));
@@ -151,6 +162,89 @@ for doubled_width_index = doubled_width_indices
     zlabel('Intensity');
 end
 
+
+%% Look at width, distance combinations with multiple transitions (as images)
+% There are several widths where there is more than one transition between 
+% 1 and 2 peaks when distance is changed. Here I plot the spectra for three 
+% widths where there is more than one transition.
+%
+% This time, I plot the spectra as images (height maps) this is the same as
+% doing the surface plots on the x-z axis only. Doing this, I can use all
+% the calculated distances without bogging down the computer.
+width_has_more_than_one_transition = cellfun(@(x) length(x), transition_widths) > 1;
+doubled_width_indices = find(width_has_more_than_one_transition);
+doubled_width_indices = doubled_width_indices(round(linspace(1, length(doubled_width_indices), 3)));
+distance_indices = 1:length(ds);
+x = linspace(0, max(ds), 1024);
+for doubled_width_index = doubled_width_indices
+    doubled_width = widths(doubled_width_index);
+    figure(doubled_width_index);
+    clf;
+    c = zeros(size(z));
+    for d_index_index = 1:length(distance_indices)
+        d_index = distance_indices(d_index_index);
+        d = ds(d_index);
+        g = GaussLorentzPeak([1,1,0,0,  2, doubled_width,0,d]); 
+        a=g(1).at(x); b=g(2).at(x); s=a+b;
+        is_local_max = [true, s(1:end-1) < s(2:end)] & [s(1:end-1) > s(2:end),true];
+        c(d_index_index, :) = s;
+        c(d_index_index, is_local_max) = 0;
+    end
+    for d_index_index = 1:length(distance_indices)
+        d_index = distance_indices(d_index_index);
+        if d_index < length(ds) && is_transition(d_index, doubled_width_index)
+            c(d_index_index, :) = max(max(c));
+        end
+    end
+    minc = min(min(c));
+    maxc = max(max(c));
+    num_colors = 512;
+    c = num_colors*(c - minc)/(maxc-minc);
+    colormap(jet(num_colors));
+    
+    image(ds(distance_indices), x, c');
+    title(sprintf('Width %f', doubled_width));
+    set(gca, 'YDir', 'normal');
+    xlabel('Distance');
+    ylabel('PPM');
+end
+
+%% Look at details of what the peaks do in the 4 regions revealed above
+%
+% Here I look at the behavior of width 1.976923. It has behavior similar to
+% the first plot above - which has 4 regions: 0 to first
+% transition, first to second, second to third and third to infinity. The
+% three transitions are: [1.07917888563049846, 1.18866080156402742, 1.3431085043988269]
+%
+% I plot the spectrum for two peaks 1 height 1 width 1 and the other
+% height 2 width 1.97692307692307701 using one distance from each region.
+% To make the peak height changes more visible, I only plot the top part of
+% each spectrum.
+%
+% In the first region we have the peaks close enough to create a 
+% shoulder. In the second, they are too far apart to merge, and have a
+% valley in between. In the third, back to a single peak with a noticable
+% shoulder. Finally, in the fourth, the peaks separate for good.
+%
+% Looking at the plots above, it seems that the second boundary moves
+% faster than the other two separating from the first boundary at a
+% critical width and then moving rightward as width increases to join the 
+% third boundary at a second critical width.
+interesting_distances = [1,1.15,1.3,1.4]; 
+interesting_distance_titles = {'Merged (d=1)' ...
+    'Two peaks not shoulder (d=1.15)' 'Shoulder (d=1.3)' ...
+    'Two clear peaks (d=1.4)'};
+for fig=1:length(interesting_distances); 
+    figure(fig); 
+    d = interesting_distances(fig); 
+    width = 1.976923; 
+    x=linspace(-0.5,2.5,1024); 
+    g = GaussLorentzPeak([1,1,0,0,  2, width,0,d]); 
+    a=g(1).at(x); b=g(2).at(x); s=a+b; 
+    plot(x, s, 'b', x, a, 'm', x, b, 'm'); 
+    ylim([1.60,2.196]); xlim([min(x) max(x)]); 
+    title(interesting_distance_titles{fig});
+end
 
 %% Plot whether mins are monotonic
 mins_are_sorted = zeros(1, size(mins,2)); 
