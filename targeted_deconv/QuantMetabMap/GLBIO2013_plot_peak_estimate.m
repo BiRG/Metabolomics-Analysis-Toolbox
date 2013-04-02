6b14dcca99a39e10954355dd98e3f09b790cb03d
Eric Moyer <eric_moyer@yahoo.com>
2013-04-02 18:23:49 -0400

    Added stub for my peak estimate plotting code.

---
+++ targeted_deconv/QuantMetabMap/GLBIO2013_plot_peak_estimate.m
@@ -0,0 +1,7 @@
+function [ output_args ] = GLBIO2013_plot_peak_estimate( input_args )
+%GLBIO2013_PLOT_PEAK_ESTIMATE Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
