47559c21c8c735c52501f788184ae65ef79f0581
Eric David Moyer <eric_moyer@yahoo.com>
2013-05-16 18:21:50 -0400

    Implemented GLBIO2013_extreme_loc_param_pairs with a bit of simple bench testing.

--- targeted_deconv/QuantMetabMap/GLBIO2013_extreme_loc_param_pairs.m
+++ targeted_deconv/QuantMetabMap/GLBIO2013_extreme_loc_param_pairs.m
@@ -32,7 +32,19 @@ function extreme = GLBIO2013_extreme_loc_param_pairs(results, loc_param_errs)
 %     extreme(3) describes the peak with the maximum parameter error among
 %          the peaks in the bottom 1% of initial peak location error
 %        
-% 
+%     The fields of extreme(i) are:
+%
+%          datum - the GLBIO2013Datum object where the peak resides
+%
+%          result_idx - the index of that datum object in the original
+%               experimental results array passed to this function
+%
+%          deconv_idx - the index of the deconvolution where the peak
+%               resides within datum.deconvolutions
+%
+%          deconv_peak_idx - the index of the peak object itself within
+%               datum.deconvolutions(deconv_idx).peaks
+%
 % -------------------------------------------------------------------------
 % Examples
 % -------------------------------------------------------------------------
@@ -44,4 +56,32 @@ function extreme = GLBIO2013_extreme_loc_param_pairs(results, loc_param_errs)
 %
 % Eric Moyer (eric_moyer@yahoo.com) May 2013
 
+% Flatten the loc_param_errs struct array
+loc_e = [loc_param_errs.peak_loc_error];
+par_e = [loc_param_errs.param_error];
 
+result_idxs = [loc_param_errs.result_idx];
+deconv_idxs = [loc_param_errs.deconv_idx];
+deconv_pk_idxs = [loc_param_errs.deconv_pk_idx];
+
+% Find the flattened indices where the extreme values lie. 
+is_top_loc_e = loc_e >= prctile(loc_e, 99);
+is_bot_loc_e = loc_e <= prctile(loc_e,  1);
+
+par_e_for_top_loc = par_e(is_top_loc_e);
+par_e_for_bot_loc = par_e(is_bot_loc_e);
+
+extreme_idx(1) = find(par_e == min(par_e_for_top_loc) & is_top_loc_e,1,'first');
+extreme_idx(2) = find(par_e == max(par_e_for_top_loc) & is_top_loc_e,1,'first');
+extreme_idx(3) = find(par_e == max(par_e_for_bot_loc) & is_bot_loc_e,1,'first');
+
+% Fill the result array from the flattened indices
+extreme(3) = struct('result_idx',[], 'datum',results(1), 'deconv_idx',[], ...
+    'deconv_peak_idx',[]);
+for i = 1:3
+    e = extreme_idx(i);
+    extreme(i).result_idx = result_idxs(e); 
+    extreme(i).datum = results(extreme(i).result_idx); 
+    extreme(i).deconv_idx = deconv_idxs(extreme_idx(i)); 
+    extreme(i).deconv_peak_idx = deconv_pk_idxs(extreme_idx(i)); 
+end
