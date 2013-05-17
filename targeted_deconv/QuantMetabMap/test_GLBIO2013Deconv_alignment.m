1780749568c9e213d5c3c459264d5e346644fffa
Eric Moyer <eric_moyer@yahoo.com>
2013-05-17 18:13:09 -0400

    Non-working commit!
    Had to stop in the middle to go to science olympiad.

--- targeted_deconv/QuantMetabMap/test_GLBIO2013Deconv_alignment.m
+++ targeted_deconv/QuantMetabMap/test_GLBIO2013Deconv_alignment.m
@@ -15,24 +15,72 @@ assertEqual(c, sum(abs(l1(a > 0)-l2(a(a > 0))).^exponent));
 function test_location_alignment %#ok<DEFNU>
 % Check that lists of locations are aligned correctly and with the correct costs.
 
-assertSingleAssignment([1,2,3], [3.1,2.8,1.8], 2, [3,2,1]);
-assertSingleAssignment([1,2,3],[3.1,2.99,1.8], 2, [3,2,1]);
-assertSingleAssignment([1,2,3],[3.1,3.101,1.8], 2, [3,1,2]);
-assertSingleAssignment([1,2,3,4],[3.1,3.101,1.8], 2, [0,3,1,2]);
-assertSingleAssignment([1,2,3,4],[1,2,3], 2, [1,2,3,0]);
-assertSingleAssignment([],[1,2,3], 2, zeros(1,0));
-assertSingleAssignment([1,2,3,4],[], 2, [0,0,0,0]);
-assertSingleAssignment([1,2,3,4],2, 2, [0,1,0,0]);
-assertSingleAssignment(3, 2, 2, 1);
-assertSingleAssignment(3, [1,4,7], 2, 2);
-assertSingleAssignment(3, [1,4], 2, 2);
-assertSingleAssignment(2, [1,4], 2, 1);
-assertSingleAssignment([1,2,3], [1,2,3], 2, [1,2,3]);
-assertSingleAssignment([2,1,3], [1,2,3], 2, [2,1,3]);
-assertSingleAssignment([2,3,1], [1,2,3], 2, [2,3,1]);
-assertSingleAssignment([3,2,1], [1,2,3], 2, [3,2,1]);
-assertSingleAssignment([3,1,2], [1,2,3], 2, [3,1,2]);
-assertSingleAssignment([1,3,2], [1,2,3], 2, [1,3,2]);
+exponent = 2;
+assertSingleAssignment([1,2,3], [3.2,0.2  ,3.1], exponent, [2,3,1]);
+assertSingleAssignment([1,2,3], [3.1,2.8  ,1.8], exponent, [3,2,1]);
+assertSingleAssignment([1,2,3], [3.1,2.99 ,1.8], exponent, [3,2,1]);
+assertSingleAssignment([1,2,3], [3.1,3.101,1.8], exponent, [3,1,2]);
+assertSingleAssignment([1,2,3,4],[3.1,3.101,1.8], exponent, [0,3,1,2]);
+assertSingleAssignment([1,2,3,4],[1,2,3], exponent, [1,2,3,0]);
+assertSingleAssignment([],[1,2,3], exponent, zeros(1,0));
+assertSingleAssignment([1,2,3,4],[], exponent, [0,0,0,0]);
+assertSingleAssignment([1,2,3,4],2, exponent, [0,1,0,0]);
+assertSingleAssignment(3, 2, exponent, 1);
+assertSingleAssignment(3, [1,4,7], exponent, 2);
+assertSingleAssignment(3, [1,4], exponent, 2);
+assertSingleAssignment(2, [1,4], exponent, 1);
+assertSingleAssignment([1,2,3], [1,2,3], exponent, [1,2,3]);
+assertSingleAssignment([2,1,3], [1,2,3], exponent, [2,1,3]);
+assertSingleAssignment([2,3,1], [1,2,3], exponent, [2,3,1]);
+assertSingleAssignment([3,2,1], [1,2,3], exponent, [3,2,1]);
+assertSingleAssignment([3,1,2], [1,2,3], exponent, [3,1,2]);
+assertSingleAssignment([1,3,2], [1,2,3], exponent, [1,3,2]);
+
+exponent = 1;
+assertSingleAssignment([1,2,3], [3.8,2.3  ,2.1], exponent, [2,3,1]);
+assertSingleAssignment([1,2,3], [3.2,0.2  ,3.1], exponent, [2,1,3]);
+assertSingleAssignment([1,2,3], [3.1,2.8  ,1.8], exponent, [3,2,1]);
+assertSingleAssignment([1,2,3], [3.1,2.99 ,1.8], exponent, [3,2,1]);
+assertSingleAssignment([1,2,3], [3.1,3.101,1.8], exponent, [3,1,2]);
+assertSingleAssignment([1,2,3,4],[3.1,3.101,1.8], exponent, [0,3,1,2]);
+assertSingleAssignment([1,2,3,4],[1,2,3], exponent, [1,2,3,0]);
+assertSingleAssignment([],[1,2,3], exponent, zeros(1,0));
+assertSingleAssignment([1,2,3,4],[], exponent, [0,0,0,0]);
+assertSingleAssignment([1,2,3,4],2, exponent, [0,1,0,0]);
+assertSingleAssignment(3, 2, exponent, 1);
+assertSingleAssignment(3, [1,4,7], exponent, 2);
+assertSingleAssignment(3, [1,4], exponent, 2);
+assertSingleAssignment(2, [1,4], exponent, 1);
+assertSingleAssignment([1,2,3], [1,2,3], exponent, [1,2,3]);
+assertSingleAssignment([2,1,3], [1,2,3], exponent, [2,1,3]);
+assertSingleAssignment([2,3,1], [1,2,3], exponent, [2,3,1]);
+assertSingleAssignment([3,2,1], [1,2,3], exponent, [3,2,1]);
+assertSingleAssignment([3,1,2], [1,2,3], exponent, [3,1,2]);
+assertSingleAssignment([1,3,2], [1,2,3], exponent, [1,3,2]);
+
+exponent = 0.5;
+assertSingleAssignment([1,2,3], [3.8,2.3  ,2.1], exponent, [2,3,1]);
+assertSingleAssignment([1,2,3], [3.2,0.2  ,3.1], exponent, [2,1,3]);
+assertSingleAssignment([1,2,3], [3.1,2.8  ,1.8], exponent, [3,2,1]);
+assertSingleAssignment([1,2,3], [3.1,2.99 ,1.8], exponent, [1,3,2]);
+assertSingleAssignment([1,2,3], [3.1,3.101,1.8], exponent, [2,3,1]);
+assertSingleAssignment([1,2,3,4],[3.1,3.101,1.8], exponent, [0,3,1,2]);
+assertSingleAssignment([1,2,3,4],[1,2,3], exponent, [1,2,3,0]);
+assertSingleAssignment([],[1,2,3], exponent, zeros(1,0));
+assertSingleAssignment([1,2,3,4],[], exponent, [0,0,0,0]);
+assertSingleAssignment([1,2,3,4],2, exponent, [0,1,0,0]);
+assertSingleAssignment(3, 2, exponent, 1);
+assertSingleAssignment(3, [1,4,7], exponent, 2);
+assertSingleAssignment(3, [1,4], exponent, 2);
+assertSingleAssignment(2, [1,4], exponent, 1);
+assertSingleAssignment([1,2,3], [1,2,3], exponent, [1,2,3]);
+assertSingleAssignment([2,1,3], [1,2,3], exponent, [2,1,3]);
+assertSingleAssignment([2,3,1], [1,2,3], exponent, [2,3,1]);
+assertSingleAssignment([3,2,1], [1,2,3], exponent, [3,2,1]);
+assertSingleAssignment([3,1,2], [1,2,3], exponent, [3,1,2]);
+assertSingleAssignment([1,3,2], [1,2,3], exponent, [1,3,2]);
+
+
 
 
 
