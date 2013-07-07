1e627bbdce38e47d3a03b408c014c7a1a045716c
Eric David Moyer <eric_moyer@yahoo.com>
2013-07-07 14:37:34 -0400

    Added missing removal code to the march 7 updater

--- glbio2013_remove_pick_and_update_mar7.sh
+++ glbio2013_remove_pick_and_update_mar7.sh
@@ -15,7 +15,9 @@ warning('off','MATLAB:RandStream:SetDefaultStream');
 in_name  = 'Mar_07_2013_test_experiment_results.mat';
 load(in_name);
 out_name = 'Mar_07_2013_test_experiment_results_after_picker_removal_and_update.mat';
-tic; Mar_07_2013_test_experiment_results_after_picker_removal_and_update=GLBIO2013_run_update_all(1, Mar_07_2013_test_experiment_results); toc
+tic; 
+tmp = GLBIO2013_remove_peak_pickers_from_data({GLBIO2013Deconv.pp_smoothed_local_max, GLBIO2013Deconv.pp_gold_std_aligned_with_local_max}, Mar_07_2013_test_experiment_results);
+Mar_07_2013_test_experiment_results_after_picker_removal_and_update=GLBIO2013_run_update_all(1, tmp); toc
 save(out_name, 'Mar_07_2013_test_experiment_results_after_picker_removal_and_update');
 exit                      % don't forget to exit
 MATLAB_ENV
