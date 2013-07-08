a6d950ee9d177bc6273748ffc3c3268352a7ebd4
Eric David Moyer <eric_moyer@yahoo.com>
2013-07-08 08:36:12 -0400

    Guessing that 'save' not finding the variable name came from truncating it because it was too long.

--- glbio2013_remove_pick_and_update_mar7.sh
+++ glbio2013_remove_pick_and_update_mar7.sh
@@ -17,8 +17,8 @@ load(in_name);
 out_name = 'Mar_07_2013_test_experiment_results_after_picker_removal_and_update.mat';
 tic; 
 tmp = GLBIO2013_remove_peak_pickers_from_data({GLBIO2013Deconv.pp_smoothed_local_max, GLBIO2013Deconv.pp_gold_std_aligned_with_local_max}, Mar_07_2013_test_experiment_results);
-Mar_07_2013_test_experiment_results_after_picker_removal_and_update=GLBIO2013_run_update_all(1, tmp); toc
-save(out_name, 'Mar_07_2013_test_experiment_results_after_picker_removal_and_update');
+Mar_07_2013_results_after_picker_removal_and_update=GLBIO2013_run_update_all(1, tmp); toc
+save(out_name, 'Mar_07_2013_results_after_picker_removal_and_update');
 exit                      % don't forget to exit
 MATLAB_ENV
 # keep this line to ensure newline
