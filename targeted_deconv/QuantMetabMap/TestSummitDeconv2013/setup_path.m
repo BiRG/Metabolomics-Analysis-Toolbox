72fa30afabd3e28e3d1845b808c6412cf9df2799
Eric Moyer <eric_moyer@yahoo.com>
2013-07-25 17:49:20 -0400

    Added code to set up the Matlab path for repeating my summit-focused experiments.

---
+++ targeted_deconv/QuantMetabMap/TestSummitDeconv2013/setup_path.m
@@ -0,0 +1,7 @@
+% Set up the path for running the 2013 summit-focused deconvolution experiments
+
+currentpath = cd('..');      % cd returns the path to the current directory
+parentpath = cd(currentpath);
+addpath(parentpath);
+
+fprintf('Path set up for summit-focused deconvolution experiments.\n');
