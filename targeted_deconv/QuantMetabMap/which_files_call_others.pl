36de09be2f1f8d14741d67176749cca09a9358a9
Eric Moyer <eric_moyer@yahoo.com>
2013-07-25 11:52:09 -0400

    Removed matlab function calling detection where the function name was a substring of the identifier or was in a comment.

--- targeted_deconv/QuantMetabMap/which_files_call_others.pl
+++ targeted_deconv/QuantMetabMap/which_files_call_others.pl
@@ -6,10 +6,10 @@ use autodie;
 #
 # Lists all pairs of matlab files in this directory where the first
 # calls the second. The pairs are tab-separated, so if there are three
-# files f1.m, f2.m, and f3.m and f1 calls f2 and f3 and f3 calls f2,
+# files f1.m, f2.m, f3.m, and f4.m and f1 calls f3 & f4 and f3 calls f2,
 # the result will be:
-# f1.m\tf2.m
 # f1.m\tf3.m
+# f1.m\tf4.m
 # f3.m\tf2.m
 
 # Read matlab files in directory
@@ -22,7 +22,7 @@ my @funcs = @files;
 # For each file see which function names it contains
 foreach my $file (@files){
     foreach my $func (@funcs){
-	system("grep","-q", $func, $file);
+	system("grep","-q", "^\[^%\]*$func\[^A-Za-z0-9_\]", $file);
 	if ($?==0) { # If there was a match grep's return value is 0
 	    my $funcfile = "${func}.m";
 	    print "$file\t$funcfile\n" if ($file ne $funcfile);
