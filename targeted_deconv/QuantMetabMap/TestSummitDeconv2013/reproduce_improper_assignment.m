62b976ce0b54ff06c8d45d71baee32d87b1ed014
Eric Moyer <eric_moyer@yahoo.com>
2013-07-29 16:34:16 -0400

    Renamed deconv_initial_vals_dirty to deconv_initial_vals_summit in the test summit experiment code. (I had forgotten to update these function calls when I did the rest of the code earlier in commit f48bbd848b9775ce98e477d34f2f52de62ec89d1)
    Tests uncover no errors.

--- targeted_deconv/QuantMetabMap/TestSummitDeconv2013/reproduce_improper_assignment.m
+++ targeted_deconv/QuantMetabMap/TestSummitDeconv2013/reproduce_improper_assignment.m
@@ -4,7 +4,7 @@ function reproduce_improper_assignment( )
 peaks = GaussLorentzPeak([1,.02,1,0.5,   1,.02,1,0.500000001]);
 spec.x = 0:0.0001:1;
 spec.Y = sum(peaks.at(spec.x),1)';
-[b,~,~]=deconv_initial_vals_dirty(spec.x, spec.Y, 0,1,[0.5, 0.500000001], ...
+[b,~,~]=deconv_initial_vals_summit(spec.x, spec.Y, 0,1,[0.5, 0.500000001], ...
     0.04, 12, 75, @do_nothing);
 dec=GaussLorentzPeak(b);
 plot(spec.x, dec(1).at(spec.x), spec.x, dec(2).at(spec.x));
