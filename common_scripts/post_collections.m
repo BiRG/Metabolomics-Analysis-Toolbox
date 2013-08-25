c51acbafe5d1d836867ab0a5a8b4bd3628d54f3a
Eric David Moyer <eric_moyer@yahoo.com>
2013-08-25 11:19:18 -0400

    Added possiblity of providing username and password to post_collectons in arguments rather than only getting them from a dialog. Added documentation to post_collections. Made successful post message both clearer ast to what was being reported and more informative as to which collections were uploaded as which.

--- common_scripts/post_collections.m
+++ common_scripts/post_collections.m
@@ -1,9 +1,62 @@
-function post_collections(main_h,collections,suffix,analysis_id)
+function post_collections(main_h,collections,suffix,analysis_id,username,password)
+% Ask for user information then post the given collections to the BIRG server
+%
+% The given collections are first saved to a temporary directory and then
+% uploaded to BIRG.
+%
+% Code originally by Paul Anderson.  Comments added after-the-fact
+%
+% -------------------------------------------------------------------------
+% Input arguments
+% -------------------------------------------------------------------------
+% main_h - ignored. Used in other functions as a handle to the main window
+%
+% collections - (cell array) a cell array of spectral collections. Each
+%     spectral collection is a struct of spectra. This is the format of
+%     the return value of load_collections.m in common_scripts.
+%
+% suffix - (string) a suffix appended to the collection number when it is
+%     saved in the temp directory
+%
+% analysis_id - (scalar) the id of the analysis to which the uploaded
+%     collections will be added
+%
+% username - (optional string) if either username or password is absent,
+%     then the user will be prompted for it
+%
+% -------------------------------------------------------------------------
+% Output parameters
+% -------------------------------------------------------------------------
+% 
+% None
+% 
+% -------------------------------------------------------------------------
+% Examples
+% -------------------------------------------------------------------------
+%
+% -------------------------------------------------------------------------
+% Authors
+% -------------------------------------------------------------------------
+%
+% Paul Anderson (Before July 2011) 
+%
+% Eric Moyer (July 2013) eric_moyer@yahoo.com
+
+% Get the username and password from the user if not passed in as
+% parameters. Exit without posting if the user cancels the dialog
+if(~exist('username','var') || ~exist('password','var'))
+    [username,password] = logindlg;
+    if ~(ischar(username) && ischar(password))
+        return;
+    end
+end
+
 tmpdir = tempname;
 mkdir(tmpdir);
-[username,password] = logindlg;
+
 for i = 1:length(collections)
-    file = save_collection(tmpdir,suffix,collections{i});    
+    collection = collections{i};
+    file = save_collection(tmpdir,suffix,collection);    
     url = sprintf('http://birg.cs.wright.edu/omics_analysis/spectra_collections.xml');
     xml = urlread(url,'post',{'name',username,'password',password,'analysis_id',num2str(analysis_id),'collection[data]',fileread(file)});
     delete(file);
@@ -14,7 +67,7 @@ for i = 1:length(collections)
     fclose(fid);
     collection_xml = xml2struct(file);
     id = collection_xml.Children.Data;
-    fprintf('Collection %d: %s\n',i,id);
+    fprintf('Successfully posted collection %s-%s as collection %d: %s\n',collection.collection_id, suffix, i,id);
     delete(file);
 end
 rmdir(tmpdir);
