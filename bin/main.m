453fc859afaf11af91bd7ac540812b9cd3519af3
Dan C. Wlodarski <daniel.wlodarski@wright.edu>
2013-09-25 16:10:46 -0400

    DAB: Backwards bin load compatibility
    Added the requested backwards compatibility to the Dynamic adaptive
    binning tool's load bin file feature. Saving is in the CSV format ONLY.

--- bin/main.m
+++ bin/main.m
@@ -880,7 +880,7 @@ if ~is_valid_state
     return;
 end
 
-[filename,pathname] = uiputfile('*.csv', 'Save regions');
+[filename,pathname] = uiputfile({'*.csv','Row-dominant comma-delimited (*.csv)'}, 'Save regions');
 
 if (isnumeric(filename) && filename == 0)
     return;
@@ -925,11 +925,12 @@ if ~is_valid_state
     return;
 end
 
-[filename, pathname] = uigetfile('*.csv', 'Load regions');
+[filename, pathname] = uigetfile({'*.csv','Row-dominant comma-delimited (*.csv)';'*.txt','Old column-dominant (*.txt)'}, 'Load regions');
 if (isnumeric(filename) && filename == 0)
     return;
 end
 
+% TODO: Take this file opening block out & put it into its own lib.
 [file_id, message] = fopen([pathname filename],'r');
 if (file_id <= 2)
     msgbox(message);
@@ -939,13 +940,56 @@ end
 binFileData = textscan(file_id,'%f,%f,%s','Delimiter','\n');
 fclose(file_id);
 
+% Check for old file format.
+if (isempty(binFileData{3}))
+    % It's the old format. Reread file and fit it to the structure.
+    
+    % TODO: Take this file opening block out & put it into its own lib.
+    [file_id, message] = fopen([pathname filename],'r');
+    % Error-check again, just to be paranoid.
+    if (file_id <= 2)
+        msgbox(message);
+        return;
+    end
+    
+    % Pull the comma'd pairs, delimited by semicolons.
+    binFileData = textscan(file_id,'%f,%f','Delimiter',';\n');
+    
+    % Pull the operation strings ("sum" vs. "deconvolve") and name
+    % strings associated with each bin, delimited by semicolons. We
+    % bastardize the stringFields variable here as it will all need to end
+    % up there anyways.
+    stringFields = textscan(file_id,'%s','EndOfLine','\n');
+    fclose(file_id);
+    
+    % Have to dereference the wrapping 1x1 cell before splitting.
+    tempStringFields = split(stringFields{1},';');
+    stringFields = cell(length(tempStringFields{1}),1);
+    
+    if (length(tempStringFields) == 1)
+        % There were no names. Append a cell array of blanks.
+        tempStringFields = {tempStringFields{1} ; cell(1, length(tempStringFields{1}))};
+    end
+    
+    % Merge stringFields & nameFields into the nx1 cell array structure 
+    % with each element referencing a 1x2 cell of strings.
+    for index = 1:length(stringFields)
+        stringFields{index} = { tempStringFields{1}{index} tempStringFields{2}{index} };
+    end
+    
+else
+    % It's the current format!
+    
+    % Parse out the operation strings ("sum" vs. "deconvolve")
+    % from the name strings.
+    stringFields = regexp(binFileData{3},'"(.*)","(.*)"$','tokens');
+    
+    % Flatten the structure
+    stringFields = stringFields(:);
+    stringFields = [ stringFields{:} ]';
+end
+
 regions = [ binFileData{1} binFileData{2} ];
-stringFields = regexp(binFileData{3},'"(.*)","(.*)"$','tokens');
-
-% Flatten the structure
-stringFields = stringFields(:);
-stringFields = [ stringFields{:} ]';
-
 binCount = length(stringFields);
 
 % Due to the cell array structure, have to iterate through
