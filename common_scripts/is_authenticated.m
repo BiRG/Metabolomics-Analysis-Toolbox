82cd822b82ff586aab014bd17a630faf498235ab
Daniel Foose <dpfoose@gmail.com>
2019-02-20 00:33:34 -0500

    changes for compatibility with Omics Dashboard v19.02a

--- common_scripts/is_authenticated.m
+++ common_scripts/is_authenticated.m
@@ -1,7 +1,7 @@
 function authenticated = is_authenticated()
 try
     omics_weboptions = evalin('base', 'omics_weboptions');
-    res = webread('https://birg.cs.wright.edu/omics/api/currentuser', omics_weboptions);
+    res = webread('https://birg.cs.wright.edu/omics/api/current_user', omics_weboptions);
     if isfield(res, 'name')
         authenticated = 1;
         return;
